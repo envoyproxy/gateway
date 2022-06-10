e9217a491cff803ef9e638d29fc3ac1c11f39524
Arko Dasgupta <arkodg@users.noreply.github.com>
2022-06-10 09:47:33 -0700

    Run make inside a container with dev tools (#102)
    * Run make inside a container with dev tools
    
    * Adds an option `MAKE_IN_DOCKER=1 make <target>`
    to allow the dev to run the make target inside a container
    with preinstalled dev tools
    
    * By default `MAKE_IN_DOCKER` is unset, so `make` would run
    locally on the host
    
    Relates to https://github.com/envoyproxy/gateway/issues/90
    
    Signed-off-by: Arko Dasgupta <arko@tetrate.io>

---
+++ DEVELOPER.md
@@ -0,0 +1,31 @@
+# Developer documentation
+
+Envoy Gateway is built using a [make](https://www.gnu.org/software/make/)-based
+build system. Our CI is based on [Github Actions](https://docs.github.com/en/actions) (see: [workflows](.github/workflows))
+
+## Prerequisites
+
+### go
+* Version: 1.18.2
+* Installation Guide: https://go.dev/doc/install to
+
+### make
+* Recommended Version: 4.3 
+* Installation Guide: https://www.gnu.org/software/make/).
+
+### docker
+* Optional when you want to build a Docker image or run make inside Docker.
+* Recommened Version: 20.10.16
+* Installation Guide: https://docs.docker.com/engine/install/
+
+### linters
+* [TODO](https://github.com/envoyproxy/gateway/issues/73)
+
+* If you do not have these tools installed on your machine,
+you can alternatively run `MAKE_IN_DOCKER=1 make <target>` to run `make` inside a Docker container which has all the
+preinstalled tools needed to support all the `make` targets.
+
+## Quick start
+
+[TODO](https://github.com/envoyproxy/gateway/issues/101) Run `make help` to see all the available targets to build, test
+and run `envoy-gateway`.
---
+++ Makefile.targets.mk
@@ -0,0 +1,38 @@
+# Golang variables
+GOOS ?= $(shell go env GOOS)
+GOARCH ?= $(shell go env GOARCH)
+
+# Docker variables
+# REGISTRY is the image registry to use for build and push image targets.
+REGISTRY ?= docker.io/envoyproxy
+# IMAGE is the image URL for build and push image targets.
+IMAGE ?= ${REGISTRY}/gateway-dev
+# REV is the short git sha of latest commit.
+REV=$(shell git rev-parse --short HEAD)
+# Tag is the tag to use for build and push image targets.
+TAG ?= $(REV)
+
+.PHONY: build
+build:  ## Build the envoy-gateway binary
+	@CGO_ENABLED=0 go build -a -o ./bin/${GOOS}/${GOARCH}/ github.com/envoyproxy/gateway/cmd/envoy-gateway
+
+build-linux-amd64:
+	@GOOS=linux GOARCH=amd64 $(MAKE) build
+
+build-linux-arm64:
+	@GOOS=linux GOARCH=arm64 $(MAKE) build
+
+build-all: build-linux-amd64 build-linux-arm64
+
+.PHONY: test
+test:
+	@go test ./...
+
+.PHONY: docker-build
+docker-build: build-all ## Build the envoy-gateway docker image.
+	@DOCKER_BUILDKIT=1 docker build -t $(IMAGE):$(TAG) -f Dockerfile bin 
+
+.PHONY: docker-push
+docker-push: ## Push the docker image for envoy-gateway.
+	@docker push $(IMAGE):$(TAG)
+
