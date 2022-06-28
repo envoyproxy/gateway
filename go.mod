d530327d18bd5f4f555a615bccfb538a0089adf2
Arko Dasgupta <arkodg@users.noreply.github.com>
2022-06-28 15:41:43 -0700

    [ir] Init Structure (#115)
    * [ir] Init Structure
    
    Relates to https://github.com/envoyproxy/gateway/issues/32
    
    Signed-off-by: Arko Dasgupta <arko@tetrate.io>

--- go.mod
+++ go.mod
@@ -3,14 +3,19 @@ module github.com/envoyproxy/gateway
 go 1.18
 
 require (
+	github.com/envoyproxy/go-control-plane v0.10.1
 	github.com/spf13/cobra v1.4.0
 	github.com/stretchr/testify v1.7.1
 )
 
 require (
+	github.com/cncf/xds/go v0.0.0-20211001041855-01bcc9b48dfe // indirect
 	github.com/davecgh/go-spew v1.1.0 // indirect
+	github.com/envoyproxy/protoc-gen-validate v0.1.0 // indirect
+	github.com/golang/protobuf v1.5.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 )
---
+++ pkg/ir/xds.go
@@ -0,0 +1,74 @@
+package ir
+
+// Xds holds the intermediate representation of a Gateway and is
+// used by the xDS Translator to convert it into xDS resources.
+type Xds struct {
+	// Name of the Xds IR.
+	Name string
+	// HTTP listeners exposed by the gateway.
+	HTTP []HTTPListener
+}
+
+// HTTPListener holds the listener configuration.
+type HTTPListener struct {
+	// Name of the HttpListener
+	Name string
+	// Address that the listener should listen on.
+	Address string
+	// Port on which the service can be expected to be accessed by clients.
+	Port uint32
+	// Hostnames (Host/Authority header value) with which the service can be expected to be accessed by clients.
+	// This field is required. Wildcard hosts are supported in the suffix or prefix form.
+	// Refer to https://www.envoyproxy.io/docs/envoy/latest/api-v3/config/route/v3/route_components.proto#config-route-v3-virtualhost
+	// for more info.
+	Hostnames []string
+	// Tls certificate info. If omitted, the gateway will expose a plain text HTTP server.
+	TLS *TLSListenerConfig
+	// Routes associated with HTTP traffic to the service.
+	Routes []*HTTPRoute
+}
+
+// HTTPRoute holds the route information associated with the HTTP Route
+type HTTPRoute struct {
+	// Name of the HttpRoute
+	Name string
+	// PathMatch defines the match conditions on the path.
+	PathMatch *StringMatch
+	// HeaderMatches define the match conditions on the request headers for this route.
+	HeaderMatches []*StringMatch
+	// QueryParamMatches define the match conditions on the query parameters.
+	QueryParamMatches []*StringMatch
+	// Destinations associated with this matched route.
+	Destinations []*RouteDestination
+}
+
+// RouteDestination holds the destination details associated with the route
+type RouteDestination struct {
+	// Host refers to the FQDN or IP address of the backend service.
+	Host string
+	// Port on the service to forward the request to.
+	Port uint32
+	// Weight associated with this destination.
+	Weight uint32
+}
+
+// TLSListenerConfig holds the configuration for downstream TLS context.
+type TLSListenerConfig struct {
+	// ServerCertificate of the server.
+	ServerCertificate []byte
+	// PrivateKey for the server.
+	PrivateKey []byte
+}
+
+// StringMatch holds the various match conditions.
+// Only one of Exact, Prefix or SafeRegex can be set.
+type StringMatch struct {
+	// Name of the field to match on.
+	Name string
+	// Exact match condition.
+	Exact *string
+	// Prefix match condition.
+	Prefix *string
+	// SafeRegex match condition.
+	SafeRegex *string
+}
