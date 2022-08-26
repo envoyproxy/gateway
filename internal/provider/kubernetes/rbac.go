80bc6122bbabcf1a7e3a6b36d4b5817a6ac6805e
danehans <daneyonhansen@gmail.com>
2022-08-26 13:07:17 -0700

    Updates RBAC for Infra Manager Permissions
    Signed-off-by: danehans <daneyonhansen@gmail.com>

--- internal/provider/kubernetes/rbac.go
+++ internal/provider/kubernetes/rbac.go
@@ -3,4 +3,7 @@ package kubernetes
 // +kubebuilder:rbac:groups="gateway.networking.k8s.io",resources=gatewayclasses;gateways;httproutes;referencepolicies;referencegrants,verbs=get;list;watch
 // +kubebuilder:rbac:groups="gateway.networking.k8s.io",resources=gatewayclasses/status;gateways/status;httproutes/status,verbs=update
 
-// +kubebuilder:rbac:groups="",resources=secrets;services;namespaces,verbs=get;list;watch
+// RBAC for Infra Manager to manage Envoy.
+// +kubebuilder:rbac:groups="",resources=secrets;namespaces,verbs=get;list;watch
+// +kubebuilder:rbac:groups="",resources=services;serviceaccounts,verbs=get;list;watch;create;update;delete
+// +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;delete
