47a24e41ad7c1c0bf3327d093ee6dd8d57ce444c
Flynn <emissary@flynn.kodachi.com>
2022-09-06 16:14:02 -0400

    Pull Envoy and Gateway API versions out of code. Include them dynamically in the docs index, and manually in a barebones compatibility matrix.
    Signed-off-by: Flynn <emissary@flynn.kodachi.com>

--- tools/make/docs.mk
+++ tools/make/docs.mk
@@ -3,7 +3,7 @@ DOCS_OUTPUT_DIR := docs/html
 
 .PHONY: docs
 docs: $(tools/sphinx-build) $(tools/goversion)
-	BUILD_VERSION=$(shell $(tools/goversion)) $(tools/sphinx-build) -j auto -b html $(DOCS_DIR) $(DOCS_OUTPUT_DIR)
+	env BUILD_VERSION=$(shell $(tools/goversion)) $(shell go run ./cmd/envoy-gateway versions --env) $(tools/sphinx-build) -j auto -b html $(DOCS_DIR) $(DOCS_OUTPUT_DIR)
 
 .PHONY: docs.clean
 docs.clean: ## Clean the built docs
