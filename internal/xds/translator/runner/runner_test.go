49fc63a0d7b5c383dac0582f69a292a58aeceae9
Arko Dasgupta <arko@tetrate.io>
2022-09-12 13:34:47 -0700

    Add test for xds translator runner
    Fixes: https://github.com/envoyproxy/gateway/issues/227
    
    Signed-off-by: Arko Dasgupta <arko@tetrate.io>

---
+++ internal/xds/translator/runner/runner_test.go
@@ -0,0 +1,84 @@
+package runner
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/envoyproxy/gateway/internal/envoygateway/config"
+	"github.com/envoyproxy/gateway/internal/ir"
+	"github.com/envoyproxy/gateway/internal/message"
+	xdstypes "github.com/envoyproxy/gateway/internal/xds/types"
+	resourcev3 "github.com/envoyproxy/go-control-plane/pkg/resource/v3"
+)
+
+func TestRunner(t *testing.T) {
+	// Setup
+	xdsIR := new(message.XdsIR)
+	xds := new(message.Xds)
+	cfg, err := config.NewDefaultServer()
+	require.NoError(t, err)
+	r := New(&Config{
+		Server: *cfg,
+		XdsIR:  xdsIR,
+		Xds:    xds,
+	})
+
+	ctx := context.Background()
+	// Start
+	err = r.Start(ctx)
+	require.NoError(t, err)
+
+	// xDS is nil at start
+	require.Equal(t, (*xdstypes.ResourceVersionTable)(nil), xds.Get())
+
+	// test translation
+	path := "example"
+	res := ir.Xds{
+		HTTP: []*ir.HTTPListener{
+			{
+				Name:      "test",
+				Address:   "0.0.0.0",
+				Port:      80,
+				Hostnames: []string{"example.com"},
+				Routes: []*ir.HTTPRoute{
+					{
+						Name: "test-route",
+						PathMatch: &ir.StringMatch{
+							Exact: &path,
+						},
+						Destinations: []*ir.RouteDestination{
+							{
+								Host: "10.11.12.13",
+								Port: 8080,
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	xdsIR.Store("test", &res)
+	require.Eventually(t, func() bool {
+		out := xds.Get()
+		if out == nil {
+			return false
+		}
+		// Ensure an xds listener is created
+		return len(out.XdsResources[resourcev3.ListenerType]) == 1
+	}, time.Second*1, time.Millisecond*20)
+
+	// Update with an empty IR triggering a delete
+	xdsIR.Store("test", &ir.Xds{})
+	require.Eventually(t, func() bool {
+		out := xds.Get()
+		if out == nil {
+			return false
+		}
+		// Ensure an xds listener is created
+		return len(out.XdsResources[resourcev3.ListenerType]) == 0
+	}, time.Second*1, time.Millisecond*20)
+
+}
