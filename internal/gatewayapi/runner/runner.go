envoyproxy/gateway

commit d926eacf274a217ba920a8be0ae0870612ea4d2a

add checks for namespace and service notifications (#420)
* wait until all the gateway API resources have been
initialized before passing the xds IR to the xds translator

Signed-off-by: Arko Dasgupta <arko@tetrate.io>

internal/gatewayapi/runner/runner.go
@@ -22,6 +22,7 @@ type Config struct {
 
 type Runner struct {
 	Config
+	xdsIRReady bool
 }
 
 func New(cfg *Config) *Runner {
@@ -41,8 +42,6 @@ func (r *Runner) Start(ctx context.Context) error {
 }
 
 func (r *Runner) subscribeAndTranslate(ctx context.Context) {
-	var xdsIRReady bool
-
 	// Subscribe to resources
 	gatewayClassesCh := r.ProviderResources.GatewayClasses.Subscribe(ctx)
 	gatewaysCh := r.ProviderResources.Gateways.Subscribe(ctx)
@@ -60,11 +59,10 @@ func (r *Runner) subscribeAndTranslate(ctx context.Context) {
 			r.waitUntilGCAndGatewaysInitialized()
 		case <-httpRoutesCh:
 			r.waitUntilAllGAPIInitialized()
-			// Now that the httproute resources have been initialized,
-			// allow the runner to publish the translated xdsIR.
-			xdsIRReady = true
 		case <-servicesCh:
+			r.waitUntilAllGAPIInitialized()
 		case <-namespacesCh:
+			r.waitUntilAllGAPIInitialized()
 		}
 		r.Logger.Info("received a notification")
 		// Load all resources required for translation
@@ -108,7 +106,7 @@ func (r *Runner) subscribeAndTranslate(ctx context.Context) {
 
 			// Wait until all HTTPRoutes have been reconciled , else the translation
 			// result will be incomplete, and might cause churn in the data plane.
-			if xdsIRReady {
+			if r.xdsIRReady {
 				if err := result.XdsIR.Validate(); err != nil {
 					r.Logger.Error(err, "unable to validate xds ir, skipped sending it")
 				} else {
@@ -142,4 +140,7 @@ func (r *Runner) waitUntilGCAndGatewaysInitialized() {
 func (r *Runner) waitUntilAllGAPIInitialized() {
 	r.waitUntilGCAndGatewaysInitialized()
 	r.ProviderResources.HTTPRoutesInitialized.Wait()
+	// Now that the httproute resources have been initialized,
+	// allow the runner to publish the translated xdsIR.
+	r.xdsIRReady = true
 }

commit 7f81feac4eea1d1a661c094847eaea03dba7e368

Stall ns notifications until gws have inited
* Fixes a regression caused by https://github.com/envoyproxy/gateway/pull/420
which stalled namespace notifications until the httproute resource were
initialized, but the namespaces are required to be populated to built
listener context which affects infra IR creation.

Signed-off-by: Arko Dasgupta <arko@tetrate.io>

internal/gatewayapi/runner/runner.go
@@ -62,7 +62,7 @@ func (r *Runner) subscribeAndTranslate(ctx context.Context) {
 		case <-servicesCh:
 			r.waitUntilAllGAPIInitialized()
 		case <-namespacesCh:
-			r.waitUntilAllGAPIInitialized()
+			r.waitUntilGCAndGatewaysInitialized()
 		}
 		r.Logger.Info("received a notification")
 		// Load all resources required for translation