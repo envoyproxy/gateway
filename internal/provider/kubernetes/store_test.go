fb48c25b40385dce2b219308d32d8305e812a066
Shubham Chauhan <shubham@tetrate.io>
2022-10-18 22:01:35 +0530

    sigh
    Signed-off-by: Shubham Chauhan <shubham@tetrate.io>

--- internal/provider/kubernetes/store_test.go
+++ internal/provider/kubernetes/store_test.go
@@ -37,22 +37,22 @@ func testRouteToServicesMappings(t *testing.T, cache *providerReferenceStore) {
 
 	// Add HTTPRoute/ns1/r1 -> ns1/svc1 mapping
 	cache.updateRouteToServicesMapping(httpr1, ns1svc1)
-	require.Equal(t, []types.NamespacedName{ns1svc1}, cache.getRouteToServicesMapping(httpr1))
+	require.Equal(t, map[types.NamespacedName]struct{}{ns1svc1: {}}, cache.getRouteToServicesMapping(httpr1))
 
 	// Add HTTPRoute/ns1/r1 -> ns1/svc2 mapping
 	// Add TLSRoute/ns1/r1 -> ns1/svc2 mapping
 	cache.updateRouteToServicesMapping(tlsr1, ns1svc2)
 	cache.updateRouteToServicesMapping(httpr1, ns1svc2)
-	require.Equal(t, []types.NamespacedName{ns1svc2}, cache.getRouteToServicesMapping(tlsr1))
-	require.Equal(t, []types.NamespacedName{ns1svc1, ns1svc2}, cache.getRouteToServicesMapping(httpr1))
+	require.Equal(t, map[types.NamespacedName]struct{}{ns1svc2: {}}, cache.getRouteToServicesMapping(tlsr1))
+	require.Equal(t, map[types.NamespacedName]struct{}{ns1svc1: {}, ns1svc2: {}}, cache.getRouteToServicesMapping(httpr1))
 
 	// Remove HTTPRoute/ns1/r1 -> ns1/svc1 mapping
 	cache.removeRouteToServicesMapping(httpr1, ns1svc1)
-	require.Equal(t, []types.NamespacedName{ns1svc2}, cache.getRouteToServicesMapping(httpr1))
+	require.Equal(t, map[types.NamespacedName]struct{}{ns1svc2: {}}, cache.getRouteToServicesMapping(httpr1))
 
 	// Remove TLSRoute/ns1/r1 -> ns1/svc2 mapping
 	cache.removeRouteToServicesMapping(tlsr1, ns1svc2)
-	require.Equal(t, []types.NamespacedName{}, cache.getRouteToServicesMapping(tlsr1))
+	require.Equal(t, map[types.NamespacedName]struct{}(nil), cache.getRouteToServicesMapping(tlsr1))
 
 	// Verify that ns1svc2 is still referred by another route (HTTPRoute/ns1/r1)
 	require.Equal(t, true, cache.isServiceReferredByRoutes(ns1svc2))
