085e6fa5a56066472a52e8c07bc1aefa3ffca5a2
Daneyon Hansen <daneyonhansen@gmail.com>
2023-01-04 08:38:27 +0100

    Adds Logging API Type to EnvoyProxy (#842)
    * Adds EnvoyProxy Logging API Type
    
    Signed-off-by: danehans <daneyonhansen@gmail.com>

--- api/config/v1alpha1/envoyproxy_types.go
+++ api/config/v1alpha1/envoyproxy_types.go
@@ -29,6 +29,12 @@ type EnvoyProxySpec struct {
 	//
 	// +optional
 	Provider *ResourceProvider `json:"provider,omitempty"`
+
+	// Logging defines logging parameters for managed proxies. If unspecified,
+	// default settings apply.
+	//
+	// +kubebuilder:default={level: {system: info}}
+	Logging ProxyLogging `json:"logging,omitempty"`
 }
 
 // ResourceProvider defines the desired state of a resource provider.
@@ -73,6 +79,69 @@ type EnvoyDeployment struct {
 
 	// TODO: Expose config as use cases are better understood, e.g. labels.
 }
+
+// ProxyLogging defines logging parameters for managed proxies.
+type ProxyLogging struct {
+	// Level is a map of logging level per component, where the component is the key
+	// and the log level is the value. If unspecified, defaults to "System: Info".
+	//
+	// +kubebuilder:default={system: info}
+	Level map[LogComponent]LogLevel `json:"level,omitempty"`
+}
+
+// LogComponent defines a component that supports a configured logging level.
+//
+// +kubebuilder:validation:Enum=system;upstream;http;connection;admin;client;filter;main;router;runtime
+type LogComponent string
+
+const (
+	// LogComponentSystem defines the "system"-wide logging component. When specified,
+	// all other logging components are ignored.
+	LogComponentSystem LogComponent = "system"
+
+	// LogComponentUpstream defines defines the "upstream" logging component.
+	LogComponentUpstream LogComponent = "upstream"
+
+	// LogComponentHTTP defines defines the "http" logging component.
+	LogComponentHTTP LogComponent = "http"
+
+	// LogComponentConnection defines defines the "connection" logging component.
+	LogComponentConnection LogComponent = "connection"
+
+	// LogComponentAdmin defines defines the "admin" logging component.
+	LogComponentAdmin LogComponent = "admin"
+
+	// LogComponentClient defines defines the "client" logging component.
+	LogComponentClient LogComponent = "client"
+
+	// LogComponentFilter defines defines the "filter" logging component.
+	LogComponentFilter LogComponent = "filter"
+
+	// LogComponentMain defines defines the "main" logging component.
+	LogComponentMain LogComponent = "main"
+
+	// LogComponentRouter defines defines the "router" logging component.
+	LogComponentRouter LogComponent = "router"
+
+	// LogComponentRuntime defines defines the "runtime" logging component.
+	LogComponentRuntime LogComponent = "runtime"
+)
+
+// LogLevel defines a log level for system logs.
+//
+// +kubebuilder:validation:Enum=debug;info;error
+type LogLevel string
+
+const (
+	// LogLevelDebug defines the "debug" logging level.
+	LogLevelDebug LogLevel = "debug"
+
+	// LogLevelInfo defines the "Info" logging level.
+	LogLevelInfo LogLevel = "info"
+
+	// LogLevelError defines the "Error" logging level.
+	LogLevelError LogLevel = "error"
+)
 
 // EnvoyProxyStatus defines the observed state of EnvoyProxy
 type EnvoyProxyStatus struct {
