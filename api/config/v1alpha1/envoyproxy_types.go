24de02f27767b3b031365dd2e6c099821f9399a4
Arko Dasgupta <arko@tetrate.io>
2023-01-05 14:01:47 -0800

    rm EnvoyProxy comments around aux control plane
    Signed-off-by: Arko Dasgupta <arko@tetrate.io>

--- api/config/v1alpha1/envoyproxy_types.go
+++ api/config/v1alpha1/envoyproxy_types.go
@@ -45,13 +45,13 @@ type ResourceProvider struct {
 	// optional auxiliary control planes. Supported types are:
 	//
 	//   * Kubernetes: Provides infrastructure resources for running the data plane,
-	//                 e.g. Envoy proxy, and optional auxiliary control planes.
+	//                 e.g. Envoy proxy.
 	//
 	// +unionDiscriminator
 	Type ProviderType `json:"type"`
 	// Kubernetes defines the desired state of the Kubernetes resource provider.
 	// Kubernetes provides infrastructure resources for running the data plane,
-	// e.g. Envoy proxy, and optional auxiliary control planes. If unspecified
+	// e.g. Envoy proxy. If unspecified
 	// and type is "Kubernetes", default settings for managed Kubernetes resources
 	// are applied.
 	//
