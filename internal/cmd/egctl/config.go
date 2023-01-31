fc07fe14c712e74863e0148d1054be7408d885cb
hejianpeng <hejianpeng2@huawei.com>
2023-01-31 10:03:25 +0800

    egctl: `config all` command
    Signed-off-by: hejianpeng <hejianpeng2@huawei.com>

---
+++ internal/cmd/egctl/config.go
@@ -0,0 +1,158 @@
+// Copyright Envoy Gateway Authors
+// SPDX-License-Identifier: Apache-2.0
+// The full text of the Apache license is available in the LICENSE file at
+// the root of the repo.
+
+package egctl
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/spf13/cobra"
+	"k8s.io/apimachinery/pkg/types"
+	cmdutil "k8s.io/kubectl/pkg/cmd/util"
+	"sigs.k8s.io/yaml"
+
+	"github.com/envoyproxy/gateway/internal/cmd/options"
+	kube "github.com/envoyproxy/gateway/internal/kubernetes"
+)
+
+var (
+	output        string
+	podName       string
+	podNamespace  string
+	containerName string
+	adminPort     int32
+)
+
+func NewConfigCommand() *cobra.Command {
+	cfgCommand := &cobra.Command{
+		Use:     "config",
+		Aliases: []string{"c"},
+		Short:   "Retrieve proxy configuration.",
+		Long:    "Retrieve information about proxy configuration from envoy proxy and gateway.",
+	}
+
+	cfgCommand.AddCommand(proxyCommand())
+
+	flags := cfgCommand.Flags()
+	options.AddKubeConfigFlags(flags)
+
+	cfgCommand.PersistentFlags().StringVarP(&output, "output", "o", "json", "One of 'yaml' or 'json'")
+	cfgCommand.PersistentFlags().StringVar(&containerName, "container-name", "envoy", "Name of container where envoy proxy is installed.")
+	cfgCommand.PersistentFlags().Int32Var(&adminPort, "admin-port", 19000, "Admin port of envoy proxy.")
+	cfgCommand.PersistentFlags().StringVarP(&podNamespace, "namespace", "n", "envoy-gateway", "Namespace where envoy proxy pod are installed.")
+
+	return cfgCommand
+}
+
+func proxyCommand() *cobra.Command {
+	c := &cobra.Command{
+		Use:     "envoy-proxy",
+		Aliases: []string{"proxy"},
+		Long:    "Retrieve information from envoy proxy.",
+	}
+
+	c.AddCommand(allConfigCmd())
+
+	return c
+}
+
+func allConfigCmd() *cobra.Command {
+
+	allConfigCmd := &cobra.Command{
+		Use:   "all <pod-name>",
+		Short: "Retrieves all configuration for the Envoy in the specified pod",
+		Long:  `Retrieve information about all configuration for the Envoy instance in the specified pod.`,
+		Example: `  # Retrieve summary about all configuration for a given pod from Envoy.
+  egctl config envoy-proxy all <pod-name> -n <pod-namespace>
+
+  # Retrieve full cluster dump as YAML
+  egctl config envoy-proxy all <pod-name> -n <pod-namespace> -o yaml
+
+  # Retrieve full cluster dump with short syntax
+  egctl c proxy a <pod-name> -n <pod-namespace>
+`,
+		Aliases: []string{"a"},
+		Run: func(c *cobra.Command, args []string) {
+			cmdutil.CheckErr(runAllConfig(c, args))
+		},
+	}
+
+	return allConfigCmd
+}
+
+func runAllConfig(c *cobra.Command, args []string) error {
+	podName = args[0]
+
+	if podName == "" {
+		return fmt.Errorf("pod name is required")
+	}
+
+	if podNamespace == "" {
+		return fmt.Errorf("pod namespace is required")
+	}
+
+	out, err := extractConfigDump(types.NamespacedName{
+		Namespace: podNamespace,
+		Name:      podName,
+	})
+	if err != nil {
+		return err
+	}
+
+	if output == "yaml" {
+		out, err = yaml.JSONToYAML(out)
+		if err != nil {
+			return err
+		}
+	}
+	_, err = fmt.Fprintln(c.OutOrStdout(), string(out))
+	return err
+}
+
+func extractConfigDump(nn types.NamespacedName) ([]byte, error) {
+	c, err := kube.NewCLIClient(options.DefaultConfigFlags.ToRawKubeConfigLoader())
+	if err != nil {
+		return nil, fmt.Errorf("build CLI client fail: %w", err)
+	}
+
+	pod, err := c.Pod(nn)
+	if err != nil {
+		return nil, fmt.Errorf("get pod %s fail: %w", nn, err)
+	}
+	if pod.Status.Phase != "Running" {
+		return nil, fmt.Errorf("pod %s is not running", nn)
+	}
+
+	fw, err := kube.NewLocalPortForwarder(c, nn, 0, int(adminPort))
+	if err != nil {
+		return nil, err
+	}
+
+	if err := fw.Start(); err != nil {
+		return nil, err
+	}
+	defer fw.Stop()
+
+	return configDumpRequest(fw.Address())
+}
+
+func configDumpRequest(address string) ([]byte, error) {
+	req, err := http.NewRequest("GET", fmt.Sprintf("http://%s/config_dump", address), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		_ = resp.Body.Close()
+	}()
+
+	return io.ReadAll(resp.Body)
+}
---
+++ internal/kubernetes/port-forwarder.go
@@ -0,0 +1,146 @@
+// Copyright Envoy Gateway Authors
+// SPDX-License-Identifier: Apache-2.0
+// The full text of the Apache license is available in the LICENSE file at
+// the root of the repo.
+
+package kubernetes
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+	"os"
+
+	"github.com/pkg/errors"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/portforward"
+	"k8s.io/client-go/transport/spdy"
+)
+
+const (
+	defaultLocalAddress = "localhost"
+)
+
+type PortForwarder interface {
+	Start() error
+
+	Stop()
+
+	// Address returns the address of the local forwarded address.
+	Address() string
+}
+
+var _ PortForwarder = &localForwarder{}
+
+type localForwarder struct {
+	types.NamespacedName
+	CLIClient
+
+	localPort int
+	podPort   int
+
+	stopCh chan struct{}
+}
+
+func NewLocalPortForwarder(client CLIClient, namespacedName types.NamespacedName, localPort, podPort int) (PortForwarder, error) {
+	f := &localForwarder{
+		stopCh:         make(chan struct{}),
+		CLIClient:      client,
+		NamespacedName: namespacedName,
+		localPort:      localPort,
+		podPort:        podPort,
+	}
+	if f.localPort == 0 {
+		// get a random port
+		p, err := f.localAvailablePort()
+		if err != nil {
+			return nil, errors.Wrapf(err, "failed to get a local available port")
+		}
+		f.localPort = p
+	}
+
+	return f, nil
+}
+
+func (f *localForwarder) localAvailablePort() (int, error) {
+	l, err := net.Listen("tcp", fmt.Sprintf("%s:0", defaultLocalAddress))
+	if err != nil {
+		return 0, err
+	}
+
+	return l.Addr().(*net.TCPAddr).Port, l.Close()
+}
+
+func (f *localForwarder) Start() error {
+	errCh := make(chan error, 1)
+	readyCh := make(chan struct{}, 1)
+	go func() {
+		for {
+			select {
+			case <-f.stopCh:
+				return
+			default:
+			}
+
+			fw, err := f.buildKubernetesPortForwarder(readyCh)
+			if err != nil {
+				errCh <- err
+				return
+			}
+
+			if err := fw.ForwardPorts(); err != nil {
+				errCh <- err
+				return
+			}
+
+			readyCh = nil
+		}
+
+	}()
+
+	select {
+	case err := <-errCh:
+		return errors.Wrap(err, "failed to start port forwarder")
+	case <-readyCh:
+		return nil
+	}
+}
+
+func (f *localForwarder) buildKubernetesPortForwarder(readyCh chan struct{}) (*portforward.PortForwarder, error) {
+	restClient, err := rest.RESTClientFor(f.RESTConfig())
+	if err != nil {
+		return nil, err
+	}
+
+	req := restClient.Post().Resource("pods").Namespace(f.Namespace).Name(f.Name).SubResource("portforward")
+	serverURL := req.URL()
+
+	roundTripper, upgrader, err := spdy.RoundTripperFor(f.RESTConfig())
+	if err != nil {
+		return nil, fmt.Errorf("failure creating roundtripper: %v", err)
+	}
+
+	dialer := spdy.NewDialer(upgrader, &http.Client{Transport: roundTripper}, http.MethodPost, serverURL)
+	fw, err := portforward.NewOnAddresses(dialer,
+		[]string{defaultLocalAddress},
+		[]string{fmt.Sprintf("%d:%d", f.localPort, f.podPort)},
+		f.stopCh,
+		readyCh,
+		io.Discard,
+		os.Stderr)
+	if err != nil {
+		return nil, fmt.Errorf("failed establishing portforward: %v", err)
+	}
+
+	return fw, nil
+}
+
+func (f *localForwarder) Stop() {
+	close(f.stopCh)
+}
+
+func (f *localForwarder) Address() string {
+	return fmt.Sprintf("%s:%d", defaultLocalAddress, f.localPort)
+}
