d883c7afb8ad9838804fb8e5c100a98c90b027c2
HoangTheQuyen <HoangTheQuyen01@gmail.com>
2023-02-07 15:25:07 +0700

    config k8s provider CustomGRPCRoute

--- internal/provider/kubernetes/predicates.go
+++ internal/provider/kubernetes/predicates.go
@@ -132,6 +132,15 @@ func (r *gatewayAPIReconciler) validateServiceForReconcile(obj client.Object) bo
 		return false
 	}
 
+	customgrpcRouteList := &gwapiv1a2.CustomGRPCRouteList{}
+	if err := r.client.List(ctx, customgrpcRouteList, &client.ListOptions{
+		FieldSelector: fields.OneTermEqualSelector(serviceCustomGRPCRouteIndex, utils.NamespacedName(svc).String()),
+	}); err != nil {
+		r.log.Error(err, "unable to find associated CustomGRPCRoutes")
+		panic(1)
+		return false
+	}
+
 	tlsRouteList := &gwapiv1a2.TLSRouteList{}
 	if err := r.client.List(ctx, tlsRouteList, &client.ListOptions{
 		FieldSelector: fields.OneTermEqualSelector(serviceTLSRouteIndex, utils.NamespacedName(svc).String()),
@@ -159,6 +168,7 @@ func (r *gatewayAPIReconciler) validateServiceForReconcile(obj client.Object) bo
 	// Check how many Route objects refer this Service
 	allAssociatedRoutes := len(httpRouteList.Items) +
 		len(grpcRouteList.Items) +
+		len(customgrpcRouteList.Items) +
 		len(tlsRouteList.Items) +
 		len(tcpRouteList.Items) +
 		len(udpRouteList.Items)
