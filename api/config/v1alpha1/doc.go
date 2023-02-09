43a2e161e33e1d0ef91042d875794804645985bd
Daneyon Hansen <daneyonhansen@gmail.com>
2023-02-09 03:40:17 +0100

    Adds API Docs Tooling (#980)
    Signed-off-by: danehans <daneyonhansen@gmail.com>
    (cherry picked from commit d5a5b25933e1d486b04e307f2b4bb6239db0cd00)
    Signed-off-by: Arko Dasgupta <arko@tetrate.io>

---
+++ api/config/v1alpha1/doc.go
@@ -0,0 +1,11 @@
+// Copyright Envoy Gateway Authors
+// SPDX-License-Identifier: Apache-2.0
+// The full text of the Apache license is available in the LICENSE file at
+// the root of the repo.
+
+// Package v1alpha1 contains API Schema definitions for the config.gateway.envoyproxy.io
+// API group.
+//
+// +kubebuilder:object:generate=true
+// +groupName=config.gateway.envoyproxy.io
+package v1alpha1
