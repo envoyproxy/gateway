741960325123aca70eb68be30db31243dad983de
zirain <hejianpeng2@huawei.com>
2023-02-15 12:09:59 +0800

    egctl: add `version` command (#914)
    * egctl: initialization
    
    Signed-off-by: hejianpeng <hejianpeng2@huawei.com>
    
    ---------
    
    Signed-off-by: hejianpeng <hejianpeng2@huawei.com>

---
+++ cmd/egctl/main.go
@@ -0,0 +1,35 @@
+// Copyright Envoy Gateway Authors
+// SPDX-License-Identifier: Apache-2.0
+// The full text of the Apache license is available in the LICENSE file at
+// the root of the repo.
+
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+
+	"github.com/envoyproxy/gateway/internal/cmd/egctl"
+)
+
+func main() {
+	if err := rootCommand().Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func rootCommand() *cobra.Command {
+	rootCmd := &cobra.Command{
+		Use:               "egctl",
+		Long:              "A command line utility for operating Envoy Gateway",
+		SilenceUsage:      true,
+		DisableAutoGenTag: true,
+	}
+
+	rootCmd.AddCommand(egctl.NewVersionCommand())
+
+	return rootCmd
+}
