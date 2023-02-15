ac43fb91a77359d3efb7689511bf43c4ea489413
Bing Han <h.bing612@gmail.com>
2023-02-15 14:46:09 +0800

    refactor: change test to test cases
    Signed-off-by: Bing Han <h.bing612@gmail.com>

--- internal/cmd/server_test.go
+++ internal/cmd/server_test.go
@@ -12,23 +12,46 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var (
+	invalidGatewayConfig = `
+kind: EnvoyGateway
+apiVersion: config.gateway.envoyproxy.io/v1alpha1
+gateway: {}
+`
+)
+
 func TestGetServerCommand(t *testing.T) {
 	got := getServerCommand()
 	assert.Equal(t, "server", got.Use)
 }
 
 func TestGetConfigValidate(t *testing.T) {
-	file, err := os.CreateTemp("", "config")
-	assert.NoError(t, err)
-	defer os.Remove(file.Name())
+	tests := []struct {
+		name   string
+		input  string
+		errors []string
+	}{
+		{
+			name:   "invalid gateway",
+			input:  invalidGatewayConfig,
+			errors: []string{"is unspecified"},
+		},
+	}
+	for _, test := range tests {
+		test := test
+		t.Run(test.name, func(t *testing.T) {
+			file, err := os.CreateTemp("", "config")
+			assert.NoError(t, err)
+			defer os.Remove(file.Name())
 
-	_, err = file.Write([]byte(`
-kind: EnvoyGateway
-apiVersion: config.gateway.envoyproxy.io/v1alpha1
-gateway: {}
-`))
-	assert.NoError(t, err)
+			_, err = file.Write([]byte(test.input))
+			assert.NoError(t, err)
 
-	_, err = getConfigByPath(file.Name())
-	assert.Error(t, err)
+			_, err = getConfigByPath(file.Name())
+			for _, e := range test.errors {
+				assert.ErrorContains(t, err, e)
+			}
+		})
+	}
+
 }
