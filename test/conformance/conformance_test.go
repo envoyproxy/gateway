e831ac74c4f472485e1c7e3a2dc48ccc3d7aebd1
HoangTheQuyen <HoangTheQuyen01@gmail.com>
2023-02-17 05:39:00 +0700

    fix confomen

--- test/conformance/conformance_test.go
+++ test/conformance/conformance_test.go
@@ -65,6 +65,8 @@ func TestGatewayAPIConformance(t *testing.T) {
 			tests.GatewayObservedGenerationBump.ShortName,
 			// Remove once https://github.com/envoyproxy/gateway/issues/1016 is fixed
 			tests.HTTPRouteObservedGenerationBump.ShortName,
+
+			// Remove when know how to fix
 		},
 	})
 	cSuite.Setup(t)
