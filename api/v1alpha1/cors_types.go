343d4ae0cbe3423f34d1b7ff148aa5793537ea55
HoangTheQuyen <HoangTheQuyen01@gmail.com>
2023-02-19 16:57:40 +0700

    kubebuilder:object:root=true

--- api/v1alpha1/cors_types.go
+++ api/v1alpha1/cors_types.go
@@ -52,6 +52,7 @@ type StringMatch struct {
 	Regex  *string `json:"regex,omitempty"`
 }
 
+// +kubebuilder:object:root=true
 type CorsFilterList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
