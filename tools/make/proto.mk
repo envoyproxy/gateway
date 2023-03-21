b5bf8f883e68256f9ff541b478d93456e32112e5
AliceProxy <alicewasko@datawire.io>
2023-03-21 08:54:39 -0700

    create extension hooks protos
    Signed-off-by: AliceProxy <alicewasko@datawire.io>

--- tools/make/proto.mk
+++ tools/make/proto.mk
@@ -3,3 +3,7 @@
 .PHONY: protos
 protos: $(tools/buf) ## Compile all protobufs
 	$(tools/buf) generate
+
+.PHONY: buf-mod-update
+buf-mod-update: $(tools/buf) ## Update buf.lock for protobuf dependency updates
+	$(tools/buf) mod update
