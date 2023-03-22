datawire/envoy-gateway

api: add extension configuration to EnvoyGateway
Signed-off-by: Hamzah Qudsi <hqudsi@datawire.io>

api/config/v1alpha1/zz_generated.deepcopy.go
@@ -52,6 +52,17 @@ func (in *EnvoyGatewaySpec) DeepCopyInto(out *EnvoyGatewaySpec) {
 		*out = new(Provider)
 		(*in).DeepCopyInto(*out)
 	}
+	if in.Extensions != nil {
+		in, out := &in.Extensions, &out.Extensions
+		*out = make([]*Extension, len(*in))
+		for i := range *in {
+			if (*in)[i] != nil {
+				in, out := &(*in)[i], &(*out)[i]
+				*out = new(Extension)
+				(*in).DeepCopyInto(*out)
+			}
+		}
+	}
 }
 
 // DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new EnvoyGatewaySpec.
@@ -155,6 +166,76 @@ func (in *EnvoyProxyStatus) DeepCopy() *EnvoyProxyStatus {
 		return nil
 	}
 	out := new(EnvoyProxyStatus)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *Extension) DeepCopyInto(out *Extension) {
+	*out = *in
+	if in.APIGroups != nil {
+		in, out := &in.APIGroups, &out.APIGroups
+		*out = make([]string, len(*in))
+		copy(*out, *in)
+	}
+	if in.Service != nil {
+		in, out := &in.Service, &out.Service
+		*out = new(ExtensionService)
+		(*in).DeepCopyInto(*out)
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new Extension.
+func (in *Extension) DeepCopy() *Extension {
+	if in == nil {
+		return nil
+	}
+	out := new(Extension)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *ExtensionService) DeepCopyInto(out *ExtensionService) {
+	*out = *in
+	if in.TLS != nil {
+		in, out := &in.TLS, &out.TLS
+		*out = new(ExtensionTLS)
+		(*in).DeepCopyInto(*out)
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new ExtensionService.
+func (in *ExtensionService) DeepCopy() *ExtensionService {
+	if in == nil {
+		return nil
+	}
+	out := new(ExtensionService)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *ExtensionTLS) DeepCopyInto(out *ExtensionTLS) {
+	*out = *in
+	if in.Secret != nil {
+		in, out := &in.Secret, &out.Secret
+		*out = new(TLSSecret)
+		**out = **in
+	}
+	if in.File != nil {
+		in, out := &in.File, &out.File
+		*out = new(TLSFile)
+		**out = **in
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new ExtensionTLS.
+func (in *ExtensionTLS) DeepCopy() *ExtensionTLS {
+	if in == nil {
+		return nil
+	}
+	out := new(ExtensionTLS)
 	in.DeepCopyInto(out)
 	return out
 }
@@ -307,6 +388,36 @@ func (in *ResourceProvider) DeepCopy() *ResourceProvider {
 		return nil
 	}
 	out := new(ResourceProvider)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *TLSFile) DeepCopyInto(out *TLSFile) {
+	*out = *in
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new TLSFile.
+func (in *TLSFile) DeepCopy() *TLSFile {
+	if in == nil {
+		return nil
+	}
+	out := new(TLSFile)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *TLSSecret) DeepCopyInto(out *TLSSecret) {
+	*out = *in
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new TLSSecret.
+func (in *TLSSecret) DeepCopy() *TLSSecret {
+	if in == nil {
+		return nil
+	}
+	out := new(TLSSecret)
 	in.DeepCopyInto(out)
 	return out
 }

envoyproxy/gateway

api/config/v1alpha1/zz_generated.deepcopy.go
@@ -57,6 +57,11 @@ func (in *EnvoyGatewaySpec) DeepCopyInto(out *EnvoyGatewaySpec) {
 		*out = new(RateLimit)
 		(*in).DeepCopyInto(*out)
 	}
+	if in.Extension != nil {
+		in, out := &in.Extension, &out.Extension
+		*out = new(Extension)
+		(*in).DeepCopyInto(*out)
+	}
 }
 
 // DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new EnvoyGatewaySpec.
@@ -170,6 +175,82 @@ func (in *EnvoyProxyStatus) DeepCopy() *EnvoyProxyStatus {
 }
 
 // DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *Extension) DeepCopyInto(out *Extension) {
+	*out = *in
+	if in.Resources != nil {
+		in, out := &in.Resources, &out.Resources
+		*out = make([]*GroupVersionKind, len(*in))
+		for i := range *in {
+			if (*in)[i] != nil {
+				in, out := &(*in)[i], &(*out)[i]
+				*out = new(GroupVersionKind)
+				**out = **in
+			}
+		}
+	}
+	if in.Service != nil {
+		in, out := &in.Service, &out.Service
+		*out = new(ExtensionService)
+		(*in).DeepCopyInto(*out)
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new Extension.
+func (in *Extension) DeepCopy() *Extension {
+	if in == nil {
+		return nil
+	}
+	out := new(Extension)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *ExtensionService) DeepCopyInto(out *ExtensionService) {
+	*out = *in
+	if in.TLS != nil {
+		in, out := &in.TLS, &out.TLS
+		*out = new(ExtensionTLS)
+		(*in).DeepCopyInto(*out)
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new ExtensionService.
+func (in *ExtensionService) DeepCopy() *ExtensionService {
+	if in == nil {
+		return nil
+	}
+	out := new(ExtensionService)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *ExtensionTLS) DeepCopyInto(out *ExtensionTLS) {
+	*out = *in
+	if in.Secret != nil {
+		in, out := &in.Secret, &out.Secret
+		*out = new(TLSSecret)
+		**out = **in
+	}
+	if in.File != nil {
+		in, out := &in.File, &out.File
+		*out = new(TLSFile)
+		**out = **in
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new ExtensionTLS.
+func (in *ExtensionTLS) DeepCopy() *ExtensionTLS {
+	if in == nil {
+		return nil
+	}
+	out := new(ExtensionTLS)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
 func (in *FileProvider) DeepCopyInto(out *FileProvider) {
 	*out = *in
 }
@@ -195,6 +276,21 @@ func (in *Gateway) DeepCopy() *Gateway {
 		return nil
 	}
 	out := new(Gateway)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *GroupVersionKind) DeepCopyInto(out *GroupVersionKind) {
+	*out = *in
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new GroupVersionKind.
+func (in *GroupVersionKind) DeepCopy() *GroupVersionKind {
+	if in == nil {
+		return nil
+	}
+	out := new(GroupVersionKind)
 	in.DeepCopyInto(out)
 	return out
 }
@@ -402,6 +498,36 @@ func (in *ResourceProvider) DeepCopy() *ResourceProvider {
 		return nil
 	}
 	out := new(ResourceProvider)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *TLSFile) DeepCopyInto(out *TLSFile) {
+	*out = *in
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new TLSFile.
+func (in *TLSFile) DeepCopy() *TLSFile {
+	if in == nil {
+		return nil
+	}
+	out := new(TLSFile)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *TLSSecret) DeepCopyInto(out *TLSSecret) {
+	*out = *in
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new TLSSecret.
+func (in *TLSSecret) DeepCopy() *TLSSecret {
+	if in == nil {
+		return nil
+	}
+	out := new(TLSSecret)
 	in.DeepCopyInto(out)
 	return out
 }