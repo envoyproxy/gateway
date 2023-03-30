1b3e89677a6866ee5c6b1325e094b4f87c30b13d
Karol Szwaj <karol.szwaj@gmail.com>
2023-03-30 17:11:03 +0200

    gateway: set up fullpath log format
    Signed-off-by: Karol Szwaj <karol.szwaj@gmail.com>

--- internal/log/log.go
+++ internal/log/log.go
@@ -9,12 +9,36 @@ import (
 	"github.com/go-logr/logr"
 	"github.com/go-logr/zapr"
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 func NewLogger() (logr.Logger, error) {
-	zapLogger, err := zap.NewDevelopment()
+	zapConfig := zap.Config{
+		Level:       zap.NewAtomicLevelAt(zap.DebugLevel),
+		Development: true,
+		Encoding:    "console",
+		EncoderConfig: zapcore.EncoderConfig{
+			TimeKey:        "T",
+			LevelKey:       "L",
+			NameKey:        "N",
+			CallerKey:      "C",
+			FunctionKey:    zapcore.OmitKey,
+			MessageKey:     "M",
+			StacktraceKey:  "S",
+			LineEnding:     zapcore.DefaultLineEnding,
+			EncodeLevel:    zapcore.CapitalLevelEncoder,
+			EncodeTime:     zapcore.ISO8601TimeEncoder,
+			EncodeDuration: zapcore.StringDurationEncoder,
+			EncodeCaller:   zapcore.FullCallerEncoder,
+		},
+		OutputPaths:      []string{"stderr"},
+		ErrorOutputPaths: []string{"stderr"},
+	}
+
+	zapLogger, err := zapConfig.Build()
 	if err != nil {
 		return logr.Logger{}, err
 	}
+
 	return zapr.NewLogger(zapLogger), nil
 }
