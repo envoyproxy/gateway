f633ffa109f6011da9231b396a7daadd2019afd5
hejianpeng <hejianpeng2@huawei.com>
2023-04-01 11:49:47 +0800

    skip lint
    Signed-off-by: hejianpeng <hejianpeng2@huawei.com>

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -1,10 +1,10 @@
-//go:build e2e
-// +build e2e
-
 // Copyright Envoy Gateway Authors
 // SPDX-License-Identifier: Apache-2.0
 // The full text of the Apache license is available in the LICENSE file at
 // the root of the repo.
+
+//go:build e2e
+// +build e2e
 
 package e2e
 
