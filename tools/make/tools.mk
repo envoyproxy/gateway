f61a136152c1fce313d9629881a6e2d8d5dfa5fc
AliceProxy <alicewasko@datawire.io>
2023-04-04 13:52:27 -0700

    tools/buf: use separate go.mod and store config yaml in srcdir
    Signed-off-by: AliceProxy <alicewasko@datawire.io>

--- tools/make/tools.mk
+++ tools/make/tools.mk
@@ -24,12 +24,6 @@ tools/protoc-gen-go-grpc = $(tools.bindir)/protoc-gen-go-grpc
 $(tools.bindir)/%: $(tools.srcdir)/%/pin.go $(tools.srcdir)/%/go.mod
 	cd $(<D) && GOOS= GOARCH= go build -o $(abspath $@) $$(sed -En 's,^import "(.*)".*,\1,p' pin.go)
 
-# Let these use the main Envoy Gateway go.mod instead of having their own go.mod to ensure runtime
-# consistency
-tools.main-gomod += $(tools/protoc-gen-go)
-tools.main-gomod += $(tools/protoc-gen-go-grpc)
-$(tools.main-gomod): $(tools.bindir)/%: $(tools.srcdir)/%/pin.go $(ROOT_DIR)/go.mod
-	cd $(<D) && GOOS= GOARCH= go build -o $(abspath $@) $$(sed -En 's,^import "(.*)".*,\1,p' pin.go)
 
 # `pip install`-able things
 # =========================
---
+++ tools/src/protoc-gen-go-grpc/go.mod
@@ -0,0 +1,7 @@
+module github.com/envoyproxy/gateway/tools/src/protoc-gen-go-grpc
+
+go 1.20
+
+require google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.3.0
+
+require google.golang.org/protobuf v1.28.1 // indirect
