72ea081af2151a018165cba6aae170c30dda589d
Shubham Chauhan <shubham@tetrate.io>
2023-04-04 14:12:53 +0530

    templatize values.yaml to allow passing custom image repo and tag (#1248)
    * templatize values.yaml to allow passing custom tags
    
    Signed-off-by: Shubham Chauhan <shubham@tetrate.io>
    
    * header
    
    Signed-off-by: Shubham Chauhan <shubham@tetrate.io>
    
    * lint
    
    Signed-off-by: Shubham Chauhan <shubham@tetrate.io>
    
    * helm-gen in gen
    
    Signed-off-by: Shubham Chauhan <shubham@tetrate.io>
    
    * gen in kube-deploy
    
    Signed-off-by: Shubham Chauhan <shubham@tetrate.io>
    
    * review comments
    
    Signed-off-by: Shubham Chauhan <shubham@tetrate.io>
    
    * review comment update var name
    
    Signed-off-by: Shubham Chauhan <shubham@tetrate.io>
    
    ---------
    
    Signed-off-by: Shubham Chauhan <shubham@tetrate.io>

--- tools/make/kube.mk
+++ tools/make/kube.mk
@@ -56,9 +56,9 @@ endif
 IMAGE_PULL_POLICY ?= Always
 
 .PHONY: kube-deploy
-kube-deploy: manifests ## Install Envoy Gateway into the Kubernetes cluster specified in ~/.kube/config.
+kube-deploy: manifests helm-generate ## Install Envoy Gateway into the Kubernetes cluster specified in ~/.kube/config.
 	@$(LOG_TARGET)
-	helm install eg charts/gateway-helm --set deployment.envoyGateway.image.repository=$(IMAGE) --set deployment.envoyGateway.image.tag=$(TAG) --set deployment.envoyGateway.imagePullPolicy=$(IMAGE_PULL_POLICY) -n envoy-gateway-system --create-namespace
+	helm install eg charts/gateway-helm --set deployment.envoyGateway.imagePullPolicy=$(IMAGE_PULL_POLICY) -n envoy-gateway-system --create-namespace
 
 .PHONY: kube-undeploy
 kube-undeploy: manifests ## Uninstall the Envoy Gateway into the Kubernetes cluster specified in ~/.kube/config.
