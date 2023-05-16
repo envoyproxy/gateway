b2d10fad3c8cfe69f8852407af531a38b0360db6
huabing zhao <zhaohuabing@gmail.com>
2023-05-16 11:14:20 +0800

    add restrations to API
    Signed-off-by: huabing zhao <zhaohuabing@gmail.com>

--- api/v1alpha1/ratelimitfilter_types.go
+++ api/v1alpha1/ratelimitfilter_types.go
@@ -120,11 +120,15 @@ const (
 )
 
 type SourceMatch struct {
+	// +optional
+	// +kubebuilder:default=Exact
 	Type *SourceMatchType `json:"type,omitempty"`
 
 	// Value is the IP CIDR that represents the range of Source IP Addresses of the client.
 	// These could also be the intermediate addresses through which the request has flown through and is part of the  `X-Forwarded-For` header.
 	// For example, `192.168.0.1/32`, `192.168.0.0/24`, `001:db8::/64`.
+	// +kubebuilder:validation:MinLength=1
+	// +kubebuilder:validation:MaxLength=256
 	Value string `json:"address"`
 }
 
