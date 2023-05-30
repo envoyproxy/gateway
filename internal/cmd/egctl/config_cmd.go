1eee5d87d78ffa4369a2ba57b18690ab826c5ca9
Xunzhuo <bitliu@tencent.com>
2023-05-30 11:09:33 +0800

    feat: add namespaced all proxies config dump support (#1082)
    feat: support namespaced proxies config dump
    
    Signed-off-by: bitliu <bitliu@tencent.com>

--- internal/cmd/egctl/config_cmd.go
+++ internal/cmd/egctl/config_cmd.go
@@ -30,6 +30,7 @@ func newConfigCommand() *cobra.Command {
 	cfgCommand.PersistentFlags().StringVarP(&output, "output", "o", "json", "One of 'yaml' or 'json'")
 	cfgCommand.PersistentFlags().StringVarP(&podNamespace, "namespace", "n", "envoy-gateway-system", "Namespace where envoy proxy pod are installed.")
 	cfgCommand.PersistentFlags().StringArrayVarP(&labelSelectors, "labels", "l", nil, "Labels to select the envoy proxy pod.")
+	cfgCommand.PersistentFlags().BoolVarP(&allNamespaces, "all-namespaces", "A", false, "List all envoy proxy pods from all namespaces.")
 
 	return cfgCommand
 }
