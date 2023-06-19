f2700287c246b1ee865d78d259b3a01e20083162
qicz <qiczzhu@gmail.com>
2023-06-19 19:17:59 +0800

    fix: fixes update k8s resource bug.
    Signed-off-by: qicz <qiczzhu@gmail.com>

--- internal/infrastructure/kubernetes/infra_client.go
+++ internal/infrastructure/kubernetes/infra_client.go
@@ -7,8 +7,10 @@ package kubernetes
 
 import (
 	"context"
+	"fmt"
 
 	kerrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/client-go/util/retry"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -23,26 +25,27 @@ func New(cli client.Client) *InfraClient {
 }
 
 func (cli *InfraClient) CreateOrUpdate(ctx context.Context, key client.ObjectKey, current client.Object, specific client.Object, updateChecker func() bool) error {
-	if err := cli.Client.Get(ctx, key, current); err != nil {
-		if kerrors.IsNotFound(err) {
-			// Create if it does not exist.
-			if err := cli.Client.Create(ctx, specific); err != nil {
-				return err
+	return retry.RetryOnConflict(retry.DefaultBackoff, func() error {
+		if err := cli.Client.Get(ctx, key, current); err != nil {
+			if kerrors.IsNotFound(err) {
+				// Create if it does not exist.
+				if err := cli.Client.Create(ctx, specific); err != nil {
+					return fmt.Errorf("for Create %v", err)
+				}
+			}
+		} else {
+			// Since the client.Object does not have a specific Spec field to compare
+			// just perform an update for now.
+			if updateChecker() {
+				specific.SetUID(current.GetUID())
+				if err := cli.Client.Update(ctx, specific); err != nil {
+					return fmt.Errorf("for Update %v", err)
+				}
 			}
 		}
-	} else {
-		// Since the client.Object does not have a specific Spec field to compare
-		// just perform an update for now.
-		if updateChecker() {
-			specific.SetResourceVersion(current.GetResourceVersion())
-			specific.SetUID(current.GetUID())
-			if err := cli.Client.Update(ctx, specific); err != nil {
-				return err
-			}
-		}
-	}
 
-	return nil
+		return nil
+	})
 }
 
 func (cli *InfraClient) Delete(ctx context.Context, object client.Object) error {
