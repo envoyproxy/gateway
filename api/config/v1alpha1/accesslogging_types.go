e260ab549bdf984e4e6d8114105769413451cc8f
hejianpeng <hejianpeng2@huawei.com>
2023-06-20 14:45:55 +0800

    accesslog: always send k8s.namespace.name and k8s.pod.name
    Signed-off-by: hejianpeng <hejianpeng2@huawei.com>

--- api/config/v1alpha1/accesslogging_types.go
+++ api/config/v1alpha1/accesslogging_types.go
@@ -60,6 +60,7 @@ const (
 	// ProxyAccessLogSinkTypeFile defines the file accesslog sink.
 	ProxyAccessLogSinkTypeFile ProxyAccessLogSinkType = "File"
 	// ProxyAccessLogSinkTypeOpenTelemetry defines the OpenTelemetry accesslog sink.
+	// EnvoyGateway always sends `k8s.namespace.name` and `k8s.pod.name` as atrributes.
 	ProxyAccessLogSinkTypeOpenTelemetry ProxyAccessLogSinkType = "OpenTelemetry"
 )
 
