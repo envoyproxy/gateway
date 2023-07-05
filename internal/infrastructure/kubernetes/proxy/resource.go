97d8920c7a44ec06d5d2ea73cf24738adc7e1c1d
quyenhoang96 <quyen.hoang@geocomply.com>
2023-07-05 10:34:05 +0700

    change init delay time to 180s

--- internal/infrastructure/kubernetes/proxy/resource.go
+++ internal/infrastructure/kubernetes/proxy/resource.go
@@ -162,7 +162,7 @@ func expectedProxyContainers(infra *ir.ProxyInfra, deploymentConfig *egcfgv1a1.K
 						Port: intstr.IntOrString{Type: intstr.Int, IntVal: bootstrap.EnvoyReadinessPort},
 					},
 				},
-				InitialDelaySeconds: 50,
+				InitialDelaySeconds: 180,
 				SuccessThreshold:    3,
 			},
 		},
