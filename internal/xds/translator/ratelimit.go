7d2901de98cb019e8638a725eeb046b4da388b62
quyenhoang96 <quyen.hoang@geocomply.com>
2023-07-06 15:17:02 +0700

    Revert "change config filter ratelimit"
    This reverts commit 17788157b54a2a88bbe0d3e9e7cb904a1292290b.

--- internal/xds/translator/ratelimit.go
+++ internal/xds/translator/ratelimit.go
@@ -74,10 +74,7 @@ func (t *Translator) isRateLimitPresent(irListener *ir.HTTPListener) bool {
 
 func buildRateLimitFilter(irListener *ir.HTTPListener) *hcmv3.HttpFilter {
 	rateLimitFilterProto := &ratelimitfilterv3.RateLimit{
-		Domain:                         getRateLimitDomain(irListener),
-		RequestType:                    "external",
-		FailureModeDeny:                true,
-		RateLimitedAsResourceExhausted: true,
+		Domain: getRateLimitDomain(irListener),
 		RateLimitService: &ratelimitv3.RateLimitServiceConfig{
 			GrpcService: &corev3.GrpcService{
 				TargetSpecifier: &corev3.GrpcService_EnvoyGrpc_{
