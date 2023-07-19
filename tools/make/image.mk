bef2faaf5f0f212034d5784e2370319d885a77d1
quyenhoang96 <quyen.hoang@geocomply.com>
2023-07-19 18:55:13 +0700

    config env for release by version

--- tools/make/image.mk
+++ tools/make/image.mk
@@ -23,6 +23,12 @@ ifeq (${IMAGES},)
   $(error Could not determine IMAGES, set ROOT_DIR or run in source dir)
 endif
 
+.PHONY: image.release
+image.release: ## Build docker images for release. See Option PLATFORM and BINS.
+	docker pull ${REGISTRY}/gateway-dev:${SHA_SHORT}
+	docker tag ${REGISTRY}/gateway-dev:${SHA_SHORT} ${REGISTRY}/gateway-dev:${RELEASE_TAG}
+	docker push ${REGISTRY}/gateway-dev:${RELEASE_TAG}
+
 .PHONY: image.verify
 image.verify:
 	@$(LOG_TARGET)
