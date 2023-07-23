d3e0e95fbcfb0c3cabb099b767feb91389111ce2
Ronnie Personal <76408835+Ronnie-personal@users.noreply.github.com>
2023-07-22 23:00:34 -0400

    add more test cases
    Signed-off-by: Ronnie Personal <76408835+Ronnie-personal@users.noreply.github.com>

--- internal/xds/translator/translator_test.go
+++ internal/xds/translator/translator_test.go
@@ -201,24 +201,30 @@ func TestTranslateXdsNegative(t *testing.T) {
 		name           string
 		dnsDomain      string
 		requireSecrets bool
-	}{
+	}{ /*
+			{
+				name: "http-route-invalid",
+			},
+			{
+				name: "tcp-route-invalid",
+			},
+			{
+				name: "udp-route-invalid",
+			},
+			{
+				name: "jsonpatch-invalid",
+			},
+			{
+				name: "accesslog-invalid",
+			},
+			{
+				name: "tracing-invalid",
+			}, */
 		{
-			name: "http-route-invalid",
+			name: "jsonpatch-invalid-listener",
 		},
 		{
-			name: "tcp-route-invalid",
-		},
-		{
-			name: "udp-route-invalid",
-		},
-		{
-			name: "jsonpatch-invalid",
-		},
-		{
-			name: "accesslog-invalid",
-		},
-		{
-			name: "tracing-invalid",
+			name: "jsonpatch-invalid-cluster",
 		},
 	}
 
