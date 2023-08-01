2c71ee582c60e952124d33d64a3c2b5e1f37f476
Arko Dasgupta <arko@tetrate.io>
2023-08-01 13:48:18 -0700

    Add E2E for EnvoyPatchPolicy
    * Use LocalReplyConfig to return a custom
    status code `418` when there is no valid route match
    
    Signed-off-by: Arko Dasgupta <arko@tetrate.io>

---
+++ test/e2e/tests/envoy-patch-policy.go
@@ -0,0 +1,49 @@
+// Copyright Envoy Gateway Authors
+// SPDX-License-Identifier: Apache-2.0
+// The full text of the Apache license is available in the LICENSE file at
+// the root of the repo.
+
+//go:build e2e
+// +build e2e
+
+package tests
+
+import (
+	"testing"
+
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/gateway-api/conformance/utils/http"
+	"sigs.k8s.io/gateway-api/conformance/utils/kubernetes"
+	"sigs.k8s.io/gateway-api/conformance/utils/roundtripper"
+	"sigs.k8s.io/gateway-api/conformance/utils/suite"
+)
+
+func init() {
+	ConformanceTests = append(ConformanceTests, EnvoyPatchPolicyTest)
+}
+
+var EnvoyPatchPolicyTest = suite.ConformanceTest{
+	ShortName:   "EnvoyPatchPolicy",
+	Description: "update xds using EnvoyPatchPolicy",
+	Manifests:   []string{"testdata/envoy-patch-policy.yaml"},
+	Test: func(t *testing.T, suite *suite.ConformanceTestSuite) {
+		t.Run("envoy patch policy", func(t *testing.T) {
+			ns := "gateway-conformance-infra"
+			routeNN := types.NamespacedName{Name: "http-envoy-patch-policy", Namespace: ns}
+			gwNN := types.NamespacedName{Name: "same-namespace", Namespace: ns}
+			gwAddr := kubernetes.GatewayAndHTTPRoutesMustBeAccepted(t, suite.Client, suite.TimeoutConfig, suite.ControllerName, kubernetes.NewGatewayRef(gwNN), routeNN)
+			customResp := http.ExpectedResponse{
+				Request: http.Request{
+					Path: "/bar",
+				},
+				Response: http.Response{
+					StatusCode: 418,
+				},
+				Namespace: ns,
+			}
+
+			// Send a request to an invalid path and expect a custom response
+			http.MakeRequestAndExpectEventuallyConsistentResponse(t, suite.RoundTripper, suite.TimeoutConfig, gwAddr, customResp)
+		})
+	},
+}
