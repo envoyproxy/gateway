ab57bee405d57d92316070d112b15bed6f16048d
sh2 <shawnhxh@outlook.com>
2023-08-10 20:57:18 +0800

    change hash sum and unify ths format of nsName
    Signed-off-by: sh2 <shawnhxh@outlook.com>

--- internal/provider/utils/utils.go
+++ internal/provider/utils/utils.go
@@ -22,12 +22,13 @@ func NamespacedName(obj client.Object) types.NamespacedName {
 	}
 }
 
-// GetHashedName returns a partially hashed name for the string including up to 48 characters of the original name before the hash
+// GetHashedName returns a partially hashed name for the string including up to 48 characters of the original name before the hash.
+// Input `nsName` should be formatted as `{Namespace}/{ResourceName}`.
 func GetHashedName(nsName string) string {
 
 	h := sha256.New() // Using sha256 instead of sha1 due to Blocklisted import crypto/sha1: weak cryptographic primitive (gosec)
-	hSum := h.Sum([]byte(nsName))
-	hashedName := strings.ToLower(fmt.Sprintf("%x", hSum))
+	h.Write([]byte(nsName))
+	hashedName := strings.ToLower(fmt.Sprintf("%x", h.Sum(nil)))
 
 	// replace `/` with `-` to create a valid K8s resource name
 	resourceName := strings.ReplaceAll(nsName, "/", "-")
