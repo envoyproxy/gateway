482e13a46d1c130c77e517661148b35731daa09b
zirain <zirain2009@gmail.com>
2023-10-29 11:16:38 +0800

    update
    Signed-off-by: zirain <zirain2009@gmail.com>

--- test/e2e/tests/prometheus.go
+++ test/e2e/tests/prometheus.go
@@ -13,6 +13,7 @@ import (
 	"testing"
 	"time"
 
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/wait"
 	"sigs.k8s.io/gateway-api/conformance/utils/suite"
@@ -23,17 +24,44 @@ func init() {
 }
 
 var PrometheusTest = suite.ConformanceTest{
-	ShortName:   "Prometheus",
-	Description: "Make sure Prometheus endpoint is working",
+	ShortName:   "ControlPlane",
+	Description: "Make sure control plane prometheus endpoint is working",
 	Manifests:   []string{"testdata/prometheus.yaml"},
 	Test: func(t *testing.T, suite *suite.ConformanceTestSuite) {
-		t.Run("MetricExists", func(t *testing.T) {
+		t.Run("Prometheus", func(t *testing.T) {
+			nn := types.NamespacedName{Name: "envoy-gateway-lb", Namespace: "envoy-gateway-system"}
 			if err := wait.PollUntilContextTimeout(context.TODO(), time.Second, time.Minute, true,
 				func(_ context.Context) (done bool, err error) {
-					if err := ScrapeMetrics(t, suite.Client, types.NamespacedName{
-						Namespace: "envoy-gateway-system",
-						Name:      "envoy-gateway-lb",
-					}, "/metrics"); err != nil {
+					svc := corev1.Service{}
+					if err := suite.Client.Get(context.Background(), nn, &svc); err != nil {
+						return false, nil
+					}
+
+					host := ""
+					switch svc.Spec.Type {
+					case corev1.ServiceTypeLoadBalancer:
+						for _, ing := range svc.Status.LoadBalancer.Ingress {
+							if ing.IP != "" {
+								host = ing.IP
+								break
+							}
+						}
+					default:
+						// do nothing
+					}
+
+					if host == "" {
+						return false, nil
+					}
+
+					return true, nil
+				}); err != nil {
+				t.Errorf("failed to get service %s : %v", nn.String(), err)
+			}
+
+			if err := wait.PollUntilContextTimeout(context.TODO(), time.Second, time.Minute, true,
+				func(_ context.Context) (done bool, err error) {
+					if err := ScrapeMetrics(t, suite.Client, nn, 19001, "/metrics"); err != nil {
 						t.Logf("failed to get metric: %v", err)
 						return false, nil
 					}
