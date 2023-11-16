3917f071fead519da7631a32f4322f29fcd58aeb
huabing zhao <zhaohuabing@gmail.com>
2023-11-15 18:49:22 -0800

    put oauth2 filter before jwtauthFilter, so we can use jwtauthfilter to check the oauth2 tokens
    Signed-off-by: huabing zhao <zhaohuabing@gmail.com>

--- internal/xds/translator/httpfilters.go
+++ internal/xds/translator/httpfilters.go
@@ -41,11 +41,11 @@ func newOrderedHTTPFilter(filter *hcmv3.HttpFilter) *OrderedHTTPFilter {
 	switch {
 	case filter.Name == wellknown.CORS:
 		order = 1
-	case filter.Name == jwtAuthnFilter:
-		order = 2
-	case filter.Name == wellknown.HTTPRateLimit:
-		order = 3
 	case isOAuth2Filter(filter):
+		order = 2
+	case filter.Name == jwtAuthnFilter:
+		order = 3
+	case filter.Name == wellknown.HTTPRateLimit:
 		order = 4
 	case filter.Name == wellknown.Router:
 		order = 100
