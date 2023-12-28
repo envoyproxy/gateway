38a3399ac20f68c7b474e308fd9178b7d562497a
Huabing Zhao <zhaohuabing@gmail.com>
2023-12-28 15:16:55 +0800

    fix: perfix match (#2369)
    Signed-off-by: huabing zhao <zhaohuabing@gmail.com>

--- internal/gatewayapi/backendtrafficpolicy.go
+++ internal/gatewayapi/backendtrafficpolicy.go
@@ -263,7 +263,7 @@ func (t *Translator) translateBackendTrafficPolicyForRoute(policy *egv1a1.Backen
 	}
 
 	// Apply IR to all relevant routes
-	prefix := irRoutePrefix(route)
+	prefix := irRoutePrefix(route) + "/" // Prevent mismatching routes with the same prefix
 	for _, ir := range xdsIR {
 		for _, http := range ir.HTTP {
 			for _, r := range http.Routes {
