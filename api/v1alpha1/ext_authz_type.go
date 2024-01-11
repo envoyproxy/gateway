783adc1b37010c854f8196bc32a9ebe787965a91
huabing zhao <zhaohuabing@gmail.com>
2024-01-11 20:33:58 +0800

    some API change
    Signed-off-by: huabing zhao <zhaohuabing@gmail.com>

--- api/v1alpha1/ext_authz_type.go
+++ api/v1alpha1/ext_authz_type.go
@@ -36,6 +36,20 @@ type ExtAuth struct {
 	// HTTPService defines the HTTP External Authorization service
 	// Only one of GRPCService or HTTPService may be specified.
 	HTTPService *HTTPExtAuthService `json:"httpService,omitempty" yaml:"httpService"`
+
+	// AllowWhenFailed indicates whether the request should be allowed when the
+	// authorization service has failed or has returned an HTTP 5xx error.
+	// When set to true, the request will be allowed.
+	// When set to false, the request will be denied with an HTTP 403 Forbidden error.
+	// Defaults to false.
+	AllowWhenFailed bool `json:"allowWhenFailed,omitempty" yaml:"allowWhenFailed"`
+
+	// AllowedHeaders defines the client request headers that will be included
+	// in the request to the external authorization service.
+	// Note: If not specified, all headers will be included in the check request
+	// to a gRPC authorization server, whereas no headers will be included in the
+	// check request to an HTTP authorization server.
+	AllowedHeaders []string `json:"allowedHeaders,omitempty" yaml:"allowedHeaders,omitempty"`
 }
 
 // GRPCExtAuthService defines the gRPC External Authorization service
@@ -45,22 +59,33 @@ type GRPCExtAuthService struct {
 
 	// Port is the network port of the gRPC External Authorization service
 	Port gwapiv1a2.PortNumber `json:"port"`
+
+	// TLS defines the TLS configuration for the gRPC External Authorization service.
+	// Note: If not specified, the proxy will talk to the gRPC External
+	// Authorization service in plaintext.
+	TLS *TLSConfig `json:"tlsSettings,omitempty" yaml:"tlsSettings"`
 }
 
 // HTTPExtAuthService defines the HTTP External Authorization service
 type HTTPExtAuthService struct {
 	// URL is the URL of the HTTP External Authorization service.
-	// The URL must be a fully qualified URL with a scheme, hostname, and optional port and path. Parameters are not allowed.
+	// The URL must be a fully qualified URL with a scheme, hostname,
+	// and optional port and path. Parameters are not allowed.
 	// The URL must use either the http or https scheme.
 	// If port is not specified, 80 for http and 443 for https are assumed.
 	// If path is specified, the authorization request will be sent to the path.
 	URl string `json:"url" yaml:"url"`
 
 	// TLS defines the TLS configuration for the HTTP External Authorization service.
+	// TLS is only valid when the URL scheme is https. If the URL scheme is
+	// https, and TLS is not specified, the proxy will use the system default
+	// certificate pool to verify the server certificate.
 	TLS *TLSConfig `json:"tlsSettings,omitempty" yaml:"tlsSettings"`
 
-	// AuthorizationRequest defines how the authorization response impacts the client request and response.
-	AuthorizationResponse *AuthorizationResponse `json:"authorizationResponse,omitempty" yaml:"authorizationResponse"`
+	// Authorization response headers that will be added to the original client request
+	// before sending it to the upstream server.
+	// Note that coexistent headers will be overridden.
+	AllowedUpstreamHeaders []string `json:"allowedUpstreamHeaders,omitempty"`
 }
 
 // TLSConfig describes a TLS configuration.
@@ -69,17 +94,16 @@ type TLSConfig struct {
 	// contains TLS certificates and private keys. These certificates are used to
 	// establish a TLS handshake with the external authorization server.
 	//
+	// If this field is not specified, the proxy will not present a client certificate
+	// and will use the system default certificate pool to verify the server certificate.
+	// +optional
 	// +kubebuilder:validation:MaxItems=64
 	CertificateRefs []gwapiv1a2.SecretObjectReference `json:"certificateRefs,omitempty"`
 }
 
 // AuthorizationResponse defines how the authorization response impacts the client request and response.
 type AuthorizationResponse struct {
-	// Authorization response headers that have a correspondent match will be added to the original client request.
+	// Authorization response headers that will be added to the original client request.
 	// Note that coexistent headers will be overridden.
 	AllowedUpstreamHeaders []string `json:"allowedUpstreamHeaders,omitempty"`
-
-	// Authorization response headers that have a correspondent match will be added to the original client request.
-	// Note that coexistent headers will be appended.
-	AllowedUpstreamHeadersToAppend []string `json:"allowedUpstreamMethods,omitempty"`
 }
