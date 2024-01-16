4954d416e55ec4c6c6481ec884096de1eb6cc5a2
huabing zhao <zhaohuabing@gmail.com>
2024-01-16 11:17:13 +0800

    address comments
    Signed-off-by: huabing zhao <zhaohuabing@gmail.com>

--- api/v1alpha1/ext_auth_types.go
+++ api/v1alpha1/ext_auth_types.go
@@ -29,20 +29,13 @@ type ExtAuth struct {
 	// +unionDiscriminator
 	Type ExtAuthServiceType `json:"type"`
 
-	// GRPCService defines the gRPC External Authorization service
+	// GRPC defines the gRPC External Authorization service
 	// Only one of GRPCService or HTTPService may be specified.
-	GRPCService *GRPCExtAuthService `json:"grpcService,omitempty" yaml:"grpcService"`
+	GRPC *GRPCExtAuthService `json:"grpcService,omitempty"`
 
-	// HTTPService defines the HTTP External Authorization service
+	// HTTP defines the HTTP External Authorization service
 	// Only one of GRPCService or HTTPService may be specified.
-	HTTPService *HTTPExtAuthService `json:"httpService,omitempty" yaml:"httpService"`
-
-	// AllowWhenFailed indicates whether the request should be allowed when the
-	// authorization service has failed or has returned an HTTP 5xx error.
-	// When set to true, the request will be allowed.
-	// When set to false, the request will be denied with an HTTP 403 Forbidden error.
-	// Defaults to false.
-	AllowWhenFailed bool `json:"allowWhenFailed,omitempty" yaml:"allowWhenFailed"`
+	HTTP *HTTPExtAuthService `json:"httpService,omitempty"`
 
 	// AllowedHeaders defines the client request headers that will be included
 	// in the request to the external authorization service.
@@ -50,13 +43,13 @@ type ExtAuth struct {
 	// services is different. All headers will be included in the check request
 	// to a gRPC authorization server, whereas no headers will be included in the
 	// check request to an HTTP authorization server.
-	AllowedHeaders []string `json:"allowedHeaders,omitempty" yaml:"allowedHeaders,omitempty"`
+	AllowedHeaders []string `json:"allowedHeaders,omitempty"`
 }
 
 // GRPCExtAuthService defines the gRPC External Authorization service
 type GRPCExtAuthService struct {
 	// Host is the hostname of the gRPC External Authorization service
-	Host gwapiv1a2.Hostname `json:"host,omitempty" yaml:"host,omitempty"`
+	Host gwapiv1a2.Hostname `json:"host"`
 
 	// Port is the network port of the gRPC External Authorization service
 	Port gwapiv1a2.PortNumber `json:"port"`
@@ -64,7 +57,7 @@ type GRPCExtAuthService struct {
 	// TLS defines the TLS configuration for the gRPC External Authorization service.
 	// Note: If not specified, the proxy will talk to the gRPC External
 	// Authorization service in plaintext.
-	TLS *TLSConfig `json:"tlsSettings,omitempty" yaml:"tlsSettings"`
+	TLS *TLSConfig `json:"tlsSettings,omitempty"`
 }
 
 // HTTPExtAuthService defines the HTTP External Authorization service
@@ -82,7 +75,7 @@ type HTTPExtAuthService struct {
 	// TLS is only valid when the URL scheme is https. If the URL scheme is
 	// https, and TLS is not specified, the proxy will use the system default
 	// certificate pool to verify the server certificate.
-	TLS *TLSConfig `json:"tlsSettings,omitempty" yaml:"tlsSettings"`
+	TLS *TLSConfig `json:"tlsSettings,omitempty"`
 
 	// Authorization response headers that will be added to the original client request
 	// before sending it to the upstream server.
@@ -101,11 +94,4 @@ type TLSConfig struct {
 	// +optional
 	// +kubebuilder:validation:MaxItems=64
 	CertificateRefs []gwapiv1a2.SecretObjectReference `json:"certificateRefs,omitempty"`
-}
-
-// AuthorizationResponse defines how the authorization response impacts the client request and response.
-type AuthorizationResponse struct {
-	// Authorization response headers that will be added to the original client request.
-	// Note that coexistent headers will be overridden.
-	AllowedUpstreamHeaders []string `json:"allowedUpstreamHeaders,omitempty"`
 }
