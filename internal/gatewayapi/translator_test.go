0767e13c012c3674482fc45242ebddfe23c8ae22
He Jie Xu <hejie.xu@intel.com>
2024-01-26 07:31:00 +0000

    add test
    Signed-off-by: He Jie Xu <hejie.xu@intel.com>

--- internal/gatewayapi/translator_test.go
+++ internal/gatewayapi/translator_test.go
@@ -43,6 +43,16 @@ func mustUnmarshal(t *testing.T, val []byte, out interface{}) {
 }
 
 func TestTranslate(t *testing.T) {
+	testCasesConfig := []struct {
+		name                    string
+		EnvoyPatchPolicyEnabled bool
+	}{
+		{
+			name:                    "envoypatchpolicy-invalid-feature-disabled",
+			EnvoyPatchPolicyEnabled: false,
+		},
+	}
+
 	inputFiles, err := filepath.Glob(filepath.Join("testdata", "*.in.yaml"))
 	require.NoError(t, err)
 
@@ -54,12 +64,19 @@ func TestTranslate(t *testing.T) {
 
 			resources := &Resources{}
 			mustUnmarshal(t, input, resources)
+			envoyPatchPolicyEnabled := true
+
+			for _, config := range testCasesConfig {
+				if config.name == strings.Split(filepath.Base(inputFile), ".")[0] {
+					envoyPatchPolicyEnabled = config.EnvoyPatchPolicyEnabled
+				}
+			}
 
 			translator := &Translator{
 				GatewayControllerName:   egv1a1.GatewayControllerName,
 				GatewayClassName:        "envoy-gateway-class",
 				GlobalRateLimitEnabled:  true,
-				EnvoyPatchPolicyEnabled: true,
+				EnvoyPatchPolicyEnabled: envoyPatchPolicyEnabled,
 			}
 
 			// Add common test fixtures
