5289a788f409527560ec59d9c71656d85697f726
He Jie Xu <hejie.xu@intel.com>
2024-01-26 23:17:28 +0000

    address comments
    Signed-off-by: He Jie Xu <hejie.xu@intel.com>

--- api/v1alpha1/compression_types.go
+++ api/v1alpha1/compression_types.go
@@ -10,22 +10,19 @@ package v1alpha1
 // +kubebuilder:validation:Enum=Gzip
 type CompressorType string
 
-// GzipCompressor defines the config for the Gzip compressor. There are some configs
-// available from the Envoy Proxy. Currently only use the default value for configs.
-// For the default value for those configs can be reference here:
+// GzipCompressor defines the config for the Gzip compressor.
+// The default values can be found here:
 // https://www.envoyproxy.io/docs/envoy/latest/api-v3/extensions/compression/gzip/compressor/v3/gzip.proto#extension-envoy-compression-gzip-compressor
 type GzipCompressor struct {
 }
 
-// Compression defines the config of compression for the http streams. Currently
-// only the minial config was added. All configs from the Envoy Proxy are using
-// the default value. For the default value can be reference here:
-// https://www.envoyproxy.io/docs/envoy/latest/api-v3/extensions/filters/http/compressor/v3/compressor.proto#extensions-filters-http-compressor-v3-compressor
+// Compression defines the config of enabling compression.
+// This can help reduce the bandwidth at the expense of higher CPU.
 type Compression struct {
-	// CompressorType defines which type compressor wants to use for compression.
+	// CompressorType defines the compressor type to use for compression.
 	//
 	// +required
-	Type CompressorType `json:"type,omitempty"`
+	Type CompressorType `json:"type"`
 
 	// The configuration for GZIP compressor.
 	//
