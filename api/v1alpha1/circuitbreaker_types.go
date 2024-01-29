383d73bf94ba4f687b4b363ee1dca071a576c96e
Guy Daich <guy.daich@sap.com>
2024-01-29 16:36:34 -0600

    add default to comment
    Signed-off-by: Guy Daich <guy.daich@sap.com>

--- api/v1alpha1/circuitbreaker_types.go
+++ api/v1alpha1/circuitbreaker_types.go
@@ -32,6 +32,7 @@ type CircuitBreaker struct {
 	MaxParallelRequests *int64 `json:"maxParallelRequests,omitempty"`
 
 	// The maximum number of requests that Envoy will make over a single connection to the referenced backend defined within a xRoute rule.
+	// Default: unlimited.
 	//
 	// +kubebuilder:validation:Minimum=0
 	// +kubebuilder:validation:Maximum=4294967295
