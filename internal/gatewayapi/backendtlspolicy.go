e4cbcdc031739a2d1dde4e0473864a1c085ba1b4
Huabing Zhao <zhaohuabing@gmail.com>
2024-03-08 09:34:22 +0800

    Add referenced BackendRefs for ExtAuth to Resource Tree (#2795)
    * add referenced BackendRefs for ExtAuth to Resource Tree
    
    Signed-off-by: huabing zhao <zhaohuabing@gmail.com>
    
    * clean up the controller code
    
    Signed-off-by: huabing zhao <zhaohuabing@gmail.com>
    
    * minor changes
    
    Signed-off-by: huabing zhao <zhaohuabing@gmail.com>
    
    * return errors
    
    Signed-off-by: huabing zhao <zhaohuabing@gmail.com>
    
    * fix validate error
    
    Signed-off-by: huabing zhao <zhaohuabing@gmail.com>
    
    * fix gen
    
    Signed-off-by: huabing zhao <zhaohuabing@gmail.com>
    
    * Support BackendTLSPolicy for the Ext HTTP/GRPC auth service
    
    Signed-off-by: huabing zhao <zhaohuabing@gmail.com>
    
    * fix lint
    
    Signed-off-by: huabing zhao <zhaohuabing@gmail.com>
    
    * fix gen
    
    Signed-off-by: huabing zhao <zhaohuabing@gmail.com>
    
    * check cross-ns reference grant
    
    Signed-off-by: huabing zhao <zhaohuabing@gmail.com>
    
    * fix test
    
    Signed-off-by: huabing zhao <zhaohuabing@gmail.com>
    
    * fix test
    
    Signed-off-by: huabing zhao <zhaohuabing@gmail.com>
    
    ---------
    
    Signed-off-by: huabing zhao <zhaohuabing@gmail.com>

--- internal/gatewayapi/backendtlspolicy.go
+++ internal/gatewayapi/backendtlspolicy.go
@@ -6,13 +6,166 @@
 package gatewayapi
 
 import (
+	"fmt"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/utils/ptr"
-	"sigs.k8s.io/gateway-api/apis/v1alpha2"
+	gwapiv1 "sigs.k8s.io/gateway-api/apis/v1"
+	gwapiv1a2 "sigs.k8s.io/gateway-api/apis/v1alpha2"
+
+	"github.com/envoyproxy/gateway/internal/ir"
+	"github.com/envoyproxy/gateway/internal/status"
 )
 
-func (t *Translator) ProcessBackendTLSPoliciesAncestorRef(backendTLSPolicies []*v1alpha2.BackendTLSPolicy, gateways []*GatewayContext) []*v1alpha2.BackendTLSPolicy {
+func (t *Translator) processBackendTLSPolicy(
+	backendRef gwapiv1.BackendObjectReference,
+	backendNamespace string,
+	parent gwapiv1a2.ParentReference,
+	resources *Resources) *ir.TLSUpstreamConfig {
+	tlsBundle, err := getBackendTLSBundle(resources.BackendTLSPolicies, resources.ConfigMaps, backendRef, backendNamespace)
+	if err == nil && tlsBundle == nil {
+		return nil
+	}
 
-	var res []*v1alpha2.BackendTLSPolicy
+	policy := getBackendTLSPolicy(resources.BackendTLSPolicies, backendRef, backendNamespace)
+
+	ancestor := gwapiv1a2.PolicyAncestorStatus{
+		AncestorRef:    parent,
+		ControllerName: gwapiv1.GatewayController(t.GatewayControllerName),
+	}
+
+	if err != nil {
+		status.SetBackendTLSPolicyCondition(
+			policy,
+			ancestor,
+			gwapiv1a2.PolicyConditionAccepted,
+			metav1.ConditionFalse,
+			gwapiv1a2.PolicyReasonInvalid,
+			status.Error2ConditionMsg(err))
+		return nil
+	}
+
+	// Check if the reference from BackendTLSPolicy to BackendRef is permitted by
+	// any ReferenceGrant
+	backendRefKind := KindService
+	if backendRef.Kind != nil {
+		backendRefKind = string(*backendRef.Kind)
+	}
+	if policy.Namespace != backendNamespace {
+		if !t.validateCrossNamespaceRef(
+			crossNamespaceFrom{
+				group:     gwapiv1.GroupName,
+				kind:      KindBackendTLSPolicy,
+				namespace: policy.Namespace,
+			},
+			crossNamespaceTo{
+				group:     "",
+				kind:      backendRefKind,
+				namespace: backendNamespace,
+				name:      string(backendRef.Name),
+			},
+			resources.ReferenceGrants,
+		) {
+			status.SetBackendTLSPolicyCondition(
+				policy,
+				ancestor,
+				gwapiv1a2.PolicyConditionAccepted,
+				metav1.ConditionFalse,
+				gwapiv1a2.PolicyReasonInvalid,
+				fmt.Sprintf("target ref to %s %s/%s not permitted by any ReferenceGrant",
+					backendRefKind, backendNamespace, backendRef.Name))
+			return nil
+		}
+	}
+
+	status.SetBackendTLSPolicyCondition(
+		policy,
+		ancestor,
+		gwapiv1a2.PolicyConditionAccepted,
+		metav1.ConditionTrue,
+		gwapiv1a2.PolicyReasonAccepted,
+		"BackendTLSPolicy is Accepted")
+	return tlsBundle
+}
+
+func backendTLSTargetMatched(policy gwapiv1a2.BackendTLSPolicy, target gwapiv1a2.PolicyTargetReferenceWithSectionName) bool {
+
+	policyTarget := policy.Spec.TargetRef
+
+	if target.Group == policyTarget.Group &&
+		target.Kind == policyTarget.Kind &&
+		target.Name == policyTarget.Name &&
+		NamespaceDerefOr(policyTarget.Namespace, policy.Namespace) == string(*target.Namespace) {
+		if policyTarget.SectionName != nil && *policyTarget.SectionName != *target.SectionName {
+			return false
+		}
+		return true
+	}
+	return false
+}
+
+func getBackendTLSPolicy(policies []*gwapiv1a2.BackendTLSPolicy, backendRef gwapiv1a2.BackendObjectReference, backendNamespace string) *gwapiv1a2.BackendTLSPolicy {
+	target := GetTargetBackendReference(backendRef, backendNamespace)
+	for _, policy := range policies {
+		if backendTLSTargetMatched(*policy, target) {
+			return policy
+		}
+	}
+	return nil
+}
+
+func getBackendTLSBundle(policies []*gwapiv1a2.BackendTLSPolicy, configmaps []*corev1.ConfigMap, backendRef gwapiv1a2.BackendObjectReference, backendNamespace string) (*ir.TLSUpstreamConfig, error) {
+
+	backendTLSPolicy := getBackendTLSPolicy(policies, backendRef, backendNamespace)
+
+	if backendTLSPolicy == nil {
+		return nil, nil
+	}
+
+	tlsBundle := &ir.TLSUpstreamConfig{
+		SNI:                 string(backendTLSPolicy.Spec.TLS.Hostname),
+		UseSystemTrustStore: ptr.Deref(backendTLSPolicy.Spec.TLS.WellKnownCACerts, "") == gwapiv1a2.WellKnownCACertSystem,
+	}
+	if tlsBundle.UseSystemTrustStore {
+		return tlsBundle, nil
+	}
+
+	caRefMap := make(map[string]string)
+
+	for _, caRef := range backendTLSPolicy.Spec.TLS.CACertRefs {
+		caRefMap[string(caRef.Name)] = string(caRef.Kind)
+	}
+
+	ca := ""
+
+	for _, cmap := range configmaps {
+		if kind, ok := caRefMap[cmap.Name]; ok && kind == cmap.Kind {
+			if crt, dataOk := cmap.Data["ca.crt"]; dataOk {
+				if ca != "" {
+					ca += "\n"
+				}
+				ca += crt
+			} else {
+				return nil, fmt.Errorf("no ca found in configmap %s", cmap.Name)
+			}
+		}
+	}
+
+	if ca == "" {
+		return nil, fmt.Errorf("no ca found in referred configmaps")
+	}
+	tlsBundle.CACertificate = &ir.TLSCACertificate{
+		Certificate: []byte(ca),
+		Name:        fmt.Sprintf("%s/%s-ca", backendTLSPolicy.Name, backendTLSPolicy.Namespace),
+	}
+
+	return tlsBundle, nil
+}
+
+func (t *Translator) ProcessBackendTLSPoliciesAncestorRef(backendTLSPolicies []*gwapiv1a2.BackendTLSPolicy, gateways []*GatewayContext) []*gwapiv1a2.BackendTLSPolicy {
+
+	var res []*gwapiv1a2.BackendTLSPolicy
 
 	for _, btlsPolicy := range backendTLSPolicies {
 
@@ -21,6 +174,9 @@ func (t *Translator) ProcessBackendTLSPoliciesAncestorRef(backendTLSPolicies []*
 
 		if policy.Status.Ancestors != nil {
 			for k, status := range policy.Status.Ancestors {
+				if status.AncestorRef.Kind != nil && *status.AncestorRef.Kind != KindGateway {
+					continue
+				}
 				exist := false
 				for _, gwContext := range gateways {
 					gw := gwContext.Gateway
@@ -38,7 +194,7 @@ func (t *Translator) ProcessBackendTLSPoliciesAncestorRef(backendTLSPolicies []*
 				}
 			}
 		} else {
-			policy.Status.Ancestors = []v1alpha2.PolicyAncestorStatus{}
+			policy.Status.Ancestors = []gwapiv1a2.PolicyAncestorStatus{}
 		}
 	}
 
--- internal/xds/translator/extauth.go
+++ internal/xds/translator/extauth.go
@@ -64,7 +64,7 @@ func (*extAuth) patchHCM(mgr *hcmv3.HttpConnectionManager, irListener *ir.HTTPLi
 		mgr.HttpFilters = append(mgr.HttpFilters, filter)
 	}
 
-	return nil
+	return errs
 }
 
 // buildHCMExtAuthFilter returns an ext_authz HTTP filter from the provided IR HTTPRoute.
@@ -93,7 +93,10 @@ func extAuthFilterName(route *ir.HTTPRoute) string {
 }
 
 func extAuthConfig(extAuth *ir.ExtAuth) *extauthv3.ExtAuthz {
-	config := &extauthv3.ExtAuthz{}
+	config := &extauthv3.ExtAuthz{
+		TransportApiVersion: corev3.ApiVersion_V3,
+		FailureModeAllow:    false,
+	}
 
 	var headersToExtAuth []*matcherv3.StringMatcher
 	for _, header := range extAuth.HeadersToExtAuth {
@@ -134,6 +137,7 @@ func httpService(http *ir.HTTPExtAuthService) *extauthv3.HttpService {
 	var (
 		uri              string
 		headersToBackend []*matcherv3.StringMatcher
+		service          = new(extauthv3.HttpService)
 	)
 
 	u := url.URL{
@@ -146,6 +150,16 @@ func httpService(http *ir.HTTPExtAuthService) *extauthv3.HttpService {
 	}
 	uri = u.String()
 
+	service.ServerUri = &corev3.HttpUri{
+		Uri: uri,
+		HttpUpstreamType: &corev3.HttpUri_Cluster{
+			Cluster: http.Destination.Name,
+		},
+		Timeout: &duration.Duration{
+			Seconds: defaultExtServiceRequestTimeout,
+		},
+	}
+
 	for _, header := range http.HeadersToBackend {
 		headersToBackend = append(headersToBackend, &matcherv3.StringMatcher{
 			MatchPattern: &matcherv3.StringMatcher_Exact{
@@ -154,22 +168,15 @@ func httpService(http *ir.HTTPExtAuthService) *extauthv3.HttpService {
 		})
 	}
 
-	return &extauthv3.HttpService{
-		ServerUri: &corev3.HttpUri{
-			Uri: uri,
-			HttpUpstreamType: &corev3.HttpUri_Cluster{
-				Cluster: http.Destination.Name,
-			},
-			Timeout: &duration.Duration{
-				Seconds: defaultExtServiceRequestTimeout,
-			},
-		},
-		AuthorizationResponse: &extauthv3.AuthorizationResponse{
+	if len(headersToBackend) > 0 {
+		service.AuthorizationResponse = &extauthv3.AuthorizationResponse{
 			AllowedUpstreamHeaders: &matcherv3.ListStringMatcher{
 				Patterns: headersToBackend,
 			},
-		},
+		}
 	}
+
+	return service
 }
 
 func grpcService(grpc *ir.GRPCExtAuthService) *corev3.GrpcService_EnvoyGrpc {
