8fc6868b842c01411134f342b478c95c39cc20b7
Guy Daich <guy.daich@sap.com>
2024-03-10 07:59:58 -0500

    add negative test
    Signed-off-by: Guy Daich <guy.daich@sap.com>

--- test/e2e/tests/backend_tls.go
+++ test/e2e/tests/backend_tls.go
@@ -26,7 +26,7 @@ var BackendTLSTest = suite.ConformanceTest{
 	Description: "Connect to backend with TLS",
 	Manifests:   []string{"testdata/backend-tls.yaml"},
 	Test: func(t *testing.T, suite *suite.ConformanceTestSuite) {
-		t.Run("Request to TLS backend", func(t *testing.T) {
+		t.Run("with a backend TLS Policy", func(t *testing.T) {
 			ns := "gateway-conformance-infra"
 			routeNN := types.NamespacedName{Name: "http-with-backend-tls", Namespace: ns}
 			gwNN := types.NamespacedName{Name: "same-namespace", Namespace: ns}
@@ -44,5 +44,25 @@ var BackendTLSTest = suite.ConformanceTest{
 
 			http.MakeRequestAndExpectEventuallyConsistentResponse(t, suite.RoundTripper, suite.TimeoutConfig, gwAddr, expectedResponse)
 		})
+
+		t.Run("without a backend TLS Policy", func(t *testing.T) {
+			ns := "gateway-conformance-infra"
+			routeNN := types.NamespacedName{Name: "http-without-backend-tls", Namespace: ns}
+			gwNN := types.NamespacedName{Name: "same-namespace", Namespace: ns}
+			gwAddr := kubernetes.GatewayAndHTTPRoutesMustBeAccepted(t, suite.Client, suite.TimeoutConfig, suite.ControllerName, kubernetes.NewGatewayRef(gwNN), routeNN)
+
+			expectedResponse := http.ExpectedResponse{
+				Request: http.Request{
+					Path: "/backend-tls-without-policy",
+				},
+				Response: http.Response{
+					StatusCode: 400, // Bad Request: Client sent an HTTP request to an HTTPS server
+				},
+				Namespace: ns,
+			}
+
+			http.MakeRequestAndExpectEventuallyConsistentResponse(t, suite.RoundTripper, suite.TimeoutConfig, gwAddr, expectedResponse)
+		})
+
 	},
 }
