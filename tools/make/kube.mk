c4e13d2150e6c67003e5b976255ca49e986f7a01
Alexander Volchok <alex.volchok@sap.com>
2024-03-11 16:32:57 +0100

    fix kube make single test exec
    Signed-off-by: Alexander Volchok <alex.volchok@sap.com>

--- tools/make/kube.mk
+++ tools/make/kube.mk
@@ -12,6 +12,7 @@ FLUENT_BIT_CHART_VERSION ?= 0.30.4
 OTEL_COLLECTOR_CHART_VERSION ?= 0.73.1
 TEMPO_CHART_VERSION ?= 1.3.1
 E2E_RUN_TEST ?=
+E2E_RUN_EG_UPGRADE_TESTS ?= false
 E2E_CLEANUP ?= true
 
 # Set Kubernetes Resources Directory Path
@@ -124,12 +125,14 @@ ifeq ($(E2E_RUN_TEST),)
 	go test -v -tags e2e ./test/e2e --gateway-class=envoy-gateway --debug=true --cleanup-base-resources=false
 	go test -v -tags e2e ./test/e2e/upgrade --gateway-class=envoy-gateway --debug=true --cleanup-base-resources=$(E2E_CLEANUP)
 else
+ifeq ($(E2E_RUN_EG_UPGRADE_TESTS),false)
 	go test -v -tags e2e ./test/e2e --gateway-class=envoy-gateway --debug=true --cleanup-base-resources=$(E2E_CLEANUP) \
 		--run-test $(E2E_RUN_TEST)
+else
 	go test -v -tags e2e ./test/e2e/upgrade --gateway-class=envoy-gateway --debug=true --cleanup-base-resources=$(E2E_CLEANUP) \
 		--run-test $(E2E_RUN_TEST)
 endif
-
+endif
 .PHONY: install-e2e-telemetry
 install-e2e-telemetry: prepare-helm-repo install-fluent-bit install-loki install-tempo install-otel-collector install-prometheus
 	@$(LOG_TARGET)
