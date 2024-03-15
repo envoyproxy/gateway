8a86e002bf33d06162bae6c08437c98ac8e589af
shawnh2 <shawnhxh@outlook.com>
2024-03-15 08:46:39 +0800

    fix resources cleanup
    Signed-off-by: shawnh2 <shawnhxh@outlook.com>

--- tools/make/kube.mk
+++ tools/make/kube.mk
@@ -122,12 +122,12 @@ run-e2e: install-e2e-telemetry
 	kubectl wait --timeout=5m -n envoy-gateway-system deployment/envoy-gateway --for=condition=Available
 	kubectl apply -f test/config/gatewayclass.yaml
 ifeq ($(E2E_RUN_TEST),)
-	go test -v -tags e2e ./test/e2e --gateway-class=envoy-gateway --debug=true --cleanup-base-resources=$(E2E_CLEANUP)
-	go test -v -tags e2e ./test/e2e/upgrade --gateway-class=upgrade --debug=true --cleanup-base-resources=$(E2E_CLEANUP)
+	go test -v -tags e2e ./test/e2e --gateway-class=envoy-gateway --debug=true --cleanup-base-resources=false
+	go test -v -tags e2e ./test/e2e/upgrade --gateway-class=upgrade --debug=true --cleanup-base-resources=false
 	go test -v -tags e2e ./test/e2e/merge-gateways --gateway-class=merge-gateways --debug=true --cleanup-base-resources=$(E2E_CLEANUP)
 else
 ifeq ($(E2E_RUN_EG_UPGRADE_TESTS),false)
-	go test -v -tags e2e ./test/e2e --gateway-class=envoy-gateway --debug=true --cleanup-base-resources=$(E2E_CLEANUP) \
+	go test -v -tags e2e ./test/e2e --gateway-class=envoy-gateway --debug=true --cleanup-base-resources=false \
 		--run-test $(E2E_RUN_TEST)
 	go test -v -tags e2e ./test/e2e/merge-gateways --gateway-class=merge-gateways --debug=true --cleanup-base-resources=$(E2E_CLEANUP) \
 		--run-test $(E2E_RUN_TEST)
