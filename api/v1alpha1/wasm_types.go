93747c94126440e77a6bea99eb17b6c662e4f997
huabing zhao <zhaohuabing@gmail.com>
2024-03-21 09:23:49 +0800

    ues envoyextenstionpolicy
    Signed-off-by: huabing zhao <zhaohuabing@gmail.com>

---
+++ api/v1alpha1/wasm_types.go
@@ -0,0 +1,55 @@
+// Copyright Envoy Gateway Authors
+// SPDX-License-Identifier: Apache-2.0
+// The full text of the Apache license is available in the LICENSE file at
+// the root of the repo.
+
+package v1alpha1
+
+import (
+	"github.com/golang/protobuf/ptypes/any"
+)
+
+// Wasm defines a wasm extension.
+type Wasm struct {
+	// Name is a unique name for this Wasm extension. It is used to identify the
+	// Wasm extension if multiple extensions are handled by the same vm_id and root_id.
+	// It's also used for logging/debugging.
+	Name string `json:"name"`
+
+	// VmID is an ID that will be used along with a hash of the wasm code to
+	// determine which VM will be used to load the Wasm extension. All extensions
+	// that have the same vm_id and code will use the same VM.
+	//
+	// Note that sharing a VM between plugins can reduce memory utilization and
+	// make sharing of data easier, but it may have security implications.
+	VmID *string `json:"vmID,omitempty"`
+
+	// RootID is a unique ID for a set of extensions in a VM which will share a
+	// RootContext and Contexts if applicable (e.g., an Wasm HttpFilter and an Wasm AccessLog).
+	// If left blank, all extensions with a blank root_id with the same vm_id will share Context(s).
+	RootID *string `json:"rootID,omitempty"`
+
+	// Code is the wasm code for the extension.
+	Code WasmCodeSource `json:"code"`
+
+	// Configuration for the wasm code.
+	Config any.Any `json:"config"`
+}
+
+// WasmCodeSource defines the source of the wasm code.
+type WasmCodeSource struct {
+	// ConfigMap is the name of the ConfigMap containing the wasm code.
+	//
+	// The key in the ConfigMap should be the name of the Wasm. For example,
+	// if the Wasm is named "my-wasm-extension", the ConfigMap should have a key
+	// named "my-wasm-extension" and the value should be the wasm code.
+	ConfigMap *string `json:"ConfigMap,omitempty"`
+
+	// HTTP is the HTTP URL containing the wasm code.
+	//
+	// Note that the HTTP server must be accessible from the Envoy proxy.
+	HTTP *string `json:"http,omitempty"`
+
+	// Image is the OCI image containing the wasm code.
+	// Image *string `json:"image,omitempty"` //TODO: Add support for OCI image in the future.
+}
