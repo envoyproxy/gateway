59a503028dadb8761f0d02ca94e1a9f91c74da58
ShyunnY <1147212064@qq.com>
2024-03-27 16:05:42 +0800

    test: supplementary metrics test
    Signed-off-by: ShyunnY <1147212064@qq.com>

--- internal/metrics/sample_gauge_test.go
+++ internal/metrics/sample_gauge_test.go
@@ -5,23 +5,80 @@
 
 package metrics_test
 
-import "github.com/envoyproxy/gateway/internal/metrics"
+import (
+	"context"
+	"io"
+	"os"
+	"testing"
 
-var (
-	irType        = metrics.NewLabel("ir-type")
-	currentIRsNum = metrics.NewGauge(
+	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/stdout/stdoutmetric"
+	"go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/resource"
+	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
+
+	"github.com/envoyproxy/gateway/internal/metrics"
+)
+
+func TestGauge(t *testing.T) {
+	writer := os.Stdout
+	if true {
+		// nolint:gosec
+		f, err := os.OpenFile("testdata/gauge_metric.yaml", os.O_CREATE|os.O_RDWR, 0644)
+		require.NoError(t, err)
+		writer = f
+	}
+	p, err := initGaugeProvider(writer)
+	require.NoError(t, err)
+
+	// simulate a function that builds an indicator and changes its value
+	metricsFunc := []func(){
+		xdsIRGauge,
+	}
+	for _, f := range metricsFunc {
+		f()
+	}
+
+	// Ensure that metrics data can be flushed by closing the provider
+	err = p.Shutdown(context.TODO())
+	require.NoError(t, err)
+}
+
+func xdsIRGauge() {
+	currentIRsNum := metrics.NewGauge(
 		"current_irs_queue_num",
 		"current number of ir in queue, by ir type",
 	)
-)
 
-func NewGauge() {
 	// only the last recorded value (2) will be exported for this gauge
-	currentIRsNum.With(irType.Value("xds")).Record(1)
-	currentIRsNum.With(irType.Value("xds")).Record(3)
-	currentIRsNum.With(irType.Value("xds")).Record(2)
+	currentIRsNum.With(metrics.NewLabel("ir-type").Value("xds")).Record(1)
+	currentIRsNum.With(metrics.NewLabel("ir-type").Value("xds")).Record(3)
+	currentIRsNum.With(metrics.NewLabel("ir-type").Value("xds")).Record(2)
 
-	currentIRsNum.With(irType.Value("infra")).Record(1)
-	currentIRsNum.With(irType.Value("infra")).Record(3)
-	currentIRsNum.With(irType.Value("infra")).Record(2)
+	currentIRsNum.With(metrics.NewLabel("ir-type").Value("xds")).Record(1)
+	currentIRsNum.With(metrics.NewLabel("ir-type").Value("xds")).Record(3)
+	currentIRsNum.With(metrics.NewLabel("ir-type").Value("xds")).Record(2)
+}
+
+func initGaugeProvider(writer io.Writer) (*metric.MeterProvider, error) {
+	stdExp, err := stdoutmetric.New(
+		stdoutmetric.WithPrettyPrint(),
+		stdoutmetric.WithWriter(writer),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	p := metric.NewMeterProvider(
+		metric.WithReader(metric.NewPeriodicReader(stdExp)),
+		metric.WithResource(resource.NewSchemaless(
+			semconv.ServiceName("test"),
+			attribute.String("metric-type", "Gauge"),
+		)),
+	)
+	otel.SetMeterProvider(p)
+
+	return p, nil
 }
--- internal/metrics/sample_histogram_test.go
+++ internal/metrics/sample_histogram_test.go
@@ -5,19 +5,78 @@
 
 package metrics_test
 
-import "github.com/envoyproxy/gateway/internal/metrics"
+import (
+	"context"
+	"io"
+	"os"
+	"testing"
 
-var (
-	method = metrics.NewLabel("method")
+	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/stdout/stdoutmetric"
+	"go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/resource"
+	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
 
-	sentBytes = metrics.NewHistogram(
+	"github.com/envoyproxy/gateway/internal/metrics"
+)
+
+func TestHistogram(t *testing.T) {
+	writer := os.Stdout
+	if true {
+		// nolint:gosec
+		f, err := os.OpenFile("testdata/histogram_metric.yaml", os.O_CREATE|os.O_RDWR, 0644)
+		require.NoError(t, err)
+		writer = f
+	}
+	p, err := initHistogramProvider(writer)
+	require.NoError(t, err)
+
+	// simulate a function that builds an indicator and changes its value
+	metricsFunc := []func(){
+		methodHistogram,
+	}
+	for _, f := range metricsFunc {
+		f()
+	}
+
+	// Ensure that metrics data can be flushed by closing the provider
+	err = p.Shutdown(context.TODO())
+	require.NoError(t, err)
+}
+
+func methodHistogram() {
+	sentBytes := metrics.NewHistogram(
 		"sent_bytes_total",
 		"Histogram of sent bytes by method",
 		[]float64{10, 50, 100, 1000, 10000},
 		metrics.WithUnit(metrics.Bytes),
 	)
-)
 
-func NewHistogram() {
-	sentBytes.With(method.Value("/request/path/1")).Record(458)
+	// This will hit Bounds of 25,500 and 2500
+	sentBytes.With(metrics.NewLabel("method").Value("/request/path/1")).Record(20)
+	sentBytes.With(metrics.NewLabel("method").Value("/request/path/1")).Record(458)
+	sentBytes.With(metrics.NewLabel("method").Value("/request/path/1")).Record(2000)
+}
+
+func initHistogramProvider(writer io.Writer) (*metric.MeterProvider, error) {
+	stdExp, err := stdoutmetric.New(
+		stdoutmetric.WithPrettyPrint(),
+		stdoutmetric.WithWriter(writer),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	p := metric.NewMeterProvider(
+		metric.WithReader(metric.NewPeriodicReader(stdExp)),
+		metric.WithResource(resource.NewSchemaless(
+			semconv.ServiceName("test"),
+			attribute.String("metric-type", "Histogram"),
+		)),
+	)
+	otel.SetMeterProvider(p)
+
+	return p, nil
 }
