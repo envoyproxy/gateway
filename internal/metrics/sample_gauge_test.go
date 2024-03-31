b84eea9b8c8e7a568fe65f75374dd118182134fc
zirain <zirain2009@gmail.com>
2024-03-31 11:51:35 +0800

    fix yamllint
    Signed-off-by: zirain <zirain2009@gmail.com>

--- internal/metrics/sample_gauge_test.go
+++ internal/metrics/sample_gauge_test.go
@@ -27,7 +27,7 @@ func TestGauge(t *testing.T) {
 	writer := os.Stdout
 	if true {
 		// nolint:gosec
-		f, err := os.OpenFile("testdata/gauge_metric.yaml", os.O_CREATE|os.O_RDWR, 0644)
+		f, err := os.OpenFile("testdata/gauge_metric.json", os.O_CREATE|os.O_RDWR, 0644)
 		require.NoError(t, err)
 		writer = f
 	}
--- internal/metrics/sample_histogram_test.go
+++ internal/metrics/sample_histogram_test.go
@@ -27,7 +27,7 @@ func TestHistogram(t *testing.T) {
 	writer := os.Stdout
 	if true {
 		// nolint:gosec
-		f, err := os.OpenFile("testdata/histogram_metric.yaml", os.O_CREATE|os.O_RDWR, 0644)
+		f, err := os.OpenFile("testdata/histogram_metric.json", os.O_CREATE|os.O_RDWR, 0644)
 		require.NoError(t, err)
 		writer = f
 	}
