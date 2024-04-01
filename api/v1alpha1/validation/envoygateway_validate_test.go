a7b5a47a4044581a39a7e4a73b96c43075e1d652
Alexander Volchok <alex.volchok@sap.com>
2024-04-01 22:04:54 +0200

    fix test
    Signed-off-by: Alexander Volchok <alex.volchok@sap.com>

--- api/v1alpha1/validation/envoygateway_validate_test.go
+++ api/v1alpha1/validation/envoygateway_validate_test.go
@@ -646,7 +646,8 @@ func TestEnvoyGatewayProvider(t *testing.T) {
 	assert.NotNil(t, envoyGateway.Provider)
 
 	envoyGatewayProvider := envoyGateway.GetEnvoyGatewayProvider()
-	assert.Nil(t, envoyGatewayProvider.Kubernetes)
+	assert.NotNil(t, envoyGatewayProvider.Kubernetes)
+	assert.NotNil(t, envoyGatewayProvider.Kubernetes.LeaderElection)
 	assert.Equal(t, envoyGateway.Provider, envoyGatewayProvider)
 
 	envoyGatewayProvider.Kubernetes = v1alpha1.DefaultEnvoyGatewayKubeProvider()
