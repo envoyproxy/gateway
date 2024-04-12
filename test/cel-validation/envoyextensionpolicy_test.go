ec9f5e43955a6528dd15a4b9649e30d6adf3905f
Guy Daich <guy.daich@sap.com>
2024-04-12 09:42:48 -0500

    fix lint
    Signed-off-by: Guy Daich <guy.daich@sap.com>

--- test/cel-validation/envoyextensionpolicy_test.go
+++ test/cel-validation/envoyextensionpolicy_test.go
@@ -11,15 +11,16 @@ package celvalidation
 import (
 	"context"
 	"fmt"
-	"k8s.io/utils/ptr"
-	gwapiv1 "sigs.k8s.io/gateway-api/apis/v1"
 	"strings"
 	"testing"
 	"time"
 
+	"k8s.io/utils/ptr"
+
 	egv1a1 "github.com/envoyproxy/gateway/api/v1alpha1"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	gwapiv1 "sigs.k8s.io/gateway-api/apis/v1"
 	gwapiv1a2 "sigs.k8s.io/gateway-api/apis/v1alpha2"
 )
 
