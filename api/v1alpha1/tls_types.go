786bf76f3f2d3cfe85eb253efc896a4beeb6e3aa
zufardhiyaulhaq <zufardhiyaulhaq@gmail.com>
2024-04-15 18:50:52 +0700

    feat: change RequiredClientCertificate to Required
    Signed-off-by: zufardhiyaulhaq <zufardhiyaulhaq@gmail.com>

--- api/v1alpha1/tls_types.go
+++ api/v1alpha1/tls_types.go
@@ -110,11 +110,11 @@ const (
 // to the Gateway.
 // By default, no client specific configuration is validated.
 type ClientValidationContext struct {
-	// RequiredClientCertificate set to true means that Envoy will reject connections without a valid client certificate.
+	// Required set to true means that Envoy will reject connections without a valid client certificate.
 	//
 	// +kubebuilder:default=true
 	// +optional
-	RequiredClientCertificate bool `json:"requiredClientCertificate,omitempty"`
+	Required bool `json:"required,omitempty"`
 
 	// CACertificateRefs contains one or more references to
 	// Kubernetes objects that contain TLS certificates of
