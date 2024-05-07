286870074e4ee5570c333580295406c0e8a591ba
zirain <zirain2009@gmail.com>
2024-05-07 19:46:10 +0800

    lint
    Signed-off-by: zirain <zirain2009@gmail.com>

--- test/conformance/experimental_conformance_test.go
+++ test/conformance/experimental_conformance_test.go
@@ -1,10 +1,10 @@
-//go:build experimental
-// +build experimental
-
 // Copyright Envoy Gateway Authors
 // SPDX-License-Identifier: Apache-2.0
 // The full text of the Apache license is available in the LICENSE file at
 // the root of the repo.
+
+//go:build experimental
+// +build experimental
 
 package conformance
 
