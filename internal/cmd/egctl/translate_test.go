02e936741d80dc91789e11a61d41ed430eb36d12
Kobi Levi <56400138+levikobi@users.noreply.github.com>
2024-05-16 00:24:24 +0300

    feat: add supported features to gateway class (#2491)
    * feat: add supported features to gateway class
    
    Signed-off-by: Kobi Levi <kobilevi503@gmail.com>
    
    * remove skipped test features from supported features list
    
    Signed-off-by: Kobi Levi <kobilevi503@gmail.com>
    
    ---------
    
    Signed-off-by: Kobi Levi <kobilevi503@gmail.com>

--- internal/cmd/egctl/translate_test.go
+++ internal/cmd/egctl/translate_test.go
@@ -22,6 +22,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/yaml"
 
+	"github.com/envoyproxy/gateway/internal/gatewayapi/status"
 	"github.com/envoyproxy/gateway/internal/utils/field"
 	"github.com/envoyproxy/gateway/internal/utils/file"
 )
@@ -349,6 +350,13 @@ func TestTranslate(t *testing.T) {
 			}
 			want := &TranslationResult{}
 			mustUnmarshal(t, requireTestDataOutFile(t, fn), want)
+
+			// Supported features are dynamic, instead of hard-coding them in the output files
+			// we define them here.
+			if want.GatewayClass != nil {
+				want.GatewayClass.Status.SupportedFeatures = status.GatewaySupportedFeatures
+			}
+
 			opts := cmpopts.IgnoreFields(metav1.Condition{}, "LastTransitionTime")
 			require.Empty(t, cmp.Diff(want, got, opts))
 		})
