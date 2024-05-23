d1e8e4c47349e499df0a6e47d19687e2a633a281
Lior Okman <lior.okman@sap.com>
2024-05-23 12:50:48 +0300

    Regenerate deepcopies.
    Signed-off-by: Lior Okman <lior.okman@sap.com>

--- internal/gatewayapi/zz_generated.deepcopy.go
+++ internal/gatewayapi/zz_generated.deepcopy.go
@@ -14,7 +14,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	discoveryv1 "k8s.io/api/discovery/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	v1 "sigs.k8s.io/gateway-api/apis/v1"
+	"sigs.k8s.io/gateway-api/apis/v1"
 	"sigs.k8s.io/gateway-api/apis/v1alpha2"
 	"sigs.k8s.io/gateway-api/apis/v1alpha3"
 	"sigs.k8s.io/gateway-api/apis/v1beta1"
