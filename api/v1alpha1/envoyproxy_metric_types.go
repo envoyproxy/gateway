5bc5e0fcb59cbba21eb33c0e86cf67ece480e23b
zirain <zirain2009@gmail.com>
2024-05-25 05:44:50 +0800

    fix api wording (#3467)
    * fix api wording
    
    Signed-off-by: zirain <zirain2009@gmail.com>
    
    * update
    
    Signed-off-by: zirain <zirain2009@gmail.com>
    
    ---------
    
    Signed-off-by: zirain <zirain2009@gmail.com>

--- api/v1alpha1/envoyproxy_metric_types.go
+++ api/v1alpha1/envoyproxy_metric_types.go
@@ -56,12 +56,12 @@ type ProxyMetricSink struct {
 // +kubebuilder:validation:XValidation:message="host or backendRefs needs to be set",rule="has(self.host) || self.backendRefs.size() > 0"
 type ProxyOpenTelemetrySink struct {
 	// Host define the service hostname.
-	// Deprecated: Use BackendRef instead.
+	// Deprecated: Use BackendRefs instead.
 	//
 	// +optional
 	Host *string `json:"host,omitempty"`
 	// Port defines the port the service is exposed on.
-	// Deprecated: Use BackendRef instead.
+	// Deprecated: Use BackendRefs instead.
 	//
 	// +optional
 	// +kubebuilder:validation:Minimum=0
