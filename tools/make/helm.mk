16e39df8b4eacf10e8e79761290e73e9427ea8c8
shawnh2 <shawnhxh@outlook.com>
2024-06-01 11:33:10 +0800

    rename to gateway-addons-helm and keep one source of truth
    Signed-off-by: shawnh2 <shawnhxh@outlook.com>

--- tools/make/helm.mk
+++ tools/make/helm.mk
@@ -1,35 +1,44 @@
 # This is a wrapper to manage helm chart
 #
-# All make targets related to helmß are defined in this file.
+# All make targets related to helms are defined in this file.
 
 include tools/make/env.mk
 
 IMAGE_PULL_POLICY ?= IfNotPresent
 OCI_REGISTRY ?= oci://docker.io/envoyproxy
 CHART_NAME ?= gateway-helm
+ADDONS_CHART_NAME ?= gateway-addons-helm
 CHART_VERSION ?= ${RELEASE_VERSION}
 
 ##@ Helm
-helm-package:
-helm-package: ## Package envoy gateway helm chart.
+helm-package: ## Package envoy gateway and add-ons helm chart.
 helm-package: helm-generate
 	@$(LOG_TARGET)
 	helm package charts/${CHART_NAME} --app-version ${TAG} --version ${CHART_VERSION} --destination ${OUTPUT_DIR}/charts/
+	helm package charts/${ADDONS_CHART_NAME} --app-version ${TAG} --version ${CHART_VERSION} --destination ${OUTPUT_DIR}/charts/
 
 helm-push:
 helm-push: ## Push envoy gateway helm chart to OCI registry.
 	@$(LOG_TARGET)
 	helm push ${OUTPUT_DIR}/charts/${CHART_NAME}-${CHART_VERSION}.tgz ${OCI_REGISTRY}
+	helm push ${OUTPUT_DIR}/charts/${ADDONS_CHART_NAME}-${CHART_VERSION}.tgz ${OCI_REGISTRY}
 
 helm-install:
 helm-install: helm-generate ## Install envoy gateway helm chart from OCI registry.
 	@$(LOG_TARGET)
 	helm install eg ${OCI_REGISTRY}/${CHART_NAME} --version ${CHART_VERSION} -n envoy-gateway-system --create-namespace
 
+helm-install-addons:
+helm-install-addons: helm-generate ## Install envoy gateway addons helm chart from OCI registry.
+	@$(LOG_TARGET)
+	helm install eg ${OCI_REGISTRY}/${ADDONS_CHART_NAME} --version ${CHART_VERSION} -n monitoring --create-namespace
+
 .PHONY: helm-generate
 helm-generate:
-	GatewayImage=${IMAGE}:${TAG} GatewayImagePullPolicy=${IMAGE_PULL_POLICY} envsubst < charts/gateway-helm/values.tmpl.yaml > ./charts/gateway-helm/values.yaml
-	helm lint charts/gateway-helm
+	GatewayImage=${IMAGE}:${TAG} GatewayImagePullPolicy=${IMAGE_PULL_POLICY} envsubst < charts/${CHART_NAME}/values.tmpl.yaml > ./charts/${CHART_NAME}/values.yaml
+	helm lint charts/${CHART_NAME}
+	helm lint charts/${ADDONS_CHART_NAME}
+	helm dependency update charts/${ADDONS_CHART_NAME} # Update dependencies for add-ons chart.
 
 HELM_VALUES := $(wildcard test/helm/*.in.yaml)
 
@@ -38,5 +47,5 @@ helm-template: ## Template envoy gateway helm chart.z
 	@for file in $(HELM_VALUES); do \
   		filename=$$(basename $${file}); \
   		output="$${filename%.in.*}.out.yaml"; \
-		helm template eg charts/gateway-helm -f $${file} > test/helm/$$output --namespace=envoy-gateway-system; \
+		helm template eg charts/${CHART_NAME} -f $${file} > test/helm/$$output --namespace=envoy-gateway-system; \
 	done
