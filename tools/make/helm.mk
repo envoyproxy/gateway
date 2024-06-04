607d8bcd9a833841eee057b263c0bdc06de0fbea
sh2 <shawnhxh@outlook.com>
2024-06-05 04:34:39 +0800

    helm: add envoy gateway addon helm chart support (#3470)
    * initial dashboard addon helm chart
    
    Signed-off-by: shawnh2 <shawnhxh@outlook.com>
    
    * rename addon name and remove gateway-helm support
    
    Signed-off-by: shawnh2 <shawnhxh@outlook.com>
    
    * remove /charts from .helmignore
    
    Signed-off-by: shawnh2 <shawnhxh@outlook.com>
    
    * rename to gateway-addons-helm and keep one source of truth
    
    Signed-off-by: shawnh2 <shawnhxh@outlook.com>
    
    * restore examples values and fix comments
    
    Signed-off-by: shawnh2 <shawnhxh@outlook.com>
    
    * rewrite helm makefile
    
    Signed-off-by: shawnh2 <shawnhxh@outlook.com>
    
    ---------
    
    Signed-off-by: shawnh2 <shawnhxh@outlook.com>

--- tools/make/helm.mk
+++ tools/make/helm.mk
@@ -1,35 +1,77 @@
 # This is a wrapper to manage helm chart
 #
-# All make targets related to helmß are defined in this file.
+# All make targets related to helms are defined in this file.
 
 include tools/make/env.mk
+
+CHARTS := $(wildcard charts/*)
 
 IMAGE_PULL_POLICY ?= IfNotPresent
 OCI_REGISTRY ?= oci://docker.io/envoyproxy
 CHART_NAME ?= gateway-helm
 CHART_VERSION ?= ${RELEASE_VERSION}
+RELEASE_NAMESPACE ?= envoy-gateway-system
 
 ##@ Helm
+.PHONY: helm-package
+helm-package: ## Package envoy gateway relevant helm charts.
 helm-package:
-helm-package: ## Package envoy gateway helm chart.
-helm-package: helm-generate
-	@$(LOG_TARGET)
+	@for chart in $(CHARTS); do \
+    	$(LOG_TARGET); \
+      	$(MAKE) $(addprefix helm-package., $$(basename $${chart})); \
+	done
+
+.PHONY: helm-package.%
+helm-package.%: helm-generate.%
+	$(eval COMMAND := $(word 1,$(subst ., ,$*)))
+	$(eval CHART_NAME := $(COMMAND))
 	helm package charts/${CHART_NAME} --app-version ${TAG} --version ${CHART_VERSION} --destination ${OUTPUT_DIR}/charts/
 
+.PHONY: helm-push
+helm-push: ## Push envoy gateway relevant helm charts to OCI registry.
 helm-push:
-helm-push: ## Push envoy gateway helm chart to OCI registry.
-	@$(LOG_TARGET)
+	@for chart in $(CHARTS); do \
+		$(LOG_TARGET); \
+		$(MAKE) $(addprefix helm-push., $$(basename $${chart})); \
+	done
+
+.PHONY: helm-push.%
+helm-push.%: helm-package.%
+	$(eval COMMAND := $(word 1,$(subst ., ,$*)))
+	$(eval CHART_NAME := $(COMMAND))
 	helm push ${OUTPUT_DIR}/charts/${CHART_NAME}-${CHART_VERSION}.tgz ${OCI_REGISTRY}
 
+.PHONY: helm-install
+helm-install: ## Install envoy gateway relevant helm charts from OCI registry.
 helm-install:
-helm-install: helm-generate ## Install envoy gateway helm chart from OCI registry.
-	@$(LOG_TARGET)
-	helm install eg ${OCI_REGISTRY}/${CHART_NAME} --version ${CHART_VERSION} -n envoy-gateway-system --create-namespace
+	@for chart in $(CHARTS); do \
+		$(LOG_TARGET); \
+		$(MAKE) $(addprefix helm-install., $$(basename $${chart})); \
+	done
+
+.PHONY: helm-install.%
+helm-install.%: helm-generate.%
+	$(eval COMMAND := $(word 1,$(subst ., ,$*)))
+	$(eval CHART_NAME := $(COMMAND))
+	helm install eg ${OCI_REGISTRY}/${CHART_NAME} --version ${CHART_VERSION} -n ${RELEASE_NAMESPACE} --create-namespace
 
 .PHONY: helm-generate
 helm-generate:
-	GatewayImage=${IMAGE}:${TAG} GatewayImagePullPolicy=${IMAGE_PULL_POLICY} envsubst < charts/gateway-helm/values.tmpl.yaml > ./charts/gateway-helm/values.yaml
-	helm lint charts/gateway-helm
+	@for chart in $(CHARTS); do \
+  		$(LOG_TARGET); \
+  		$(MAKE) $(addprefix helm-generate., $$(basename $${chart})); \
+  	done
+
+.PHONY: helm-generate.%
+helm-generate.%:
+	$(eval COMMAND := $(word 1,$(subst ., ,$*)))
+	$(eval CHART_NAME := $(COMMAND))
+	@if test -f "charts/${CHART_NAME}/values.tmpl.yaml"; then \
+  		GatewayImage=${IMAGE}:${TAG} GatewayImagePullPolicy=${IMAGE_PULL_POLICY} \
+  		envsubst < charts/${CHART_NAME}/values.tmpl.yaml > ./charts/${CHART_NAME}/values.yaml; \
+  	fi
+	helm lint charts/${CHART_NAME}
+	helm dependency update charts/${CHART_NAME} # Update dependencies for add-ons chart.
 
 HELM_VALUES := $(wildcard test/helm/*.in.yaml)
 
@@ -38,5 +80,5 @@ helm-template: ## Template envoy gateway helm chart.z
 	@for file in $(HELM_VALUES); do \
   		filename=$$(basename $${file}); \
   		output="$${filename%.in.*}.out.yaml"; \
-		helm template eg charts/gateway-helm -f $${file} > test/helm/$$output --namespace=envoy-gateway-system; \
+		helm template eg charts/${CHART_NAME} -f $${file} > test/helm/$$output --namespace=${RELEASE_NAMESPACE}; \
 	done
