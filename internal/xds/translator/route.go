f9eac402e7a18596502e2aaf97353f4442aaa99f
sh2 <shawnhxh@outlook.com>
2024-07-18 04:56:33 +0800

    fix: nil pointer err during hash load balancing build (#3886)
    fix nil pointer err in buildHashPolicy
    
    Signed-off-by: shawnh2 <shawnhxh@outlook.com>

--- internal/xds/translator/route.go
+++ internal/xds/translator/route.go
@@ -505,12 +505,12 @@ func buildHashPolicy(httpRoute *ir.HTTPRoute) []*routev3.RouteAction_HashPolicy 
 			hashPolicy.GetCookie().Ttl = durationpb.New(ch.Cookie.TTL.Duration)
 		}
 		if ch.Cookie.Attributes != nil {
-			attributes := make([]*routev3.RouteAction_HashPolicy_CookieAttribute, len(ch.Cookie.Attributes))
-			i := 0
+			attributes := make([]*routev3.RouteAction_HashPolicy_CookieAttribute, 0, len(ch.Cookie.Attributes))
 			for name, value := range ch.Cookie.Attributes {
-				attributes[i].Name = name
-				attributes[i].Value = value
-				i++
+				attributes = append(attributes, &routev3.RouteAction_HashPolicy_CookieAttribute{
+					Name:  name,
+					Value: value,
+				})
 			}
 			hashPolicy.GetCookie().Attributes = attributes
 		}
