1b33fc3fe759d5fe9322ac457908f9db1cd72d29
Arko Dasgupta <arkodg@users.noreply.github.com>
2024-08-09 15:13:17 -0700

    reduce readinessProbe failureThreshold and periodSeconds (#4021)
    * Reduces time for the endpoint to be removed from the endpointSlice
    from `30s` (3 * 10) to `5s` (1 * 5)
    
    * Since kube-proxy and CNIs rely on this info and so do external LBs
    like GKE https://cloud.google.com/kubernetes-engine/docs/concepts/service-load-balancer
    
    Signed-off-by: Arko Dasgupta <arko@tetrate.io>
    (cherry picked from commit 67575b8b7be29ce3ccbf8b20bea18201f6b45a0e)
    Signed-off-by: Arko Dasgupta <arko@tetrate.io>

--- internal/infrastructure/kubernetes/ratelimit/resource.go
+++ internal/infrastructure/kubernetes/ratelimit/resource.go
@@ -171,9 +171,9 @@ func expectedRateLimitContainers(rateLimit *egv1a1.RateLimit, rateLimitDeploymen
 					},
 				},
 				TimeoutSeconds:   1,
-				PeriodSeconds:    10,
+				PeriodSeconds:    5,
 				SuccessThreshold: 1,
-				FailureThreshold: 3,
+				FailureThreshold: 1,
 			},
 		},
 	}
