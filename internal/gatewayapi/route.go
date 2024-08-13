fb8a091c92d2139927fa4ba510b5059aac0268b4
Kobi Levi <kobilevi503@gmail.com>
2024-08-13 04:29:58 +0000

    feat: gateway http listener isolation
    Signed-off-by: Kobi Levi <kobilevi503@gmail.com>

--- internal/gatewayapi/route.go
+++ internal/gatewayapi/route.go
@@ -699,7 +699,7 @@ func (t *Translator) processHTTPRouteParentRefListener(route RouteContext, route
 	var hasHostnameIntersection bool
 
 	for _, listener := range parentRef.listeners {
-		hosts := computeHosts(GetHostnames(route), listener.Hostname)
+		hosts := computeHosts(GetHostnames(route), listener)
 		if len(hosts) == 0 {
 			continue
 		}
@@ -867,7 +867,7 @@ func (t *Translator) processTLSRouteParentRefs(tlsRoute *TLSRouteContext, resour
 
 		var hasHostnameIntersection bool
 		for _, listener := range parentRef.listeners {
-			hosts := computeHosts(GetHostnames(tlsRoute), listener.Hostname)
+			hosts := computeHosts(GetHostnames(tlsRoute), listener)
 			if len(hosts) == 0 {
 				continue
 			}
