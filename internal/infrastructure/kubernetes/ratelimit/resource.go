1d83511a2aaa793f77ffa77c8b4aac58fe393f14
zirain <zirain2009@gmail.com>
2024-08-20 06:31:11 +0800

    fix ratelimit statsd not working (#4073)
    fix ratelimit statd not working
    
    Signed-off-by: zirain <zirain2009@gmail.com>
    (cherry picked from commit 6ab6482dd7c807e64d4e87d78dfb06c0a1e16731)
    Signed-off-by: Arko Dasgupta <arko@tetrate.io>

--- internal/infrastructure/kubernetes/ratelimit/resource.go
+++ internal/infrastructure/kubernetes/ratelimit/resource.go
@@ -327,7 +327,11 @@ func expectedRateLimitContainerEnv(rateLimit *egv1a1.RateLimit, rateLimitDeploym
 		},
 		{
 			Name:  UseStatsdEnvVar,
-			Value: "false",
+			Value: "true",
+		},
+		{
+			Name:  "STATSD_PORT",
+			Value: "9125",
 		},
 		{
 			Name:  ConfigTypeEnvVar,
