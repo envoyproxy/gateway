af74047dbd95f0c71131e6bda6ec1c0033e88158
Lior Okman <lior.okman@sap.com>
2024-08-21 11:01:42 +0300

    If no expected status was explicitly set, use the default value as described in the documentation.
    Signed-off-by: Lior Okman <lior.okman@sap.com>

--- internal/gatewayapi/clustersettings.go
+++ internal/gatewayapi/clustersettings.go
@@ -10,6 +10,7 @@ import (
 	"fmt"
 	"math"
 	"math/big"
+	"net/http"
 	"strings"
 	"time"
 
@@ -452,6 +453,10 @@ func buildHTTPActiveHealthChecker(h *egv1a1.HTTPActiveHealthChecker) *ir.HTTPHea
 	statusSet := sets.NewInt()
 	for _, r := range h.ExpectedStatuses {
 		statusSet.Insert(int(r))
+	}
+	// If no ExpectedStatus was set, use the default value (200)
+	if statusSet.Len() == 0 {
+		statusSet.Insert(http.StatusOK)
 	}
 	irStatuses := make([]ir.HTTPStatus, 0, statusSet.Len())
 
