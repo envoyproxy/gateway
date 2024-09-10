20d5648ac90d0b48237b5b5750287bd96cc518a9
zirain <zirain2009@gmail.com>
2024-09-10 13:47:09 +0800

    bump ENVTEST_K8S_VERSION
    Signed-off-by: zirain <zirain2009@gmail.com>

--- tools/make/kube.mk
+++ tools/make/kube.mk
@@ -1,7 +1,7 @@
 # ENVTEST_K8S_VERSION refers to the version of kubebuilder assets to be downloaded by envtest binary.
-ENVTEST_K8S_VERSION ?= 1.28.0
+ENVTEST_K8S_VERSION ?= 1.28.14
 # Need run cel validation across multiple versions of k8s
-ENVTEST_K8S_VERSIONS ?= 1.27.1 1.28.0 1.29.0
+ENVTEST_K8S_VERSIONS ?= 1.28.13 1.29.8 1.30.4 1.31.0
 # GATEWAY_API_VERSION refers to the version of Gateway API CRDs.
 # For more details, see https://gateway-api.sigs.k8s.io/guides/getting-started/#installing-gateway-api
 GATEWAY_API_VERSION ?= $(shell go list -m -f '{{.Version}}' sigs.k8s.io/gateway-api)
