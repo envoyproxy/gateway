8b2a072ef31f54b82f7bd0a96d17104a37bff3ed
shawnh2 <shawnhxh@outlook.com>
2024-09-16 21:47:00 +0800

    add create strategy for crds
    Signed-off-by: shawnh2 <shawnhxh@outlook.com>

--- go.mod
+++ go.mod
@@ -65,6 +65,7 @@ require (
 	github.com/docker/docker v27.2.0+incompatible
 	github.com/replicatedhq/troubleshoot v0.100.0
 	google.golang.org/grpc v1.66.0
+	k8s.io/apiserver v0.31.0
 )
 
 require (
@@ -84,6 +85,7 @@ require (
 	github.com/Masterminds/squirrel v1.5.4 // indirect
 	github.com/Microsoft/go-winio v0.6.2 // indirect
 	github.com/Microsoft/hcsshim v0.12.5 // indirect
+	github.com/NYTimes/gziphandler v1.1.1 // indirect
 	github.com/antlr4-go/antlr/v4 v4.13.0 // indirect
 	github.com/apparentlymart/go-cidr v1.1.0 // indirect
 	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
@@ -100,6 +102,7 @@ require (
 	github.com/containers/libtrust v0.0.0-20230121012942-c1716e8a8d01 // indirect
 	github.com/containers/ocicrypt v1.2.0 // indirect
 	github.com/containers/storage v1.55.0 // indirect
+	github.com/coreos/go-semver v0.3.1 // indirect
 	github.com/coreos/go-systemd/v22 v22.5.0 // indirect
 	github.com/cyphar/filepath-securejoin v0.3.1 // indirect
 	github.com/distribution/distribution/v3 v3.0.0-beta.1 // indirect
@@ -123,6 +126,7 @@ require (
 	github.com/gorilla/handlers v1.5.2 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/gosuri/uitable v0.0.4 // indirect
+	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/huandu/xstrings v1.4.0 // indirect
@@ -133,6 +137,7 @@ require (
 	github.com/klauspost/compress v1.17.9 // indirect
 	github.com/klauspost/pgzip v1.2.6 // indirect
 	github.com/kortschak/goroutine v1.1.2 // indirect
+	github.com/kylelemons/godebug v1.1.0 // indirect
 	github.com/lann/builder v0.0.0-20180802200727-47ae307949d0 // indirect
 	github.com/lann/ps v0.0.0-20150810152359-62de8c46ede0 // indirect
 	github.com/lib/pq v1.10.9 // indirect
@@ -190,16 +195,22 @@ require (
 	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
 	github.com/xeipuuv/gojsonschema v1.2.0 // indirect
 	github.com/yusufpapurcu/wmi v1.2.4 // indirect
+	go.etcd.io/etcd/api/v3 v3.5.14 // indirect
+	go.etcd.io/etcd/client/pkg/v3 v3.5.14 // indirect
+	go.etcd.io/etcd/client/v3 v3.5.14 // indirect
 	go.opencensus.io v0.24.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.53.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.53.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.28.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.27.0 // indirect
 	golang.org/x/crypto v0.27.0 // indirect
 	golang.org/x/crypto/x509roots/fallback v0.0.0-20240904212608-c9da6b9a4008 // indirect
 	gopkg.in/evanphx/json-patch.v4 v4.12.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
-	k8s.io/apiserver v0.31.0 // indirect
 	k8s.io/metrics v0.31.0 // indirect
 	oras.land/oras-go v1.2.6 // indirect
 	periph.io/x/host/v3 v3.8.2 // indirect
+	sigs.k8s.io/apiserver-network-proxy/konnectivity-client v0.30.3 // indirect
 )
 
 require (
---
+++ internal/gatewayapi/resource/strategy.go
@@ -0,0 +1,197 @@
+// Copyright Envoy Gateway Authors
+// SPDX-License-Identifier: Apache-2.0
+// The full text of the Apache license is available in the LICENSE file at
+// the root of the repo.
+
+package resource
+
+import (
+	"context"
+	"fmt"
+	"sync"
+
+	apiextensionshelpers "k8s.io/apiextensions-apiserver/pkg/apihelpers"
+	apiextensionsinternal "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	"k8s.io/apiextensions-apiserver/pkg/apiserver"
+	structuralschema "k8s.io/apiextensions-apiserver/pkg/apiserver/schema"
+	structuraldefaulting "k8s.io/apiextensions-apiserver/pkg/apiserver/schema/defaulting"
+	apiservervalidation "k8s.io/apiextensions-apiserver/pkg/apiserver/validation"
+	"k8s.io/apiextensions-apiserver/pkg/crdserverscheme"
+	"k8s.io/apiextensions-apiserver/pkg/registry/customresource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apiserver/pkg/registry/rest"
+)
+
+// versionedCreateStrategyMap only stores latest version of CreateStrategy.
+type versionedCreateStrategyMap map[string]rest.RESTCreateStrategy
+
+func (v versionedCreateStrategyMap) Validate(kind string, obj runtime.Object) error {
+	createStrategy, ok := v[kind]
+	if !ok {
+		return nil
+	}
+
+	errList := createStrategy.Validate(context.TODO(), obj)
+	if errList != nil {
+		return errList.ToAggregate()
+	}
+
+	return nil
+}
+
+var (
+	// crdCreateStrategyMap stores CreateStrategy for supported CRDs.
+	// Since it's a lazy loading map, calling getCreateStrategyMapForCRDs
+	// before using this map, directly using this map is not recommended.
+	crdCreateStrategyMap versionedCreateStrategyMap
+	once                 sync.Once
+)
+
+func getCreateStrategyMapForCRDs() versionedCreateStrategyMap {
+	once.Do(func() {
+		// This error should never happen. If it does, stop loading strategy map.
+		crds, _ := loadCRDsFromPath("crds")
+		if len(crds) == 0 {
+			return
+		}
+		crdCreateStrategyMap = make(versionedCreateStrategyMap, len(crds))
+		for _, crd := range crds {
+			vcs, _ := getVersionedCreateStrategyForCRD(crd)
+			crdCreateStrategyMap[crd.Spec.Names.Kind] = vcs
+		}
+	})
+	return crdCreateStrategyMap
+}
+
+// getVersionedCreateStrategyForCRD will only get latest version of the input CRD and
+// generate its CreateStrategy correspondingly.
+//
+// This function contains code copied and modified from kubernetes/apiextensions-apiserver,
+// https://github.com/kubernetes/apiextensions-apiserver
+// from the source file
+// https://github.com/kubernetes/apiextensions-apiserver/blob/master/pkg/apiserver/customresource_handler.go#L605
+// and is provided here subject to the following:
+// Copyright Project Kubernetes Authors
+// SPDX-License-Identifier: Apache-2.0
+//
+// Modifications:
+// - Remove everything except how to derive NewStrategy, and separate them into some new functions.
+func getVersionedCreateStrategyForCRD(crd *apiextensionsv1.CustomResourceDefinition) (rest.RESTCreateStrategy, error) {
+	structuralSchema, err := getStructuralSchema(crd)
+	if err != nil {
+		return nil, err
+	}
+
+	v := crd.Spec.Versions[0]
+	// In addition to Unstructured objects (Custom Resources), we also may sometimes need to
+	// decode unversioned Options objects, so we delegate to parameterScheme for such types.
+	parameterScheme := runtime.NewScheme()
+	parameterScheme.AddUnversionedTypes(schema.GroupVersion{Group: crd.Spec.Group, Version: v.Name},
+		&metav1.ListOptions{},
+		&metav1.GetOptions{},
+		&metav1.DeleteOptions{},
+	)
+
+	kind := schema.GroupVersionKind{Group: crd.Spec.Group, Version: v.Name, Kind: crd.Status.AcceptedNames.Kind}
+	typer := apiserver.UnstructuredObjectTyper{
+		Delegate:          parameterScheme,
+		UnstructuredTyper: crdserverscheme.NewUnstructuredObjectTyper(),
+	}
+
+	validationSchema, err := apiextensionshelpers.GetSchemaForVersion(crd, v.Name)
+	if err != nil {
+		return nil, fmt.Errorf("the server could not properly serve the CR schema")
+	}
+
+	var internalSchemaProps *apiextensionsinternal.JSONSchemaProps
+	var internalValidationSchema *apiextensionsinternal.CustomResourceValidation
+	if validationSchema != nil {
+		internalValidationSchema = &apiextensionsinternal.CustomResourceValidation{}
+		if err = apiextensionsv1.Convert_v1_CustomResourceValidation_To_apiextensions_CustomResourceValidation(validationSchema, internalValidationSchema, nil); err != nil {
+			return nil, fmt.Errorf("failed to convert CRD validation to internal version: %v", err)
+		}
+		internalSchemaProps = internalValidationSchema.OpenAPIV3Schema
+	}
+
+	validator, _, err := apiservervalidation.NewSchemaValidator(internalSchemaProps)
+	if err != nil {
+		return nil, err
+	}
+
+	var statusSpec *apiextensionsinternal.CustomResourceSubresourceStatus
+	var statusValidator apiservervalidation.SchemaValidator
+	subResources, err := apiextensionshelpers.GetSubresourcesForVersion(crd, v.Name)
+	if err != nil {
+		return nil, fmt.Errorf("the server could not properly serve the CR subresources")
+	}
+	if subResources != nil && subResources.Status != nil {
+		statusSpec = &apiextensionsinternal.CustomResourceSubresourceStatus{}
+		if err = apiextensionsv1.Convert_v1_CustomResourceSubresourceStatus_To_apiextensions_CustomResourceSubresourceStatus(subResources.Status, statusSpec, nil); err != nil {
+			return nil, fmt.Errorf("failed converting CRD status subresource to internal version: %v", err)
+		}
+		// for the status subresource, validate only against the status schema
+		if internalValidationSchema != nil && internalValidationSchema.OpenAPIV3Schema != nil && internalValidationSchema.OpenAPIV3Schema.Properties != nil {
+			if statusSchema, ok := internalValidationSchema.OpenAPIV3Schema.Properties["status"]; ok {
+				statusValidator, _, err = apiservervalidation.NewSchemaValidator(&statusSchema)
+				if err != nil {
+					return nil, err
+				}
+			}
+		}
+	}
+
+	var scaleSpec *apiextensionsinternal.CustomResourceSubresourceScale
+	if subResources != nil && subResources.Scale != nil {
+		scaleSpec = &apiextensionsinternal.CustomResourceSubresourceScale{}
+		if err = apiextensionsv1.Convert_v1_CustomResourceSubresourceScale_To_apiextensions_CustomResourceSubresourceScale(subResources.Scale, scaleSpec, nil); err != nil {
+			return nil, fmt.Errorf("failed converting CRD status subresource to internal version: %v", err)
+		}
+	}
+
+	return customresource.NewStrategy(
+		typer,
+		crd.Spec.Scope == apiextensionsv1.NamespaceScoped,
+		kind,
+		validator,
+		statusValidator,
+		structuralSchema,
+		statusSpec,
+		scaleSpec,
+		v.SelectableFields,
+	), nil
+}
+
+func getStructuralSchema(crd *apiextensionsv1.CustomResourceDefinition) (*structuralschema.Structural, error) {
+	v := crd.Spec.Versions[0]
+
+	val, err := apiextensionshelpers.GetSchemaForVersion(crd, v.Name)
+	if err != nil {
+		return nil, fmt.Errorf("the server could not properly serve the CR schema")
+	}
+
+	internalValidation := &apiextensionsinternal.CustomResourceValidation{}
+	if err = apiextensionsv1.Convert_v1_CustomResourceValidation_To_apiextensions_CustomResourceValidation(val, internalValidation, nil); err != nil {
+		return nil, fmt.Errorf("failed converting CRD validation to internal version: %v", err)
+	}
+
+	s, err := structuralschema.NewStructural(internalValidation.OpenAPIV3Schema)
+	if crd.Spec.PreserveUnknownFields == false && err != nil {
+		// This should never happen. If it does, it is a programming error.
+		return nil, fmt.Errorf("the server could not properly serve the CR schema") // validation should avoid this
+	}
+
+	if crd.Spec.PreserveUnknownFields == false {
+		// we don't own s completely, e.g. defaults are not deep-copied. So better make a copy here.
+		s = s.DeepCopy()
+
+		if err = structuraldefaulting.PruneDefaults(s); err != nil {
+			// This should never happen. If it does, it is a programming error.
+			return nil, fmt.Errorf("the server could not properly serve the CR schema") // validation should avoid this
+		}
+	}
+
+	return s, nil
+}
