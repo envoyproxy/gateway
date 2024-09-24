32afbe3db906ff897e18cac209654d966a47bca9
Arko Dasgupta <arkodg@users.noreply.github.com>
2024-09-23 18:26:39 -0700

    [release/v1.1] Bump EnvoyProxy to v1.32.2 (#4294)
    Release Notes: https://github.com/envoyproxy/envoy/releases/tag/v1.31.2
    
    Signed-off-by: Arko Dasgupta <arko@tetrate.io>

--- api/v1alpha1/shared_types.go
+++ api/v1alpha1/shared_types.go
@@ -21,7 +21,7 @@ const (
 	// DefaultDeploymentMemoryResourceRequests for deployment memory resource
 	DefaultDeploymentMemoryResourceRequests = "512Mi"
 	// DefaultEnvoyProxyImage is the default image used by envoyproxy
-	DefaultEnvoyProxyImage = "envoyproxy/envoy:distroless-v1.31.0"
+	DefaultEnvoyProxyImage = "envoyproxy/envoy:distroless-v1.31.2"
 	// DefaultShutdownManagerCPUResourceRequests for shutdown manager cpu resource
 	DefaultShutdownManagerCPUResourceRequests = "10m"
 	// DefaultShutdownManagerMemoryResourceRequests for shutdown manager memory resource
