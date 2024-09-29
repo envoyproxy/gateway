b91f296667badbf45f2fc6d7a342bde1f96ae082
sh2 <shawnhxh@outlook.com>
2024-09-29 12:08:14 +0800

    feat: enable local validations for egctl translate and file provider (#4257)
    * add crds into gatewayapi resources embed
    
    Signed-off-by: shawnh2 <shawnhxh@outlook.com>
    
    * add create strategy for crds
    
    Signed-off-by: shawnh2 <shawnhxh@outlook.com>
    
    * add validations for kubernetesYAMLToResources and kind support for type-meta
    
    Signed-off-by: shawnh2 <shawnhxh@outlook.com>
    
    * fix lint
    
    Signed-off-by: shawnh2 <shawnhxh@outlook.com>
    
    * add unit test for create strategy map
    
    Signed-off-by: shawnh2 <shawnhxh@outlook.com>
    
    * fix cannot load strings full of comments
    
    Signed-off-by: shawnh2 <shawnhxh@outlook.com>
    
    * load crds from literal string instead of embed file
    
    Signed-off-by: shawnh2 <shawnhxh@outlook.com>
    
    * add egctl x validate command
    
    Signed-off-by: shawnh2 <shawnhxh@outlook.com>
    
    * use kubectl-validate as local validator and gateway-crds as embed string
    
    Signed-off-by: shawnh2 <shawnhxh@outlook.com>
    
    * fix gen-check
    
    Signed-off-by: shawnh2 <shawnhxh@outlook.com>
    
    * fix lint
    
    Signed-off-by: shawnh2 <shawnhxh@outlook.com>
    
    * fix ci and add some tests
    
    Signed-off-by: shawnh2 <shawnhxh@outlook.com>
    
    * rm copy of gateway_crds.yaml
    
    Signed-off-by: shawnh2 <shawnhxh@outlook.com>
    
    * fix lint
    
    Signed-off-by: shawnh2 <shawnhxh@outlook.com>
    
    ---------
    
    Signed-off-by: shawnh2 <shawnhxh@outlook.com>
    Co-authored-by: Xunzhuo <bitliu@tencent.com>

---
+++ internal/cmd/egctl/validate_test.go
@@ -0,0 +1,94 @@
+// Copyright Envoy Gateway Authors
+// SPDX-License-Identifier: Apache-2.0
+// The full text of the Apache license is available in the LICENSE file at
+// the root of the repo.
+
+package egctl
+
+import (
+	"bytes"
+	"context"
+	"io"
+	"path"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestRunValidate(t *testing.T) {
+	testCases := []struct {
+		name   string
+		output string
+	}{
+		{
+			name: "invalid-resources",
+			output: `apiVersion: gateway.networking.k8s.io/v1
+kind: Gateway
+metadata:
+  name: eg1
+  namespace: default
+spec:
+...
+local validation error: Gateway.gateway.networking.k8s.io "eg1" is invalid: spec.listeners[0].port: Invalid value: 88888: spec.listeners[0].port in body should be less than or equal to 65535
+
+apiVersion: gateway.networking.k8s.io/v1
+kind: Gateway
+metadata:
+  name: eg2
+  namespace: default
+spec:
+...
+local validation error: Gateway.gateway.networking.k8s.io "eg2" is invalid: [spec.listeners[1]: Duplicate value: map[string]interface {}{"name":"tcp"}, spec.listeners: Invalid value: "array": Listener name must be unique within the Gateway, spec.listeners: Invalid value: "array": Combination of port, protocol and hostname must be unique for each listener]
+
+apiVersion: gateway.networking.k8s.io/v1
+kind: HTTPRoute
+metadata:
+  name: backend
+  namespace: default
+spec:
+...
+local validation error: HTTPRoute.gateway.networking.k8s.io "backend" is invalid: spec.hostnames[0]: Invalid value: ".;'.';[]": spec.hostnames[0] in body should match '^(\*\.)?[a-z0-9]([-a-z0-9]*[a-z0-9])?(\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*$'
+
+apiVersion: gateway.envoyproxy.io/v1alpha1
+kind: Backend
+metadata:
+  name: backend-1
+  namespace: default
+spec:
+...
+local validation error: Backend.gateway.envoyproxy.io "backend-1" is invalid: spec.endpoints[0].ip.address: Invalid value: "a.b.c.d": spec.endpoints[0].ip.address in body should match '^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$'
+
+apiVersion: gateway.envoyproxy.io/v1alpha1
+kind: Backend
+metadata:
+  name: backend-2
+  namespace: default
+spec:
+...
+local validation error: Backend.gateway.envoyproxy.io "backend-2" is invalid: spec.endpoints: Invalid value: "array": fqdn addresses cannot be mixed with other address types
+
+`,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			b := bytes.NewBufferString("")
+			root := newValidateCommand()
+			root.SetOut(b)
+			root.SetErr(b)
+			args := []string{
+				"--file",
+				path.Join("testdata", "validate", tc.name+".yaml"),
+			}
+
+			root.SetArgs(args)
+			err := root.ExecuteContext(context.Background())
+			require.NoError(t, err)
+
+			out, err := io.ReadAll(b)
+			require.NoError(t, err)
+			require.Equal(t, tc.output, string(out))
+		})
+	}
+}
