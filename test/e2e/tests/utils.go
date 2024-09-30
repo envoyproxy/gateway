f707eaded80e17dbad341209ff336201732e96b3
zirain <zirain2009@gmail.com>
2024-09-30 11:09:33 +0800

    chore: correct eg namespace
    Signed-off-by: zirain <zirain2009@gmail.com>

--- test/e2e/tests/utils.go
+++ test/e2e/tests/utils.go
@@ -686,7 +686,7 @@ func createTagsQueryParam(tags map[string]string) (string, error) {
 // CollectAndDump collects and dumps the cluster data for troubleshooting and log.
 // This function should be call within t.Cleanup.
 func CollectAndDump(t *testing.T, rest *rest.Config) {
-	result := tb.CollectResult(context.TODO(), rest, "", "envoy-gateway")
+	result := tb.CollectResult(context.TODO(), rest, "", "envoy-gateway-system")
 	for r, data := range result {
 		tlog.Logf(t, "filename: %s", r)
 		tlog.Logf(t, "data: \n%s", data)
