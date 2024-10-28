b2fc02b57d5574cc3c44c2d3dbff998d9e8e6ca0
Huabing Zhao <zhaohuabing@gmail.com>
2024-10-28 09:07:56 +0000

    remove unnecessary comments
    Signed-off-by: Huabing Zhao <zhaohuabing@gmail.com>

--- internal/provider/kubernetes/controller.go
+++ internal/provider/kubernetes/controller.go
@@ -631,7 +631,7 @@ func (r *gatewayAPIReconciler) processSecretRef(
 			}
 		}
 	}
-	resourceMap.allAssociatedNamespaces.Insert(secretNS) // TODO Zhaohuabing do we need this line?
+	resourceMap.allAssociatedNamespaces.Insert(secretNS)
 	key := utils.NamespacedName(secret).String()
 	if !resourceMap.allAssociatedSecrets.Has(key) {
 		resourceMap.allAssociatedSecrets.Insert(key)
@@ -740,7 +740,7 @@ func (r *gatewayAPIReconciler) processConfigMapRef(
 			}
 		}
 	}
-	resourceMap.allAssociatedNamespaces.Insert(configMapNS) // TODO Zhaohuabing do we need this line?
+	resourceMap.allAssociatedNamespaces.Insert(configMapNS)
 	if !resourceMap.allAssociatedConfigMaps.Has(utils.NamespacedName(configMap).String()) {
 		resourceMap.allAssociatedConfigMaps.Insert(utils.NamespacedName(configMap).String())
 		resourceTree.ConfigMaps = append(resourceTree.ConfigMaps, configMap)
