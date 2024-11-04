cc7104891d49d1c89b15f4b712ef055dbf4d65c9
Arko Dasgupta <arkodg@users.noreply.github.com>
2024-11-04 15:45:57 -0800

    add docker.io registry name in image name (#4628)
    * add docker.io registry name in image name
    
    Fixes: https://github.com/envoyproxy/gateway/issues/4626
    
    Signed-off-by: Arko Dasgupta <arko@tetrate.io>
    
    * add api file
    
    Signed-off-by: Arko Dasgupta <arko@tetrate.io>
    
    ---------
    
    Signed-off-by: Arko Dasgupta <arko@tetrate.io>

--- api/v1alpha1/shared_types.go
+++ api/v1alpha1/shared_types.go
@@ -22,15 +22,15 @@ const (
 	// DefaultDeploymentMemoryResourceRequests for deployment memory resource
 	DefaultDeploymentMemoryResourceRequests = "512Mi"
 	// DefaultEnvoyProxyImage is the default image used by envoyproxy
-	DefaultEnvoyProxyImage = "envoyproxy/envoy:distroless-dev"
+	DefaultEnvoyProxyImage = "docker.io/envoyproxy/envoy:distroless-dev"
 	// DefaultShutdownManagerCPUResourceRequests for shutdown manager cpu resource
 	DefaultShutdownManagerCPUResourceRequests = "10m"
 	// DefaultShutdownManagerMemoryResourceRequests for shutdown manager memory resource
 	DefaultShutdownManagerMemoryResourceRequests = "32Mi"
 	// DefaultShutdownManagerImage is the default image used for the shutdown manager.
-	DefaultShutdownManagerImage = "envoyproxy/gateway-dev:latest"
+	DefaultShutdownManagerImage = "docker.io/envoyproxy/gateway-dev:latest"
 	// DefaultRateLimitImage is the default image used by ratelimit.
-	DefaultRateLimitImage = "envoyproxy/ratelimit:master"
+	DefaultRateLimitImage = "docker.io/envoyproxy/ratelimit:master"
 	// HTTPProtocol is the common-used http protocol.
 	HTTPProtocol = "http"
 	// GRPCProtocol is the common-used grpc protocol.
