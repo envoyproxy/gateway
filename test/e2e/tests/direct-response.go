24581b4b61d3461bcb58a2de79864b7641aaac29
Arko Dasgupta <arkodg@users.noreply.github.com>
2024-11-03 20:20:50 -0800

    direct response docs and tests (#4583)
    * tests: direct response
    
    Signed-off-by: Arko Dasgupta <arko@tetrate.io>
    
    * unit tests
    
    Signed-off-by: Arko Dasgupta <arko@tetrate.io>
    
    * fix ns
    
    Signed-off-by: Arko Dasgupta <arko@tetrate.io>
    
    * docs for direct response
    
    Signed-off-by: Arko Dasgupta <arko@tetrate.io>
    
    * negative tests
    
    Signed-off-by: Arko Dasgupta <arko@tetrate.io>
    (cherry picked from commit f384a64f4d2d6f0fbee9c698fd4b32cd71a64108)
    Signed-off-by: Huabing Zhao <zhaohuabing@gmail.com>

---
+++ test/e2e/tests/direct-response.go
@@ -0,0 +1,38 @@
+// Copyright Envoy Gateway Authors
+// SPDX-License-Identifier: Apache-2.0
+// The full text of the Apache license is available in the LICENSE file at
+// the root of the repo.
+
+//go:build e2e
+
+package tests
+
+import (
+	"testing"
+
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/gateway-api/conformance/utils/kubernetes"
+	"sigs.k8s.io/gateway-api/conformance/utils/suite"
+)
+
+func init() {
+	ConformanceTests = append(ConformanceTests, DirectResponseTest)
+}
+
+var DirectResponseTest = suite.ConformanceTest{
+	ShortName:   "DirectResponse",
+	Description: "Direct",
+	Manifests:   []string{"testdata/direct-response.yaml"},
+	Test: func(t *testing.T, suite *suite.ConformanceTestSuite) {
+		t.Run("direct response", func(t *testing.T) {
+			ns := "gateway-conformance-infra"
+			routeNN := types.NamespacedName{Name: "direct-response", Namespace: ns}
+			gwNN := types.NamespacedName{Name: "same-namespace", Namespace: ns}
+			gwAddr := kubernetes.GatewayAndHTTPRoutesMustBeAccepted(t, suite.Client, suite.TimeoutConfig, suite.ControllerName, kubernetes.NewGatewayRef(gwNN), routeNN)
+
+			kubernetes.HTTPRouteMustHaveResolvedRefsConditionsTrue(t, suite.Client, suite.TimeoutConfig, routeNN, gwNN)
+			verifyCustomResponse(t, suite.TimeoutConfig, gwAddr, "/inline", "text/plain", "Oops! Your request is not found.")
+			verifyCustomResponse(t, suite.TimeoutConfig, gwAddr, "/value-ref", "application/json", `{"error": "Internal Server Error"}`)
+		})
+	},
+}
