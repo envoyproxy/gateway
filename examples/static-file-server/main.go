0d046031526b945f91a8c91422751e2af0771135
zirain <zirain2009@gmail.com>
2024-11-14 13:16:14 +0800

    ci: use static file server instead of github.com
    Signed-off-by: zirain <zirain2009@gmail.com>

---
+++ examples/static-file-server/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"os"
+	"path"
+)
+
+var (
+	port      string
+	directory string
+	certPath  string
+)
+
+func main() {
+	flag.StringVar(&port, "port", "8080", "port to serve on")
+	flag.StringVar(&directory, "dir", "./files", "the directory of static file to host")
+	flag.StringVar(&certPath, "certPath", "/etc/certs", "path to extProcServer certificate and private key")
+	flag.Parse()
+
+	http.Handle("/", http.FileServer(http.Dir(directory)))
+
+	if _, err := os.Stat(path.Join(certPath, "tls.crt")); err != nil {
+		log.Printf("Serving %s on HTTP port: %s\n", directory, port)
+		log.Fatal(http.ListenAndServe(":"+port, nil))
+		return
+	}
+
+	log.Printf("Serving %s on HTTPS port: %s\n", directory, port)
+	log.Fatal(http.ListenAndServeTLS(":"+port,
+		path.Join(certPath, "tls.crt"), path.Join(certPath, "tls.key"), nil))
+}
