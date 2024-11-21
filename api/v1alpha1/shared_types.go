8157937959f2a1112ffc2b10366a8be516f17d0f
Arko Dasgupta <arkodg@users.noreply.github.com>
2024-11-20 20:06:14 -0800

    [release/v1.2] Bump ratelimit image 49af5cca (#4749)
    * [release/v1.2] Bump ratelimit image 49af5cca
    
    Fixes the below CVEs
    
    * https://nvd.nist.gov/vuln/detail/CVE-2024-34158
    * https://nvd.nist.gov/vuln/detail/CVE-2024-34156
    * https://nvd.nist.gov/vuln/detail/CVE-2024-34155
    
    Signed-off-by: Arko Dasgupta <arko@tetrate.io>
    
    * also charts
    
    Signed-off-by: Arko Dasgupta <arko@tetrate.io>
    
    * fix docs
    
    Signed-off-by: Arko Dasgupta <arko@tetrate.io>
    
    ---------
    
    Signed-off-by: Arko Dasgupta <arko@tetrate.io>

--- api/v1alpha1/shared_types.go
+++ api/v1alpha1/shared_types.go
@@ -30,7 +30,7 @@ const (
 	// DefaultShutdownManagerImage is the default image used for the shutdown manager.
 	DefaultShutdownManagerImage = "docker.io/envoyproxy/gateway-dev:latest"
 	// DefaultRateLimitImage is the default image used by ratelimit.
-	DefaultRateLimitImage = "envoyproxy/ratelimit:28b1629a"
+	DefaultRateLimitImage = "docker.io/envoyproxy/ratelimit:49af5cca"
 	// HTTPProtocol is the common-used http protocol.
 	HTTPProtocol = "http"
 	// GRPCProtocol is the common-used grpc protocol.
