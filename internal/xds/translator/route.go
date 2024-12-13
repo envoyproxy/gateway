envoyproxy/gateway

commit 60888c00c73d4c40435da90b9cf16ce9d900c8af

feat: support BackendRef HTTP filters (#3246)
* feat: support for BackendRef HTTP filters

Signed-off-by: Karol Szwaj <karol.szwaj@gmail.com>

* add xds configuration

Signed-off-by: Karol Szwaj <karol.szwaj@gmail.com>

* gofumpt

Signed-off-by: Karol Szwaj <karol.szwaj@gmail.com>

* enable backendRef filters conformance tests

Signed-off-by: Karol Szwaj <karol.szwaj@gmail.com>

* build multiple weighted clusters

Signed-off-by: Karol Szwaj <karol.szwaj@gmail.com>

* fix lint

Signed-off-by: Karol Szwaj <karol.szwaj@gmail.com>

* refactor translator

Signed-off-by: Karol Szwaj <karol.szwaj@gmail.com>

* add weight from setting

Signed-off-by: Karol Szwaj <karol.szwaj@gmail.com>

* restore backend weights to set only when invalid

Signed-off-by: Karol Szwaj <karol.szwaj@gmail.com>

* fix filters func

Signed-off-by: Karol Szwaj <karol.szwaj@gmail.com>

* validate other filters

Signed-off-by: Karol Szwaj <karol.szwaj@gmail.com>

* refactor code

Signed-off-by: Karol Szwaj <karol.szwaj@gmail.com>

---------

Signed-off-by: Karol Szwaj <karol.szwaj@gmail.com>

internal/xds/translator/route.go
@@ -71,7 +71,7 @@ func buildXdsRoute(httpRoute *ir.HTTPRoute) (*routev3.Route, error) {
 		router.Action = &routev3.Route_Route{Route: routeAction}
 	default:
 		backendWeights := httpRoute.Destination.ToBackendWeights()
-		routeAction := buildXdsRouteAction(backendWeights)
+		routeAction := buildXdsRouteAction(backendWeights, httpRoute.Destination.Settings)
 		routeAction.IdleTimeout = idleTimeout(httpRoute)
 
 		if httpRoute.Mirrors != nil {
@@ -222,33 +222,62 @@ func buildXdsStringMatcher(irMatch *ir.StringMatch) *matcherv3.StringMatcher {
 	return stringMatcher
 }
 
-func buildXdsRouteAction(backendWeights *ir.BackendWeights) *routev3.RouteAction {
+func buildXdsRouteAction(backendWeights *ir.BackendWeights, settings []*ir.DestinationSetting) *routev3.RouteAction {
 	// only use weighted cluster when there are invalid weights
-	if backendWeights.Invalid == 0 {
+	if hasFiltersInSettings(settings) || backendWeights.Invalid != 0 {
+		return buildXdsWeightedRouteAction(backendWeights, settings)
+	}
+
+	return &routev3.RouteAction{
+		ClusterSpecifier: &routev3.RouteAction_Cluster{
+			Cluster: backendWeights.Name,
+		},
+	}
+}
+
+func buildXdsWeightedRouteAction(backendWeights *ir.BackendWeights, settings []*ir.DestinationSetting) *routev3.RouteAction {
+	weightedClusters := []*routev3.WeightedCluster_ClusterWeight{}
+	if backendWeights.Invalid > 0 {
+		invalidCluster := &routev3.WeightedCluster_ClusterWeight{
+			Name:   "invalid-backend-cluster",
+			Weight: &wrapperspb.UInt32Value{Value: backendWeights.Invalid},
+		}
+		weightedClusters = append(weightedClusters, invalidCluster)
 		return &routev3.RouteAction{
-			ClusterSpecifier: &routev3.RouteAction_Cluster{
-				Cluster: backendWeights.Name,
+			// Intentionally route to a non-existent cluster and return a 500 error when it is not found
+			ClusterNotFoundResponseCode: routev3.RouteAction_INTERNAL_SERVER_ERROR,
+			ClusterSpecifier: &routev3.RouteAction_WeightedClusters{
+				WeightedClusters: &routev3.WeightedCluster{
+					Clusters: weightedClusters,
+				},
 			},
 		}
 	}
 
-	return buildXdsWeightedRouteAction(backendWeights)
-}
+	for _, destinationSetting := range settings {
+		if destinationSetting.Filters != nil {
+			validCluster := &routev3.WeightedCluster_ClusterWeight{
+				Name:   backendWeights.Name,
+				Weight: &wrapperspb.UInt32Value{Value: *destinationSetting.Weight},
+			}
 
-func buildXdsWeightedRouteAction(backendWeights *ir.BackendWeights) *routev3.RouteAction {
-	clusters := []*routev3.WeightedCluster_ClusterWeight{
-		{
-			Name:   "invalid-backend-cluster",
-			Weight: &wrapperspb.UInt32Value{Value: backendWeights.Invalid},
-		},
-	}
+			if len(destinationSetting.Filters.AddRequestHeaders) > 0 {
+				validCluster.RequestHeadersToAdd = append(validCluster.RequestHeadersToAdd, buildXdsAddedHeaders(destinationSetting.Filters.AddRequestHeaders)...)
+			}
 
-	if backendWeights.Valid > 0 {
-		validCluster := &routev3.WeightedCluster_ClusterWeight{
-			Name:   backendWeights.Name,
-			Weight: &wrapperspb.UInt32Value{Value: backendWeights.Valid},
+			if len(destinationSetting.Filters.RemoveRequestHeaders) > 0 {
+				validCluster.RequestHeadersToRemove = append(validCluster.RequestHeadersToRemove, destinationSetting.Filters.RemoveRequestHeaders...)
+			}
+
+			if len(destinationSetting.Filters.AddResponseHeaders) > 0 {
+				validCluster.ResponseHeadersToAdd = append(validCluster.ResponseHeadersToAdd, buildXdsAddedHeaders(destinationSetting.Filters.AddResponseHeaders)...)
+			}
+
+			if len(destinationSetting.Filters.RemoveResponseHeaders) > 0 {
+				validCluster.ResponseHeadersToRemove = append(validCluster.ResponseHeadersToRemove, destinationSetting.Filters.RemoveResponseHeaders...)
+			}
+			weightedClusters = append(weightedClusters, validCluster)
 		}
-		clusters = append(clusters, validCluster)
 	}
 
 	return &routev3.RouteAction{
@@ -256,7 +285,7 @@ func buildXdsWeightedRouteAction(backendWeights *ir.BackendWeights) *routev3.Rou
 		ClusterNotFoundResponseCode: routev3.RouteAction_INTERNAL_SERVER_ERROR,
 		ClusterSpecifier: &routev3.RouteAction_WeightedClusters{
 			WeightedClusters: &routev3.WeightedCluster{
-				Clusters: clusters,
+				Clusters: weightedClusters,
 			},
 		},
 	}
@@ -587,4 +616,14 @@ func buildRetryOn(triggers []ir.TriggerEnum) (string, error) {
 	}
 
 	return b.String(), nil
+}
+
+func hasFiltersInSettings(settings []*ir.DestinationSetting) bool {
+	for _, setting := range settings {
+		filters := setting.Filters
+		if filters != nil {
+			return true
+		}
+	}
+	return false
 }

internal/xds/translator/testdata/out/xds-ir/http-route-weighted-invalid-backend.routes.yaml
@@ -16,5 +16,3 @@
           clusters:
           - name: invalid-backend-cluster
             weight: 1
-          - name: first-route-dest
-            weight: 1

internal/gatewayapi/route.go
@@ -1225,15 +1225,69 @@ func (t *Translator) processDestination(backendRefContext BackendRefContext,
 			gwapiv1a2.RouteReasonResolvedRefs,
 			"Mixed endpointslice address type for the same backendRef is not supported")
 	}
-
+	destinationFilters := t.processDestinationFilters(routeType, backendRefContext, parentRef, route, resources)
 	ds = &ir.DestinationSetting{
 		Weight:      &weight,
 		Protocol:    protocol,
 		Endpoints:   endpoints,
 		AddressType: addrType,
 		TLS:         backendTLS,
+		Filters:     destinationFilters,
 	}
+
 	return ds
+}
+
+func getBackendFilters(routeType gwapiv1.Kind, backendRefContext BackendRefContext) (backendFilters any) {
+	filters := GetFilters(backendRefContext)
+	switch routeType {
+	case KindHTTPRoute:
+		if len(filters.([]gwapiv1.HTTPRouteFilter)) > 0 {
+			return filters.([]gwapiv1.HTTPRouteFilter)
+		}
+	case KindGRPCRoute:
+		if len(filters.([]gwapiv1.GRPCRouteFilter)) > 0 {
+			return filters.([]gwapiv1.GRPCRouteFilter)
+		}
+	}
+
+	return nil
+}
+
+func (t *Translator) processDestinationFilters(routeType gwapiv1.Kind, backendRefContext BackendRefContext, parentRef *RouteParentContext, route RouteContext, resources *Resources) *ir.DestinationFilters {
+	backendFilters := getBackendFilters(routeType, backendRefContext)
+	if backendFilters == nil {
+		return nil
+	}
+
+	var httpFiltersContext *HTTPFiltersContext
+	var destFilters ir.DestinationFilters
+
+	switch filters := backendFilters.(type) {
+	case []gwapiv1.HTTPRouteFilter:
+		httpFiltersContext = t.ProcessHTTPFilters(parentRef, route, filters, 0, resources)
+
+	case []gwapiv1.GRPCRouteFilter:
+		httpFiltersContext = t.ProcessGRPCFilters(parentRef, route, filters, resources)
+	}
+	applyHTTPFiltersContextToDestinationFilters(httpFiltersContext, &destFilters)
+
+	return &destFilters
+}
+
+func applyHTTPFiltersContextToDestinationFilters(httpFiltersContext *HTTPFiltersContext, destFilters *ir.DestinationFilters) {
+	if len(httpFiltersContext.AddRequestHeaders) > 0 {
+		destFilters.AddRequestHeaders = httpFiltersContext.AddRequestHeaders
+	}
+	if len(httpFiltersContext.RemoveRequestHeaders) > 0 {
+		destFilters.RemoveRequestHeaders = httpFiltersContext.RemoveRequestHeaders
+	}
+	if len(httpFiltersContext.AddResponseHeaders) > 0 {
+		destFilters.AddResponseHeaders = httpFiltersContext.AddResponseHeaders
+	}
+	if len(httpFiltersContext.RemoveResponseHeaders) > 0 {
+		destFilters.RemoveResponseHeaders = httpFiltersContext.RemoveResponseHeaders
+	}
 }
 
 func inspectAppProtocolByRouteKind(kind gwapiv1.Kind) ir.AppProtocol {

internal/gatewayapi/conformance/features.go
@@ -15,7 +15,7 @@ import (
 // SkipTests is a list of tests that are skipped in the conformance suite.
 var SkipTests = []suite.ConformanceTest{
 	tests.GatewayStaticAddresses,
-	tests.GatewayHTTPListenerIsolation,          // https://github.com/kubernetes-sigs/gateway-api/issues/3049
+	tests.GatewayHTTPListenerIsolation,          // https://github.com/kubernetes-sigs/gateway-api/issues/3352
 	tests.HTTPRouteBackendRequestHeaderModifier, // https://github.com/envoyproxy/gateway/issues/3338
 }
 

internal/gatewayapi/contexts.go
@@ -368,5 +368,9 @@ func GetBackendRef(b BackendRefContext) *gwapiv1.BackendRef {
 }
 
 func GetFilters(b BackendRefContext) any {
-	return reflect.ValueOf(b).FieldByName("Filters").Interface()
+	filters := reflect.ValueOf(b).FieldByName("Filters")
+	if !filters.IsValid() {
+		return nil
+	}
+	return filters.Interface()
 }

internal/gatewayapi/testdata/httproute-with-backendref-add-multiple-filters.in.yaml
@@ -1,0 +1,57 @@
+gateways:
+  - apiVersion: gateway.networking.k8s.io/v1
+    kind: Gateway
+    metadata:
+      namespace: envoy-gateway
+      name: gateway-1
+    spec:
+      gatewayClassName: envoy-gateway-class
+      listeners:
+        - name: http
+          protocol: HTTP
+          port: 80
+          allowedRoutes:
+            namespaces:
+              from: All
+httpRoutes:
+  - apiVersion: gateway.networking.k8s.io/v1
+    kind: HTTPRoute
+    metadata:
+      namespace: default
+      name: httproute-1
+    spec:
+      parentRefs:
+        - namespace: envoy-gateway
+          name: gateway-1
+      rules:
+        - matches:
+            - path:
+                value: "/"
+          backendRefs:
+            - name: service-1
+              port: 8080
+              weight: 8
+              filters:
+                - type: RequestHeaderModifier
+                  requestHeaderModifier:
+                    add:
+                      - name: "add-header-1"
+                        value: "some-value"
+                      - name: "add-header-2"
+                        value: "some-value"
+            - name: service-2
+              port: 8080
+              weight: 2
+        - matches:
+            - path:
+                value: "/foo"
+          backendRefs:
+            - name: service-3
+              port: 8080
+              weight: 1
+              filters:
+                - type: RequestHeaderModifier
+                  requestHeaderModifier:
+                    set:
+                      - name: "add-header-3"
+                        value: "some-value"

internal/gatewayapi/testdata/httproute-with-backendref-add-multiple-filters.out.yaml
@@ -53,17 +53,35 @@ httpRoutes:
     rules:
     - backendRefs:
       - filters:
-        - requestRedirect:
-            hostname: redirected.com
-            scheme: https
-            statusCode: 301
-          type: RequestRedirect
+        - requestHeaderModifier:
+            add:
+            - name: add-header-1
+              value: some-value
+            - name: add-header-2
+              value: some-value
+          type: RequestHeaderModifier
         name: service-1
         port: 8080
+        weight: 8
+      - name: service-2
+        port: 8080
+        weight: 2
       matches:
       - path:
-          type: Exact
-          value: /exact
+          value: /
+    - backendRefs:
+      - filters:
+        - requestHeaderModifier:
+            set:
+            - name: add-header-3
+              value: some-value
+          type: RequestHeaderModifier
+        name: service-3
+        port: 8080
+        weight: 1
+      matches:
+      - path:
+          value: /foo
   status:
     parents:
     - conditions:
@@ -73,10 +91,9 @@ httpRoutes:
         status: "True"
         type: Accepted
       - lastTransitionTime: null
-        message: Specific filter is not supported within BackendRef, only RequestHeaderModifier
-          and ResponseHeaderModifier are supported
-        reason: UnsupportedRefValue
-        status: "False"
+        message: Resolved all the Object references for the Route
+        reason: ResolvedRefs
+        status: "True"
         type: ResolvedRefs
       controllerName: gateway.envoyproxy.io/gatewayclass-controller
       parentRef:
@@ -115,15 +132,53 @@ xdsIR:
       port: 10080
       routes:
       - destination:
+          name: httproute/default/httproute-1/rule/1
+          settings:
+          - addressType: IP
+            endpoints:
+            - host: 7.7.7.7
+              port: 8080
+            filters:
+              addRequestHeaders:
+              - append: false
+                name: add-header-3
+                value: some-value
+            protocol: HTTP
+            weight: 1
+        hostname: '*'
+        isHTTP2: false
+        name: httproute/default/httproute-1/rule/1/match/0/*
+        pathMatch:
+          distinct: false
+          name: ""
+          prefix: /foo
+      - destination:
           name: httproute/default/httproute-1/rule/0
           settings:
-          - weight: 1
-        directResponse:
-          statusCode: 500
+          - addressType: IP
+            endpoints:
+            - host: 7.7.7.7
+              port: 8080
+            filters:
+              addRequestHeaders:
+              - append: true
+                name: add-header-1
+                value: some-value
+              - append: true
+                name: add-header-2
+                value: some-value
+            protocol: HTTP
+            weight: 8
+          - addressType: IP
+            endpoints:
+            - host: 7.7.7.7
+              port: 8080
+            protocol: HTTP
+            weight: 2
         hostname: '*'
         isHTTP2: false
         name: httproute/default/httproute-1/rule/0/match/0/*
         pathMatch:
           distinct: false
-          exact: /exact
           name: ""
+          prefix: /

internal/gatewayapi/testdata/httproute-with-invalid-backend-ref-unsupported-filter.in.yaml
@@ -32,8 +32,8 @@ httpRoutes:
       - name: service-1
         port: 8080
         filters:
-        - type: ResponseHeaderModifier
-          responseHeaderModifier:
-            set:
-            - name: "set-header-1"
-              value: "some-value"
+        - type: RequestRedirect
+          requestRedirect:
+            scheme: https
+            statusCode: 301
+            hostname: "redirected.com"

commit 368092bbae2eff7690866ff158ed9297c0f23ca7

Fix Weighted Invaid Backend Logic
We were not adding valid clusters where there was a invalid cluster

Regression from https://github.com/envoyproxy/gateway/pull/3246

Signed-off-by: Arko Dasgupta <arko@tetrate.io>

internal/xds/translator/route.go
@@ -244,39 +244,33 @@ func buildXdsWeightedRouteAction(backendWeights *ir.BackendWeights, settings []*
 			Weight: &wrapperspb.UInt32Value{Value: backendWeights.Invalid},
 		}
 		weightedClusters = append(weightedClusters, invalidCluster)
-		return &routev3.RouteAction{
-			// Intentionally route to a non-existent cluster and return a 500 error when it is not found
-			ClusterNotFoundResponseCode: routev3.RouteAction_INTERNAL_SERVER_ERROR,
-			ClusterSpecifier: &routev3.RouteAction_WeightedClusters{
-				WeightedClusters: &routev3.WeightedCluster{
-					Clusters: weightedClusters,
-				},
-			},
-		}
 	}
 
 	for _, destinationSetting := range settings {
-		if destinationSetting.Filters != nil {
+		if len(destinationSetting.Endpoints) > 0 {
 			validCluster := &routev3.WeightedCluster_ClusterWeight{
 				Name:   backendWeights.Name,
 				Weight: &wrapperspb.UInt32Value{Value: *destinationSetting.Weight},
 			}
 
-			if len(destinationSetting.Filters.AddRequestHeaders) > 0 {
-				validCluster.RequestHeadersToAdd = append(validCluster.RequestHeadersToAdd, buildXdsAddedHeaders(destinationSetting.Filters.AddRequestHeaders)...)
+			if destinationSetting.Filters != nil {
+				if len(destinationSetting.Filters.AddRequestHeaders) > 0 {
+					validCluster.RequestHeadersToAdd = append(validCluster.RequestHeadersToAdd, buildXdsAddedHeaders(destinationSetting.Filters.AddRequestHeaders)...)
+				}
+
+				if len(destinationSetting.Filters.RemoveRequestHeaders) > 0 {
+					validCluster.RequestHeadersToRemove = append(validCluster.RequestHeadersToRemove, destinationSetting.Filters.RemoveRequestHeaders...)
+				}
+
+				if len(destinationSetting.Filters.AddResponseHeaders) > 0 {
+					validCluster.ResponseHeadersToAdd = append(validCluster.ResponseHeadersToAdd, buildXdsAddedHeaders(destinationSetting.Filters.AddResponseHeaders)...)
+				}
+
+				if len(destinationSetting.Filters.RemoveResponseHeaders) > 0 {
+					validCluster.ResponseHeadersToRemove = append(validCluster.ResponseHeadersToRemove, destinationSetting.Filters.RemoveResponseHeaders...)
+				}
 			}
 
-			if len(destinationSetting.Filters.RemoveRequestHeaders) > 0 {
-				validCluster.RequestHeadersToRemove = append(validCluster.RequestHeadersToRemove, destinationSetting.Filters.RemoveRequestHeaders...)
-			}
-
-			if len(destinationSetting.Filters.AddResponseHeaders) > 0 {
-				validCluster.ResponseHeadersToAdd = append(validCluster.ResponseHeadersToAdd, buildXdsAddedHeaders(destinationSetting.Filters.AddResponseHeaders)...)
-			}
-
-			if len(destinationSetting.Filters.RemoveResponseHeaders) > 0 {
-				validCluster.ResponseHeadersToRemove = append(validCluster.ResponseHeadersToRemove, destinationSetting.Filters.RemoveResponseHeaders...)
-			}
 			weightedClusters = append(weightedClusters, validCluster)
 		}
 	}

internal/xds/translator/testdata/out/xds-ir/http-route-weighted-invalid-backend.routes.yaml
@@ -16,3 +16,5 @@
           clusters:
           - name: invalid-backend-cluster
             weight: 1
+          - name: first-route-dest
+            weight: 1