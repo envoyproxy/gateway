0ee0a8af8e0a86bfbafbca4cee6d5432afa07830
Daniele Fognini <fogninid@users.noreply.github.com>
2025-01-17 19:49:27 +0000

    feat: continue using and drain serving endpointslices during termination (#4946)
    Signed-off-by: Daniele Fognini <d.github.75db@f12o.eu>
    Signed-off-by: Arko Dasgupta <arkodg@users.noreply.github.com>
    Co-authored-by: Arko Dasgupta <arkodg@users.noreply.github.com>

--- internal/ir/xds.go
+++ internal/ir/xds.go
@@ -1472,6 +1472,8 @@ type DestinationEndpoint struct {
 	Port uint32 `json:"port" yaml:"port"`
 	// Path refers to the Unix Domain Socket
 	Path *string `json:"path,omitempty" yaml:"path,omitempty"`
+	// Draining is true if this endpoint should be drained
+	Draining bool `json:"draining,omitempty" yaml:"draining,omitempty"`
 }
 
 // Validate the fields within the DestinationEndpoint structure
@@ -1503,10 +1505,11 @@ func (d DestinationEndpoint) Validate() error {
 }
 
 // NewDestEndpoint creates a new DestinationEndpoint.
-func NewDestEndpoint(host string, port uint32) *DestinationEndpoint {
+func NewDestEndpoint(host string, port uint32, draining bool) *DestinationEndpoint {
 	return &DestinationEndpoint{
-		Host: host,
-		Port: port,
+		Host:     host,
+		Port:     port,
+		Draining: draining,
 	}
 }
 
