ed103771954d3105124891acd184b23375addb65
Ryan Hristovski <61257223+ryanhristovski@users.noreply.github.com>
2025-02-12 10:19:25 -0500

    Update api/v1alpha1/ratelimit_types.go
    Co-authored-by: Arko Dasgupta <arkodg@users.noreply.github.com>
    Signed-off-by: Ryan Hristovski <61257223+ryanhristovski@users.noreply.github.com>
    Signed-off-by: Ryan Hristovski <ryan.hristovski@docker.com>

--- api/v1alpha1/ratelimit_types.go
+++ api/v1alpha1/ratelimit_types.go
@@ -24,7 +24,7 @@ type RateLimitSpec struct {
 	Local *LocalRateLimit `json:"local,omitempty"`
 
 	// Shared determines whether this rate limit rule applies globally across the gateway.
-	// If set to true, the rule is treated as a common bucket and is shared across all routes under the gateway.
+	// If set to true, the rule is treated as a common bucket and is shared across all policy targets (xRoutes).
 	// Must have targetRef set to Gateway
 	// Default: false.
 	//
