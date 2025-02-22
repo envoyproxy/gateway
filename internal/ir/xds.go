4eefcc553aaeac4bc1526a48ddbeefc14ad11237
Alexander Volchok <alex.volchok@sap.com>
2025-02-22 07:21:23 +0100

    updating
    Signed-off-by: Alexander Volchok <alex.volchok@sap.com>

--- internal/ir/xds.go
+++ internal/ir/xds.go
@@ -1469,12 +1469,9 @@ type DestinationSetting struct {
 	AddressType *DestinationAddressType `json:"addressType,omitempty" yaml:"addressType,omitempty"`
 	// IPFamily specifies the IP family (IPv4 or IPv6) to use for this destination's endpoints.
 	// This is derived from the backend service and endpoint slice information.
-	IPFamily *egv1a1.IPFamily `json:"ipFamily,omitempty" yaml:"ipFamily,omitempty"`
-	// DNSLookupFamily specifies a preferred resolution strategy for ip.
-	// This is derived from the backend service and endpoint slice information.
-	DNSLookupFamily *egv1a1.DNSLookupFamily `json:"DNSLookupFamily,omitempty" yaml:"ipFamily,omitempty"`
-	TLS             *TLSUpstreamConfig      `json:"tls,omitempty" yaml:"tls,omitempty"`
-	Filters         *DestinationFilters     `json:"filters,omitempty" yaml:"filters,omitempty"`
+	IPFamily *egv1a1.IPFamily    `json:"ipFamily,omitempty" yaml:"ipFamily,omitempty"`
+	TLS      *TLSUpstreamConfig  `json:"tls,omitempty" yaml:"tls,omitempty"`
+	Filters  *DestinationFilters `json:"filters,omitempty" yaml:"filters,omitempty"`
 }
 
 // Validate the fields within the DestinationSetting structure
