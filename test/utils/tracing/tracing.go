3e2d35af3a5302d7061eee9de4d1ead18f46c20c
zirain <zirain2009@gmail.com>
2025-04-19 16:54:39 +0800

    lint
    Signed-off-by: zirain <zirain2009@gmail.com>

--- test/utils/tracing/tracing.go
+++ test/utils/tracing/tracing.go
@@ -1,3 +1,8 @@
+// Copyright Envoy Gateway Authors
+// SPDX-License-Identifier: Apache-2.0
+// The full text of the Apache license is available in the LICENSE file at
+// the root of the repo.
+
 package tracing
 
 import (
