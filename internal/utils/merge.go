50913817bc2e4746b2317f676b8ca3e594e17c7a
zirain <zirain2009@gmail.com>
2025-04-23 13:43:28 +0800

    impl mergeType in BackendTrafficPolicy
    Signed-off-by: zirain <zirain2009@gmail.com>

---
+++ internal/utils/merge.go
@@ -0,0 +1,56 @@
+// Copyright Envoy Gateway Authors
+// SPDX-License-Identifier: Apache-2.0
+// The full text of the Apache license is available in the LICENSE file at
+// the root of the repo.
+
+package utils
+
+import (
+	"encoding/json"
+	"fmt"
+
+	jsonpatch "github.com/evanphx/json-patch"
+	"k8s.io/apimachinery/pkg/util/strategicpatch"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	egv1a1 "github.com/envoyproxy/gateway/api/v1alpha1"
+)
+
+func Merge[T client.Object](original T, patch T, mergeType egv1a1.MergeType) (T, error) {
+	var (
+		patchedJSON  []byte
+		originalJSON []byte
+		patchJSON    []byte
+		err          error
+	)
+
+	originalJSON, err = json.Marshal(original)
+	if err != nil {
+		return original, fmt.Errorf("error marshaling original service: %w", err)
+	}
+	patchJSON, err = json.Marshal(patch)
+	if err != nil {
+		return original, fmt.Errorf("error marshaling original service: %w", err)
+	}
+	switch mergeType {
+	case egv1a1.StrategicMerge:
+		patchedJSON, err = strategicpatch.StrategicMergePatch(originalJSON, patchJSON, egv1a1.BackendTrafficPolicy{})
+		if err != nil {
+			return original, fmt.Errorf("error during strategic merge: %w", err)
+		}
+	case egv1a1.JSONMerge:
+		patchedJSON, err = jsonpatch.MergePatch(originalJSON, patchJSON)
+		if err != nil {
+			return original, fmt.Errorf("error during JSON merge: %w", err)
+		}
+	default:
+		return original, fmt.Errorf("unsupported merge type: %s", mergeType)
+	}
+
+	patchedBTP := new(T)
+	if err = json.Unmarshal(patchedJSON, patchedBTP); err != nil {
+		return original, fmt.Errorf("error unmarshaling patched service: %w", err)
+	}
+
+	return *patchedBTP, nil
+}
