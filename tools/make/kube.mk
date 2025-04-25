7ec471651d16880456315b0b90b864625af2784d
Huabing (Robin) Zhao <zhaohuabing@gmail.com>
2025-04-25 08:13:32 +0000

    update version
    Signed-off-by: Huabing (Robin) Zhao <zhaohuabing@gmail.com>

--- tools/make/kube.mk
+++ tools/make/kube.mk
@@ -1,9 +1,9 @@
 # ENVTEST_K8S_VERSION refers to the version of kubebuilder assets to be downloaded by envtest binary.
 # To know the available versions check:
 # - https://github.com/kubernetes-sigs/controller-tools/blob/main/envtest-releases.yaml
-ENVTEST_K8S_VERSION ?= v1.30.11
+ENVTEST_K8S_VERSION ?= v1.30.12
 # Need run cel validation across multiple versions of k8s
-ENVTEST_K8S_VERSIONS ?= v1.30.11, v1.31.8, v1.32.4, v1.33.0
+ENVTEST_K8S_VERSIONS ?= v1.30.12, v1.31.8, v1.32.4, v1.33.0
 
 # GATEWAY_API_VERSION refers to the version of Gateway API CRDs.
 # For more details, see https://gateway-api.sigs.k8s.io/guides/getting-started/#installing-gateway-api
