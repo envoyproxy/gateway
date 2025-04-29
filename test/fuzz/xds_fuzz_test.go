aadd02a37047b0125cb2025a6a56bbe79cc3388c
Sudipto Baral <sudiptobaral.me@gmail.com>
2025-04-29 10:03:03 -0400

    Improve fuzzers to potentially generate more coverage.
    Signed-off-by: sudipto baral <sudiptobaral.me@gmail.com>

--- test/fuzz/xds_fuzz_test.go
+++ test/fuzz/xds_fuzz_test.go
@@ -9,55 +9,37 @@ import (
 	"testing"
 
 	fuzz "github.com/AdaLogics/go-fuzz-headers"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	gwapiv1 "sigs.k8s.io/gateway-api/apis/v1"
-	"sigs.k8s.io/yaml"
-
 	"github.com/envoyproxy/gateway/internal/cmd/egctl"
 	"github.com/envoyproxy/gateway/internal/gatewayapi/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	gwapiv1 "sigs.k8s.io/gateway-api/apis/v1"
 )
 
 func FuzzGatewayAPIToXDS(f *testing.F) {
-	f.Fuzz(func(t *testing.T, data []byte) {
-		fc := fuzz.NewConsumer(data)
-		resources := &resource.Resources{}
-		if err := fc.GenerateStruct(resources); err != nil {
-			return
-		}
-		namespace, err := fc.GetString()
+	f.Fuzz(func(t *testing.T, b []byte) {
+		rs, err := resource.LoadResourcesFromYAMLBytes(b, true)
 		if err != nil {
 			return
 		}
-		dnsDomain, err := fc.GetString()
-		if err != nil {
-			return
-		}
-		resourceType, err := fc.GetString()
-		if err != nil {
-			return
-		}
-
-		// Populate default values
-		yamlBytes, err := yaml.Marshal(resources)
-		if err != nil {
-			return
-		}
-		addMissingResources, err := fc.GetBool()
-		if err != nil {
-			return
-		}
-		rs, err := resource.LoadResourcesFromYAMLBytes(yamlBytes, addMissingResources)
-		if err != nil {
-			return
-		}
+		fc := fuzz.NewConsumer(b)
+		namespace, _ := fc.GetString()
+		dnsDomain, _ := fc.GetString()
+		resourceType, _ := fc.GetString()
 
 		_, _ = egctl.TranslateGatewayAPIToXds(namespace, dnsDomain, resourceType, rs)
 	})
 }
 
 func FuzzGatewayClassToXDS(f *testing.F) {
-	f.Fuzz(func(t *testing.T, name, controllerName, namespace, dnsDomain, resourceType string) {
-		resources := &resource.Resources{
+	f.Fuzz(func(t *testing.T, b []byte) {
+		fc := fuzz.NewConsumer(b)
+		namespace, _ := fc.GetString()
+		name, _ := fc.GetString()
+		controllerName, _ := fc.GetString()
+		dnsDomain, _ := fc.GetString()
+		resourceType, _ := fc.GetString()
+
+		rs := &resource.Resources{
 			GatewayClass: &gwapiv1.GatewayClass{
 				ObjectMeta: metav1.ObjectMeta{
 					Name:      name,
@@ -69,6 +51,6 @@ func FuzzGatewayClassToXDS(f *testing.F) {
 			},
 		}
 
-		_, _ = egctl.TranslateGatewayAPIToXds(namespace, dnsDomain, resourceType, resources)
+		_, _ = egctl.TranslateGatewayAPIToXds(namespace, dnsDomain, resourceType, rs)
 	})
 }
