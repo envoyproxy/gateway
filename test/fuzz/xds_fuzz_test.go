474b7a7f712dea1fdb68fdf1a9da42035af4d017
Sudipto Baral <sudiptobaral.me@gmail.com>
2025-05-02 13:26:29 -0400

    Add seed corpus to guide the fuzzer to generate combinations of gateway resources.
    Signed-off-by: sudipto baral <sudiptobaral.me@gmail.com>

--- test/fuzz/xds_fuzz_test.go
+++ test/fuzz/xds_fuzz_test.go
@@ -9,9 +9,6 @@ import (
 	"testing"
 
 	fuzz "github.com/AdaLogics/go-fuzz-headers"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	gwapiv1 "sigs.k8s.io/gateway-api/apis/v1"
-
 	"github.com/envoyproxy/gateway/internal/cmd/egctl"
 	"github.com/envoyproxy/gateway/internal/gatewayapi/resource"
 )
@@ -26,31 +23,6 @@ func FuzzGatewayAPIToXDS(f *testing.F) {
 		namespace, _ := fc.GetString()
 		dnsDomain, _ := fc.GetString()
 		resourceType, _ := fc.GetString()
-
-		_, _ = egctl.TranslateGatewayAPIToXds(namespace, dnsDomain, resourceType, rs)
-	})
-}
-
-func FuzzGatewayClassToXDS(f *testing.F) {
-	f.Fuzz(func(t *testing.T, b []byte) {
-		fc := fuzz.NewConsumer(b)
-		namespace, _ := fc.GetString()
-		name, _ := fc.GetString()
-		controllerName, _ := fc.GetString()
-		dnsDomain, _ := fc.GetString()
-		resourceType, _ := fc.GetString()
-
-		rs := &resource.Resources{
-			GatewayClass: &gwapiv1.GatewayClass{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      name,
-					Namespace: namespace,
-				},
-				Spec: gwapiv1.GatewayClassSpec{
-					ControllerName: gwapiv1.GatewayController(controllerName),
-				},
-			},
-		}
 
 		_, _ = egctl.TranslateGatewayAPIToXds(namespace, dnsDomain, resourceType, rs)
 	})
