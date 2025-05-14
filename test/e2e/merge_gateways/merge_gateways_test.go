aaee30cd8cd5c3bfb633b547f97e92afe4e4dbd9
Karol Szwaj <karol.szwaj@gmail.com>
2025-05-14 22:04:08 +0200

    skip merge gateways test
    Signed-off-by: Karol Szwaj <karol.szwaj@gmail.com>

--- test/e2e/merge_gateways/merge_gateways_test.go
+++ test/e2e/merge_gateways/merge_gateways_test.go
@@ -37,6 +37,12 @@ func TestMergeGateways(t *testing.T) {
 			*flags.GatewayClassName, *flags.CleanupBaseResources, *flags.ShowDebug)
 	}
 
+	var skipTests []string
+	if tests.IsGatewayNamespaceMode() {
+		// Skip MergeGateways test because it is not supported in GatewayNamespaceMode
+		skipTests = append(skipTests, tests.MergeGatewaysTest.ShortName)
+	}
+
 	cSuite, err := suite.NewConformanceTestSuite(suite.ConformanceOptions{
 		Client:               c,
 		RestConfig:           cfg,
@@ -46,8 +52,8 @@ func TestMergeGateways(t *testing.T) {
 		RunTest:              *flags.RunTest,
 		// SupportedFeatures cannot be empty, so we set it to SupportGateway
 		// All e2e tests should leave Features empty.
-		SupportedFeatures: sets.New[features.FeatureName](features.SupportGateway),
-		SkipTests:         []string{},
+		SupportedFeatures: sets.New(features.SupportGateway),
+		SkipTests:         skipTests,
 	})
 	if err != nil {
 		t.Fatalf("Failed to create ConformanceTestSuite: %v", err)
