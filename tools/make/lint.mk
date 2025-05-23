980e32d142caa04142680154dc37d6d8afb57f6a
Rudrakh Panigrahi <rudrakh97@gmail.com>
2025-05-23 17:01:10 +0530

    chore: add make help prompt to run golint
    Signed-off-by: Rudrakh Panigrahi <rudrakh97@gmail.com>

--- tools/make/lint.mk
+++ tools/make/lint.mk
@@ -68,8 +68,11 @@ lint.shellcheck: $(tools/shellcheck)
 	@$(LOG_TARGET)
 	$(tools/shellcheck) tools/hack/*.sh
 
+.PHONY: fix-golint
+fix-golint: lint.fix-golint ## Run golangci-lint and gci to automatically fix code lint issues
+
 .PHONY: lint.fix-golint
-lint.fix-golint: ## Run all linter of code sources and fix the issues.
+lint.fix-golint:
 	@$(LOG_TARGET)
 	$(MAKE) lint.golint GOLANGCI_LINT_FLAGS="--fix"
 	find . -name "*.go" | xargs go tool gci write --skip-generated -s Standard -s Default -s "Prefix(github.com/envoyproxy/gateway)"
