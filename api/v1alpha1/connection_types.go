d3992382ff333050816baaf4664b0842adffbc4f
jukie <10012479+Jukie@users.noreply.github.com>
2025-05-29 16:34:13 -0600

    adjust description
    Signed-off-by: jukie <10012479+Jukie@users.noreply.github.com>

--- api/v1alpha1/connection_types.go
+++ api/v1alpha1/connection_types.go
@@ -40,7 +40,7 @@ type ClientConnection struct {
 	// MaxAcceptPerSocketEvent provides configuration for the maximum number of connections to accept from the kernel
 	// per socket event. If there are more than MaxAcceptPerSocketEvent connections
 	// pending accept, connections over this threshold will be accepted in later event loop iterations.
-	// If no value is provided Envoy will accept all connections pending accept from the kernel.
+	// Default: 1
 	// +optional
 	MaxAcceptPerSocketEvent *uint32 `json:"maxAcceptPerSocketEvent,omitempty"`
 }
