2be30a0a8a39dc8b65321bb30d96488e561e89e6
Ryan Hristovski <ryan.hristovski@docker.com>
2025-06-02 18:33:13 -0400

    fix import
    Signed-off-by: Ryan Hristovski <ryan.hristovski@docker.com>

--- internal/gatewayapi/status/gateway_test.go
+++ internal/gatewayapi/status/gateway_test.go
@@ -11,8 +11,7 @@ import (
 	"strconv"
 	"testing"
 
-	"github.com/google/go-cmp/cmp"
-	"github.com/google/go-cmp/cmp/cmp/cmpopts"
+	"github.com/google/go-cmp/cmp/cmpopts"
 	"github.com/stretchr/testify/assert"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
