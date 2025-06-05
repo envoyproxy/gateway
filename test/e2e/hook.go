8df558d0926c0cc4e5155f262986fa1e7182d92c
Huabing (Robin) Zhao <zhaohuabing@gmail.com>
2025-06-05 00:38:25 +0000

    Revert "disable dump"
    This reverts commit 73cd33cf6943506ae4e8342824fb686d603d9942.

--- test/e2e/hook.go
+++ test/e2e/hook.go
@@ -10,10 +10,13 @@ import (
 
 	"sigs.k8s.io/gateway-api/conformance/utils/suite"
 	"sigs.k8s.io/gateway-api/conformance/utils/tlog"
+
+	"github.com/envoyproxy/gateway/test/e2e/tests"
 )
 
 var Hook = func(t *testing.T, test suite.ConformanceTest, suite *suite.ConformanceTestSuite) {
 	if t.Failed() {
 		tlog.Logf(t, "Test %s failed, collecting and dumping resources", test.ShortName)
+		tests.CollectAndDump(t, suite.RestConfig)
 	}
 }
