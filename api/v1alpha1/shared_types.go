6f425319985de6f4d6c7af5985bfee435e2e5179
Sudipto Baral <sudiptobaral.me@gmail.com>
2025-06-24 13:28:43 -0400

    chore(installation): early validate presence of tag in `image` and `imageRepository` (#6354)
    * chore(installation): early validate presence of tag in `image` and `imageRepository` field in KubernetesContainerSpec
    
    Signed-off-by: sudipto baral <sudiptobaral.me@gmail.com>
    
    * chore(installation): CEL validate presence of tag in image and imageRepository
    
    Signed-off-by: sudipto baral <sudiptobaral.me@gmail.com>
    
    * chore(installation): add more CEL test
    
    Signed-off-by: sudipto baral <sudiptobaral.me@gmail.com>
    
    * chore(installation): stricter CEL rules.
    
    Signed-off-by: sudipto baral <sudiptobaral.me@gmail.com>
    
    ---------
    
    Signed-off-by: sudipto baral <sudiptobaral.me@gmail.com>
    Co-authored-by: sh2 <shawnhxh@outlook.com>

--- api/v1alpha1/shared_types.go
+++ api/v1alpha1/shared_types.go
@@ -231,6 +231,7 @@ type KubernetesContainerSpec struct {
 	// Image specifies the EnvoyProxy container image to be used including a tag, instead of the default image.
 	// This field is mutually exclusive with ImageRepository.
 	//
+	// +kubebuilder:validation:XValidation:rule="self.matches('^[a-zA-Z0-9._/-]+:[a-zA-Z0-9._-]+$')",message="Image must include a tag and allowed characters only (e.g., 'repo:tag')."
 	// +optional
 	Image *string `json:"image,omitempty"`
 
@@ -238,6 +239,7 @@ type KubernetesContainerSpec struct {
 	// The default tag will be used.
 	// This field is mutually exclusive with Image.
 	//
+	// +kubebuilder:validation:XValidation:rule="self.matches('^[a-zA-Z0-9._/-]+$')",message="ImageRepository must contain only allowed characters and must not include a tag or any colons."
 	// +optional
 	ImageRepository *string `json:"imageRepository,omitempty"`
 
