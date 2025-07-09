e2969396723249987f86413fcb095761875fad87
kkk777-7 <kota.kimura0725@gmail.com>
2025-07-10 01:08:41 +0900

    support truncate ancestor for all policy
    Signed-off-by: kkk777-7 <kota.kimura0725@gmail.com>

--- internal/gatewayapi/envoyextensionpolicy.go
+++ internal/gatewayapi/envoyextensionpolicy.go
@@ -221,6 +221,12 @@ func (t *Translator) ProcessEnvoyExtensionPolicies(envoyExtensionPolicies []*egv
 		}
 	}
 
+	for _, policy := range res {
+		// Truncate Ancestor list of longer than 16
+		if len(policy.Status.Ancestors) > 16 {
+			status.TruncatePolicyAncestors(&policy.Status, t.GatewayControllerName, policy.Generation)
+		}
+	}
 	return res
 }
 
