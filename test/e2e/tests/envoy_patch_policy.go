0cd2b1eb630436dfb916a1c0fccbabf775e72d11
Huabing (Robin) Zhao <zhaohuabing@gmail.com>
2025-07-18 01:26:52 +0000

    fix e2e
    Signed-off-by: Huabing (Robin) Zhao <zhaohuabing@gmail.com>

--- test/e2e/tests/envoy_patch_policy.go
+++ test/e2e/tests/envoy_patch_policy.go
@@ -8,12 +8,25 @@
 package tests
 
 import (
+	"context"
+	"fmt"
 	"testing"
+	"time"
 
+	"github.com/stretchr/testify/require"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	gwapiv1 "sigs.k8s.io/gateway-api/apis/v1"
+	gwapiv1a2 "sigs.k8s.io/gateway-api/apis/v1alpha2"
 	"sigs.k8s.io/gateway-api/conformance/utils/http"
 	"sigs.k8s.io/gateway-api/conformance/utils/kubernetes"
 	"sigs.k8s.io/gateway-api/conformance/utils/suite"
+	"sigs.k8s.io/gateway-api/conformance/utils/tlog"
+
+	egv1a1 "github.com/envoyproxy/gateway/api/v1alpha1"
+	"github.com/envoyproxy/gateway/internal/gatewayapi"
+	"github.com/envoyproxy/gateway/internal/gatewayapi/resource"
 )
 
 func init() {
@@ -30,6 +43,15 @@ var EnvoyPatchPolicyTest = suite.ConformanceTest{
 			routeNN := types.NamespacedName{Name: "http-envoy-patch-policy", Namespace: ns}
 			gwNN := types.NamespacedName{Name: "same-namespace", Namespace: ns}
 			gwAddr := kubernetes.GatewayAndHTTPRoutesMustBeAccepted(t, suite.Client, suite.TimeoutConfig, suite.ControllerName, kubernetes.NewGatewayRef(gwNN), routeNN)
+
+			ancestorRef := gwapiv1a2.ParentReference{
+				Group:     gatewayapi.GroupPtr(gwapiv1.GroupName),
+				Kind:      gatewayapi.KindPtr(resource.KindGateway),
+				Namespace: gatewayapi.NamespacePtr(gwNN.Namespace),
+				Name:      gwapiv1.ObjectName(gwNN.Name),
+			}
+			envoyPatchPolicyMustBeAccepted(t, suite.Client, types.NamespacedName{Name: "custom-response-patch-policy", Namespace: ns}, suite.ControllerName, ancestorRef)
+
 			OkResp := http.ExpectedResponse{
 				Request: http.Request{
 					Path: "/foo",
@@ -57,4 +79,27 @@ var EnvoyPatchPolicyTest = suite.ConformanceTest{
 			http.MakeRequestAndExpectEventuallyConsistentResponse(t, suite.RoundTripper, suite.TimeoutConfig, gwAddr, customResp)
 		})
 	},
+}
+
+// envoyPatchPolicyMustBeAccepted waits for the specified EnvoyPatchPolicy to be accepted.
+func envoyPatchPolicyMustBeAccepted(t *testing.T, client client.Client, policyName types.NamespacedName, controllerName string, ancestorRef gwapiv1a2.ParentReference) {
+	t.Helper()
+
+	waitErr := wait.PollUntilContextTimeout(context.Background(), 1*time.Second, 60*time.Second, true, func(ctx context.Context) (bool, error) {
+		policy := &egv1a1.EnvoyPatchPolicy{}
+		err := client.Get(ctx, policyName, policy)
+		if err != nil {
+			return false, fmt.Errorf("error fetching EnvoyPatchPolicy: %w", err)
+		}
+
+		if policyAcceptedByAncestor(policy.Status.Ancestors, controllerName, ancestorRef) {
+			tlog.Logf(t, "EnvoyPatchPolicy has been accepted: %v", policy)
+			return true, nil
+		}
+
+		tlog.Logf(t, "EnvoyPatchPolicy not yet accepted: %v", policy)
+		return false, nil
+	})
+
+	require.NoErrorf(t, waitErr, "error waiting for EnvoyPatchPolicy to be accepted")
 }
