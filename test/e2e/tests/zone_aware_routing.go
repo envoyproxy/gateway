ac6aef18b0d0451b6d0bbbfaeca0b231d3165c08
jukie <10012479+Jukie@users.noreply.github.com>
2025-07-31 23:30:40 -0600

    fix preferlocal translation
    Signed-off-by: jukie <10012479+Jukie@users.noreply.github.com>

--- test/e2e/tests/zone_aware_routing.go
+++ test/e2e/tests/zone_aware_routing.go
@@ -10,7 +10,13 @@ package tests
 import (
 	"testing"
 
+	"k8s.io/apimachinery/pkg/types"
+	gwapiv1 "sigs.k8s.io/gateway-api/apis/v1"
+	gwapiv1a2 "sigs.k8s.io/gateway-api/apis/v1alpha2"
 	"sigs.k8s.io/gateway-api/conformance/utils/suite"
+
+	"github.com/envoyproxy/gateway/internal/gatewayapi"
+	"github.com/envoyproxy/gateway/internal/gatewayapi/resource"
 )
 
 func init() {
@@ -20,7 +26,11 @@ func init() {
 var ZoneAwareRoutingTest = suite.ConformanceTest{
 	ShortName:   "ZoneAwareRouting",
 	Description: "Test Zone Aware Routing is working",
-	Manifests:   []string{"testdata/zone-aware-routing-backendref-enabled.yaml", "testdata/zone-aware-routing-btp-enabled.yaml", "testdata/zone-aware-routing-deployments.yaml"},
+	Manifests: []string{
+		"testdata/zone-aware-routing-backendref-enabled.yaml",
+		"testdata/zone-aware-routing-btp-enabled.yaml",
+		"testdata/zone-aware-routing-deployments.yaml",
+	},
 	Test: func(t *testing.T, suite *suite.ConformanceTestSuite) {
 		t.Run("topology aware routing - only local zone should get requests", func(t *testing.T) {
 			// Pods from the backend-local deployment have affinity
@@ -29,16 +39,28 @@ var ZoneAwareRoutingTest = suite.ConformanceTest{
 				"zone-aware-backend-local":    sendRequests,
 				"zone-aware-backend-nonlocal": 0,
 			}
-			runWeightedBackendTest(t, suite, "topology-aware-routing", "/topology-aware-routing", "topology-aware-routing", expected)
+			runWeightedBackendTest(t, suite, "topology-aware-routing", "/topology-aware-routing", "zone-aware-backend", expected)
 		})
 		t.Run("BackendTrafficPolicy - only local zone should get requests", func(t *testing.T) {
+			BackendTrafficPolicyMustBeAccepted(t,
+				suite.Client,
+				types.NamespacedName{Name: "btp-zone-aware", Namespace: "gateway-conformance-infra"},
+				suite.ControllerName,
+				gwapiv1a2.ParentReference{
+					Group:     gatewayapi.GroupPtr(gwapiv1.GroupName),
+					Kind:      gatewayapi.KindPtr(resource.KindGateway),
+					Namespace: gatewayapi.NamespacePtr("gateway-conformance-infra"),
+					Name:      gwapiv1.ObjectName("same-namespace"),
+				},
+			)
+
 			// Pods from the backend-local deployment have affinity
 			// for the Envoy Proxy pods so should receive all requests.
 			expected := map[string]int{
 				"zone-aware-backend-local":    sendRequests,
 				"zone-aware-backend-nonlocal": 0,
 			}
-			runWeightedBackendTest(t, suite, "btp-zone-aware", "/btp-zone-aware", "btp-zone-aware", expected)
+			runWeightedBackendTest(t, suite, "btp-zone-aware", "/btp-zone-aware", "zone-aware-backend", expected)
 		})
 	},
 }
