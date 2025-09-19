514869823b172bdefa7e04b3b67b09234eed4499
Karol Szwaj <karol.szwaj@gmail.com>
2025-09-19 12:40:38 +0200

    fix tests
    Signed-off-by: Karol Szwaj <karol.szwaj@gmail.com>

--- internal/infrastructure/common/proxy_jwt.go
+++ internal/infrastructure/common/proxy_jwt.go
@@ -9,10 +9,11 @@ import "fmt"
 
 const (
 	// #nosec G101 - This is a file path, not a credential
-	SdsServiceAccountTokenFilename = "xds_service_account_token.json"
+	SdsServiceAccountTokenFilename = "xds-service-account-token.json"
 )
 
+// GetSdsServiceAccountTokenConfigMapData returns the SDS config map data for the service account token used in GatewayNamespaceMode.
 func GetSdsServiceAccountTokenConfigMapData(tokenPath string) string {
 	return fmt.Sprintf(`{"resources":[{"@type":"type.googleapis.com/envoy.extensions.transport_sockets.tls.v3.Secret",`+
-		`"name":"xds_service_account_token","service_account_token":{"path":"%s"}}]}`, tokenPath)
+		`"name":"xds-service-account-token","service_account_token":{"path":"%s"}}]}`, tokenPath)
 }
