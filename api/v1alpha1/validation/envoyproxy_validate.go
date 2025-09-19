4438fff3cad5d9771ff1778f8fe3901d447f5417
Ignasi Barrera <nacx@apache.org>
2025-09-19 19:30:16 +0200

    standalone: use the right version of Envoy (#6891)
    When using the standalone mode and the Host infrastructure, EG uses func-e to download and/or start Envoy, but the version of Envoy to run is not explicitly set. This could lead to undersider behaviour if content already exists in the /tmp/envoy-gateway directory and EG may not use the expected version.
    
    This PR adds a change to:
    
    Infer the version to use from the default Envoy Image tag. This is only set on the release branches, but not main. In that case, fallback to the old behaviour for backwards compatibility and let func-e decide the version to use.
    Ad a config knob to the EnvoyProxy API to allow setting the version of Envoy to use when in standalone mode:
    spec:
      provider:
        type: Host
        host:
          envoyVersion: "1.35.0"
    ---------
    
    Signed-off-by: Ignasi Barrera <nacx@apache.org>

--- api/v1alpha1/validation/envoyproxy_validate.go
+++ api/v1alpha1/validation/envoyproxy_validate.go
@@ -67,24 +67,30 @@ func validateEnvoyProxySpec(spec *egv1a1.EnvoyProxySpec) error {
 func validateProvider(spec *egv1a1.EnvoyProxySpec) []error {
 	var errs []error
 	if spec != nil && spec.Provider != nil {
-		if spec.Provider.Type != egv1a1.ProviderTypeKubernetes {
+		switch spec.Provider.Type {
+		case egv1a1.EnvoyProxyProviderTypeKubernetes:
+			validateDeploymentErrs := validateDeployment(spec)
+			if len(validateDeploymentErrs) != 0 {
+				errs = append(errs, validateDeploymentErrs...)
+			}
+			validateHpaErrors := validateHpa(spec)
+			if len(validateHpaErrors) != 0 {
+				errs = append(errs, validateHpaErrors...)
+			}
+			validatePdbErrors := validatePdb(spec)
+			if len(validatePdbErrors) != 0 {
+				errs = append(errs, validatePdbErrors...)
+			}
+			validateServiceErrs := validateService(spec)
+			if len(validateServiceErrs) != 0 {
+				errs = append(errs, validateServiceErrs...)
+			}
+		case egv1a1.EnvoyProxyProviderTypeHost:
+			if spec.Provider.Host == nil {
+				errs = append(errs, fmt.Errorf("field 'host' should be specified when provider type is 'Host'"))
+			}
+		default:
 			errs = append(errs, fmt.Errorf("unsupported provider type %v", spec.Provider.Type))
-		}
-		validateDeploymentErrs := validateDeployment(spec)
-		if len(validateDeploymentErrs) != 0 {
-			errs = append(errs, validateDeploymentErrs...)
-		}
-		validateHpaErrors := validateHpa(spec)
-		if len(validateHpaErrors) != 0 {
-			errs = append(errs, validateHpaErrors...)
-		}
-		validatePdbErrors := validatePdb(spec)
-		if len(validatePdbErrors) != 0 {
-			errs = append(errs, validatePdbErrors...)
-		}
-		validateServiceErrs := validateService(spec)
-		if len(validateServiceErrs) != 0 {
-			errs = append(errs, validateServiceErrs...)
 		}
 	}
 	return errs
