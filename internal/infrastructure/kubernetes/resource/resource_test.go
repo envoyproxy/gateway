4a7d7061ed1a1d5153c64bf37846f7c0b4be98b2
Arko Dasgupta <arko@tetrate.io>
2025-09-24 13:35:48 -0700

    fix test
    Signed-off-by: Arko Dasgupta <arko@tetrate.io>

--- internal/infrastructure/kubernetes/resource/resource_test.go
+++ internal/infrastructure/kubernetes/resource/resource_test.go
@@ -11,8 +11,6 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/util/intstr"
 	"k8s.io/utils/ptr"
 
 	egv1a1 "github.com/envoyproxy/gateway/api/v1alpha1"
