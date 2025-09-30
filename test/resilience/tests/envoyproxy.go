c5b17b3407a0c8a0eb227713f0f61c714b19de25
ADITYATIWARI342005 <142050150+ADITYATIWARI342005@users.noreply.github.com>
2025-09-30 20:34:54 +0530

    refactor(resilience): replace AwaitConvergence loops with MakeRequestAndExpectEventuallyConsistentResponse; cleanup unused imports
    Signed-off-by: ADITYATIWARI342005 <142050150+ADITYATIWARI342005@users.noreply.github.com>

--- test/resilience/tests/envoyproxy.go
+++ test/resilience/tests/envoyproxy.go
@@ -17,7 +17,6 @@ import (
 	gwapiv1 "sigs.k8s.io/gateway-api/apis/v1"
 	"sigs.k8s.io/gateway-api/conformance/utils/http"
 	"sigs.k8s.io/gateway-api/conformance/utils/kubernetes"
-	"sigs.k8s.io/gateway-api/conformance/utils/tlog"
 
 	"github.com/envoyproxy/gateway/test/resilience/suite"
 )
