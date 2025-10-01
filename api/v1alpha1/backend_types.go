257cc7e89126192e99e738b595ab6c5715d65518
Huabing Zhao <zhaohuabing@gmail.com>
2025-10-01 14:58:18 +0800

    provider
    Signed-off-by: Huabing Zhao <zhaohuabing@gmail.com>

--- api/v1alpha1/backend_types.go
+++ api/v1alpha1/backend_types.go
@@ -217,11 +217,10 @@ type BackendTLSSettings struct {
 	// +optional
 	SNI *gwapiv1.PreciseHostname `json:"sni,omitempty"`
 
-	// ClientTLS defines the client-side TLS identity and settings Envoy should use
-	// when initiating TLS connections to this backend. This allows configuring the
-	// client certificate/key as well as TLS protocol parameters such as ciphers,
-	// TLS versions, and ALPN. When omitted, Envoy will fall back to the
-	// EnvoyProxy BackendTLS defaults, if any.
+	// ClientTLS defines the client TLS settings that Envoy will use when initiating
+	// TLS connections to this backend. This allows configuring the client certificate/key
+	// as well as TLS protocol parameters such as ciphers, TLS versions, and ALPN.
+	// When omitted, Envoy will fall back to the EnvoyProxy BackendTLS defaults, if any.
 	//
 	// +optional
 	ClientTLS *BackendTLSConfig `json:"clientTLS,omitempty"`
