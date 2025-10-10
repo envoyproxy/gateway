c5dcdf70d2aa0d3221cc4b9f7174a676e9e7a6f8
Adam Buran <aburan28@gmail.com>
2025-10-10 08:54:37 -0700

    add enforcing percentage to 100
    Signed-off-by: Adam Buran <aburan28@gmail.com>

--- internal/xds/translator/cluster.go
+++ internal/xds/translator/cluster.go
@@ -583,7 +583,7 @@ func buildXdsOutlierDetection(outlierDetection *ir.OutlierDetection) *clusterv3.
 
 	if outlierDetection.FailurePercentageThreshold != nil {
 		od.FailurePercentageThreshold = wrapperspb.UInt32(*outlierDetection.FailurePercentageThreshold)
-		od.EnforcingFailurePercentage = wrapperspb.UInt32(*outlierDetection.FailurePercentageThreshold)
+		od.EnforcingFailurePercentage = wrapperspb.UInt32(100)
 	}
 
 	return od
