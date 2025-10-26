67d607503b7064812ac0ec6ce373cab2759394c8
Adam Buran <aburan@roblox.com>
2025-10-26 14:02:06 -0700

    decompressor
    Signed-off-by: Adam Buran <aburan@roblox.com>

---
+++ api/v1alpha1/decompressor_types.go
@@ -0,0 +1,67 @@
+// Copyright Envoy Gateway Authors
+// SPDX-License-Identifier: Apache-2.0
+// The full text of the Apache license is available in the LICENSE file at
+// the root of the repo.
+
+package v1alpha1
+
+// DecompressorType defines the types of decompressor library supported by Envoy Gateway.
+//
+// +kubebuilder:validation:Enum=Gzip;Brotli;Zstd
+type DecompressorType string
+
+const (
+	GzipDecompressorType DecompressorType = "Gzip"
+
+	BrotliDecompressorType DecompressorType = "Brotli"
+
+	ZstdDecompressorType DecompressorType = "Zstd"
+)
+
+// GzipDecompressor defines the config for the Gzip decompressor.
+// The default values can be found here:
+// https://www.envoyproxy.io/docs/envoy/latest/api-v3/extensions/compression/gzip/decompressor/v3/gzip.proto#extension-envoy-compression-gzip-decompressor
+type GzipDecompressor struct {
+	// WindowBits is the window size for decompression. The valid range is 9 to 15.
+	// The default value is 15.
+	//
+	// +optional
+	// +kubebuilder:validation:Minimum=9
+	// +kubebuilder:validation:Maximum=15
+	// +kubebuilder:default=15
+	WindowBits *uint32 `json:"windowBits,omitempty"`
+}
+
+// BrotliDecompressor defines the config for the Brotli decompressor.
+// The default values can be found here:
+// https://www.envoyproxy.io/docs/envoy/latest/api-v3/extensions/compression/brotli/decompressor/v3/brotli.proto#extension-envoy-compression-brotli-decompressor
+type BrotliDecompressor struct{}
+
+// ZstdDecompressor defines the config for the Zstd decompressor.
+// The default values can be found here:
+// https://www.envoyproxy.io/docs/envoy/latest/api-v3/extensions/compression/zstd/decompressor/v3/zstd.proto#extension-envoy-compression-zstd-decompressor
+type ZstdDecompressor struct{}
+
+// Decompression defines the config of enabling decompression.
+// This can help decompress compressed request/response bodies.
+type Decompression struct {
+	// DecompressorType defines the decompressor type to use for decompression.
+	//
+	// +required
+	Type DecompressorType `json:"type"`
+
+	// The configuration for Brotli decompressor.
+	//
+	// +optional
+	Brotli *BrotliDecompressor `json:"brotli,omitempty"`
+
+	// The configuration for GZIP decompressor.
+	//
+	// +optional
+	Gzip *GzipDecompressor `json:"gzip,omitempty"`
+
+	// The configuration for Zstd decompressor.
+	//
+	// +optional
+	Zstd *ZstdDecompressor `json:"zstd,omitempty"`
+}
---
+++ internal/xds/translator/decompressor.go
@@ -0,0 +1,165 @@
+// Copyright Envoy Gateway Authors
+// SPDX-License-Identifier: Apache-2.0
+// The full text of the Apache license is available in the LICENSE file at
+// the root of the repo.
+
+package translator
+
+import (
+	"fmt"
+	"strings"
+
+	corev3 "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
+	routev3 "github.com/envoyproxy/go-control-plane/envoy/config/route/v3"
+	brotlidecompressorv3 "github.com/envoyproxy/go-control-plane/envoy/extensions/compression/brotli/decompressor/v3"
+	gzipdecompressorv3 "github.com/envoyproxy/go-control-plane/envoy/extensions/compression/gzip/decompressor/v3"
+	zstddecompressorv3 "github.com/envoyproxy/go-control-plane/envoy/extensions/compression/zstd/decompressor/v3"
+	decompressorv3 "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/http/decompressor/v3"
+	hcmv3 "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/network/http_connection_manager/v3"
+	protobuf "google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/anypb"
+	"google.golang.org/protobuf/types/known/wrapperspb"
+
+	egv1a1 "github.com/envoyproxy/gateway/api/v1alpha1"
+	"github.com/envoyproxy/gateway/internal/ir"
+	"github.com/envoyproxy/gateway/internal/utils/proto"
+	"github.com/envoyproxy/gateway/internal/xds/types"
+)
+
+func init() {
+	registerHTTPFilter(&decompressor{})
+}
+
+type decompressor struct{}
+
+var _ httpFilter = &decompressor{}
+
+// patchHCM builds and appends the decompressor Filter to the HTTP Connection Manager
+// if applicable, and it does not already exist.
+// Note: This is a no-op for decompressor as it requires global configuration.
+// The actual decompressor filter addition is handled by patchHCMWithDecompressor in the translator.
+func (*decompressor) patchHCM(mgr *hcmv3.HttpConnectionManager, irListener *ir.HTTPListener) error {
+	// Decompressor configuration is handled at the translator level
+	// since it requires access to the global Xds configuration
+	return nil
+}
+
+func (*decompressor) patchResources(*types.ResourceVersionTable, []*ir.HTTPRoute) error {
+	return nil
+}
+
+// patchRoute enables the decompressor filters for all routes when decompression is configured globally.
+func (*decompressor) patchRoute(route *routev3.Route, irRoute *ir.HTTPRoute, _ *ir.HTTPListener) error {
+	// Route-level configuration is handled by patchHCMWithDecompressor
+	return nil
+}
+
+// patchHCMWithDecompressor adds decompressor filters to the HCM based on the global Xds configuration.
+// This is called from the translator which has access to the full Xds configuration.
+func (t *Translator) patchHCMWithDecompressor(mgr *hcmv3.HttpConnectionManager, xds *ir.Xds) error {
+	if mgr == nil || xds == nil || len(xds.Decompression) == 0 {
+		return nil
+	}
+
+	var (
+		filter *hcmv3.HttpFilter
+		err    error
+	)
+
+	// Process each decompression type
+	for _, irDecomp := range xds.Decompression {
+		filterName := decompressorFilterName(irDecomp.Type)
+
+		// Skip if filter already exists
+		if hcmContainsFilter(mgr, filterName) {
+			continue
+		}
+
+		// Extract window bits for gzip if configured
+		var windowBits *uint32
+		if irDecomp.Type == egv1a1.GzipDecompressorType && irDecomp.Gzip != nil {
+			windowBits = irDecomp.Gzip.WindowBits
+		}
+
+		if filter, err = buildDecompressorFilter(irDecomp.Type, windowBits); err != nil {
+			return err
+		}
+
+		mgr.HttpFilters = append(mgr.HttpFilters, filter)
+	}
+
+	return nil
+}
+
+func decompressorFilterName(decompressorType egv1a1.DecompressorType) string {
+	return fmt.Sprintf("%s.%s", egv1a1.EnvoyFilterDecompressor.String(), strings.ToLower(string(decompressorType)))
+}
+
+// buildDecompressorFilter builds a decompressor filter with the provided decompressionType.
+func buildDecompressorFilter(decompressionType egv1a1.DecompressorType, windowBits *uint32) (*hcmv3.HttpFilter, error) {
+	var (
+		decompressorProto *decompressorv3.Decompressor
+		extensionName     string
+		extensionMsg      protobuf.Message
+		extensionAny      *anypb.Any
+		decompressorAny   *anypb.Any
+		err               error
+	)
+
+	switch decompressionType {
+	case egv1a1.BrotliDecompressorType:
+		extensionName = "envoy.compression.brotli.decompressor"
+		extensionMsg = &brotlidecompressorv3.Brotli{}
+	case egv1a1.GzipDecompressorType:
+		extensionName = "envoy.compression.gzip.decompressor"
+		gzipMsg := &gzipdecompressorv3.Gzip{}
+		if windowBits != nil {
+			gzipMsg.WindowBits = wrapperspb.UInt32(*windowBits)
+		}
+		extensionMsg = gzipMsg
+	case egv1a1.ZstdDecompressorType:
+		extensionName = "envoy.compression.zstd.decompressor"
+		extensionMsg = &zstddecompressorv3.Zstd{}
+	default:
+		return nil, fmt.Errorf("unsupported decompressor type: %s", decompressionType)
+	}
+
+	if extensionAny, err = proto.ToAnyWithValidation(extensionMsg); err != nil {
+		return nil, err
+	}
+
+	decompressorProto = &decompressorv3.Decompressor{
+		DecompressorLibrary: &corev3.TypedExtensionConfig{
+			Name:        extensionName,
+			TypedConfig: extensionAny,
+		},
+		// Enable decompression for both request and response by default
+		RequestDirectionConfig: &decompressorv3.Decompressor_RequestDirectionConfig{
+			CommonConfig: &decompressorv3.Decompressor_CommonDirectionConfig{
+				Enabled: &corev3.RuntimeFeatureFlag{
+					DefaultValue: wrapperspb.Bool(true),
+				},
+			},
+		},
+		ResponseDirectionConfig: &decompressorv3.Decompressor_ResponseDirectionConfig{
+			CommonConfig: &decompressorv3.Decompressor_CommonDirectionConfig{
+				Enabled: &corev3.RuntimeFeatureFlag{
+					DefaultValue: wrapperspb.Bool(true),
+				},
+			},
+		},
+	}
+
+	if decompressorAny, err = proto.ToAnyWithValidation(decompressorProto); err != nil {
+		return nil, err
+	}
+
+	return &hcmv3.HttpFilter{
+		Name: decompressorFilterName(decompressionType),
+		ConfigType: &hcmv3.HttpFilter_TypedConfig{
+			TypedConfig: decompressorAny,
+		},
+		// Decompressor is enabled globally, not disabled by default
+		Disabled: false,
+	}, nil
+}
