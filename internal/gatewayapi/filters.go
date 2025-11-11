66471dca17f8f8a9fe755d6f09ca273f46974d73
Huabing Zhao <zhaohuabing@gmail.com>
2025-11-11 10:11:51 +0800

    check redirect respose filter
    Signed-off-by: Huabing Zhao <zhaohuabing@gmail.com>

--- internal/gatewayapi/filters.go
+++ internal/gatewayapi/filters.go
@@ -69,7 +69,10 @@ type HTTPFilterIR struct {
 // Header value pattern according to RFC 7230
 var HeaderValueRegexp = regexp.MustCompile(`^[!-~]+([\t ]?[!-~]+)*$`)
 
-const requestMirrorDirectResponseConflictMsg = "RequestMirror filter cannot be used when the rule also configures a DirectResponse filter"
+const (
+	requestMirrorDirectResponseConflictMsg = "RequestMirror filter cannot be used when the rule also configures a DirectResponse filter"
+	requestMirrorRedirectConflictMsg       = "RequestMirror filter cannot be used when the rule also configures a RequestRedirect filter"
+)
 
 // ProcessHTTPFilters translates gateway api http filters to IRs.
 func (t *Translator) ProcessHTTPFilters(parentRef *RouteParentContext,
@@ -116,8 +119,12 @@ func (t *Translator) ProcessHTTPFilters(parentRef *RouteParentContext,
 		}
 	}
 
+	// Check for conflicts between RequestMirror and DirectResponse or RequestRedirect filters
 	if httpFiltersContext.DirectResponse != nil && len(httpFiltersContext.Mirrors) > 0 {
 		updateRouteStatusForFilter(httpFiltersContext, requestMirrorDirectResponseConflictMsg)
+	}
+	if httpFiltersContext.RedirectResponse != nil && len(httpFiltersContext.Mirrors) > 0 {
+		updateRouteStatusForFilter(httpFiltersContext, requestMirrorRedirectConflictMsg)
 	}
 
 	return httpFiltersContext, err
