e950538f074af0c131e1713c9ca6da9128c0474a
Huabing Zhao <zhaohuabing@gmail.com>
2025-12-12 18:29:12 +0800

    fix data race
    Signed-off-by: Huabing Zhao <zhaohuabing@gmail.com>

--- internal/envoygateway/config/loader/configloader.go
+++ internal/envoygateway/config/loader/configloader.go
@@ -8,6 +8,7 @@ package loader
 import (
 	"context"
 	"io"
+	"sync"
 	"time"
 
 	"github.com/envoyproxy/gateway/api/v1alpha1/validation"
@@ -24,6 +25,7 @@ type Loader struct {
 	logger  logging.Logger
 	cancel  context.CancelFunc
 	hook    HookFunc
+	mu      sync.RWMutex
 
 	w filewatcher.FileWatcher
 }
@@ -76,10 +78,13 @@ func (r *Loader) Start(ctx context.Context, logOut io.Writer) error {
 
 				// Set defaults for unset fields
 				eg.SetEnvoyGatewayDefaults()
+				eg.Logging.SetEnvoyGatewayLoggingDefaults()
+
+				r.mu.Lock()
 				r.cfg.EnvoyGateway = eg
 				// update cfg logger
-				eg.Logging.SetEnvoyGatewayLoggingDefaults()
 				r.cfg.Logger = logging.NewLogger(logOut, eg.Logging)
+				r.mu.Unlock()
 
 				// cancel last
 				if r.cancel != nil {
@@ -111,11 +116,28 @@ func (r *Loader) runHook(ctx context.Context) {
 	}
 
 	r.logger.Info("running hook")
+	cfgCopy := r.snapshotConfig()
 	c, cancel := context.WithCancel(ctx)
 	r.cancel = cancel
 	go func(ctx context.Context) {
-		if err := r.hook(ctx, r.cfg); err != nil {
+		if err := r.hook(ctx, cfgCopy); err != nil {
 			r.logger.Error(err, "hook error")
 		}
 	}(c)
+}
+
+func (r *Loader) snapshotConfig() *config.Server {
+	r.mu.RLock()
+	defer r.mu.RUnlock()
+
+	if r.cfg == nil {
+		return nil
+	}
+
+	cp := *r.cfg
+	if r.cfg.EnvoyGateway != nil {
+		cp.EnvoyGateway = r.cfg.EnvoyGateway.DeepCopy()
+	}
+
+	return &cp
 }
