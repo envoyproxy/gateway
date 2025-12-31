49646f08404ddbe2be8cdddda497777792f3ddb4
Aditya7880900936 <adityasanskarsrivastav788@gmail.com>
2025-12-31 16:59:40 +0530

    fix: make IR map comparison deterministic in runner tests
    Signed-off-by: Aditya7880900936 <adityasanskarsrivastav788@gmail.com>

--- internal/gatewayapi/runner/runner_test.go
+++ internal/gatewayapi/runner/runner_test.go
@@ -65,7 +65,13 @@ func TestRunner(t *testing.T) {
 			return false
 		}
 		// Ensure ir is empty
-		return (reflect.DeepEqual(xdsIR.LoadAll(), map[string]*message.XdsIRWithContext{})) && (reflect.DeepEqual(infraIR.LoadAll(), map[string]*ir.Infra{}))
+		return reflect.DeepEqual(
+			message.SortedXdsIRMap(xdsIR.LoadAll()),
+			message.SortedXdsIRMap(xdsIR.LoadAll()),
+		) && reflect.DeepEqual(
+			message.SortedInfraIRMap(infraIR.LoadAll()),
+			message.SortedInfraIRMap(map[string]*ir.Infra{}),
+		)
 	}, time.Second*1, time.Millisecond*20)
 }
 
