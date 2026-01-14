5aadb866de59485b71e08f7c8bdd65fc78dfc7fe
Kota Kimura <86363983+kkk777-7@users.noreply.github.com>
2026-01-14 13:58:25 +0900

    fix: validation behavior for multiple certificate refs (#7909)
    * fix: multiple certificates validation behavior
    
    Signed-off-by: kkk777-7 <kota.kimura0725@gmail.com>

--- internal/gatewayapi/tls.go
+++ internal/gatewayapi/tls.go
@@ -8,17 +8,26 @@ package gatewayapi
 import (
 	"crypto/x509"
 	"encoding/pem"
+	"errors"
 	"fmt"
 	"time"
 
 	corev1 "k8s.io/api/core/v1"
+	gwapiv1 "sigs.k8s.io/gateway-api/apis/v1"
+
+	"github.com/envoyproxy/gateway/internal/gatewayapi/status"
 )
 
 // parseCertsFromTLSSecretsData parses the cert and key provided in a secret
-// and ensures that they are not malformed and can be properly parsed
-func parseCertsFromTLSSecretsData(secrets []*corev1.Secret) ([]*x509.Certificate, error) {
-	var publicKeyAlgorithm string
-	var parseErr error
+// and ensures that they are not malformed and can be properly parsed.
+// this function returns a list of valid secrets and certificates.
+func parseCertsFromTLSSecretsData(secrets []*corev1.Secret) ([]*corev1.Secret, []*x509.Certificate, status.ListenerError) {
+	var (
+		publicKeyAlgorithm string
+		errs               []error
+	)
+
+	validSecrets := make([]*corev1.Secret, 0, len(secrets))
 	certs := make([]*x509.Certificate, 0, len(secrets))
 
 	pkaSecretSet := make(map[string]string)
@@ -26,17 +35,23 @@ func parseCertsFromTLSSecretsData(secrets []*corev1.Secret) ([]*x509.Certificate
 		certData := secret.Data[corev1.TLSCertKey]
 
 		if err := validateCertificates(certData); err != nil {
-			return nil, fmt.Errorf("%s/%s must contain valid %s and %s, unable to validate certificate in %s: %w", secret.Namespace, secret.Name, corev1.TLSCertKey, corev1.TLSPrivateKeyKey, corev1.TLSCertKey, err)
+			errs = append(errs, fmt.Errorf("%s/%s must contain valid %s and %s, unable to validate certificate in %s: %w",
+				secret.Namespace, secret.Name, corev1.TLSCertKey, corev1.TLSPrivateKeyKey, corev1.TLSCertKey, err))
+			continue
 		}
 
 		certBlock, _ := pem.Decode(certData)
 		if certBlock == nil {
-			return nil, fmt.Errorf("%s/%s must contain valid %s and %s, unable to decode pem data in %s", secret.Namespace, secret.Name, corev1.TLSCertKey, corev1.TLSPrivateKeyKey, corev1.TLSCertKey)
+			errs = append(errs, fmt.Errorf("%s/%s must contain valid %s and %s, unable to decode pem data in %s",
+				secret.Namespace, secret.Name, corev1.TLSCertKey, corev1.TLSPrivateKeyKey, corev1.TLSCertKey))
+			continue
 		}
 
 		cert, err := x509.ParseCertificate(certBlock.Bytes)
 		if err != nil {
-			return nil, fmt.Errorf("%s/%s must contain valid %s and %s, unable to parse certificate in %s: %w", secret.Namespace, secret.Name, corev1.TLSCertKey, corev1.TLSPrivateKeyKey, corev1.TLSCertKey, err)
+			errs = append(errs, fmt.Errorf("%s/%s must contain valid %s and %s, unable to parse certificate in %s: %w",
+				secret.Namespace, secret.Name, corev1.TLSCertKey, corev1.TLSPrivateKeyKey, corev1.TLSCertKey, err))
+			continue
 		}
 		publicKeyAlgorithm = cert.PublicKeyAlgorithm.String()
 
@@ -44,7 +59,9 @@ func parseCertsFromTLSSecretsData(secrets []*corev1.Secret) ([]*x509.Certificate
 
 		keyBlock, _ := pem.Decode(keyData)
 		if keyBlock == nil {
-			return nil, fmt.Errorf("%s/%s must contain valid %s and %s, unable to decode pem data in %s", secret.Namespace, secret.Name, corev1.TLSCertKey, corev1.TLSPrivateKeyKey, corev1.TLSPrivateKeyKey)
+			errs = append(errs, fmt.Errorf("%s/%s must contain valid %s and %s, unable to decode pem data in %s",
+				secret.Namespace, secret.Name, corev1.TLSCertKey, corev1.TLSPrivateKeyKey, corev1.TLSPrivateKeyKey))
+			continue
 		}
 
 		// SNI and SAN/Cert Domain mismatch is allowed
@@ -60,40 +77,72 @@ func parseCertsFromTLSSecretsData(secrets []*corev1.Secret) ([]*x509.Certificate
 		}
 
 		// Check uniqueness for each domain in the certificate with this algorithm
+		hasConflictDomainAlgorithm := false
 		for _, domain := range certDomains {
 			pkaSecretKey := fmt.Sprintf("%s/%s", publicKeyAlgorithm, domain)
 
 			// Check whether the public key algorithm and certificate domain are unique
 			if _, ok := pkaSecretSet[pkaSecretKey]; ok {
-				return nil, fmt.Errorf("%s/%s public key algorithm must be unique, certificate domain %s has a conflicting algorithm [%s]",
-					secret.Namespace, secret.Name, domain, publicKeyAlgorithm)
+				errs = append(errs, fmt.Errorf("%s/%s public key algorithm must be unique, certificate domain %s has a conflicting algorithm [%s]",
+					secret.Namespace, secret.Name, domain, publicKeyAlgorithm))
+				hasConflictDomainAlgorithm = true
+				break
 			}
 			pkaSecretSet[pkaSecretKey] = domain
+		}
+		if hasConflictDomainAlgorithm {
+			continue
 		}
 
 		switch keyBlock.Type {
 		case "PRIVATE KEY":
 			_, err := x509.ParsePKCS8PrivateKey(keyBlock.Bytes)
 			if err != nil {
-				parseErr = fmt.Errorf("%s/%s must contain valid %s and %s, unable to parse PKCS8 formatted private key in %s", secret.Namespace, secret.Name, corev1.TLSCertKey, corev1.TLSPrivateKeyKey, corev1.TLSPrivateKeyKey)
+				errs = append(errs, fmt.Errorf("%s/%s must contain valid %s and %s, unable to parse PKCS8 formatted private key in %s",
+					secret.Namespace, secret.Name, corev1.TLSCertKey, corev1.TLSPrivateKeyKey, corev1.TLSPrivateKeyKey))
+				continue
 			}
 		case "RSA PRIVATE KEY":
 			_, err := x509.ParsePKCS1PrivateKey(keyBlock.Bytes)
 			if err != nil {
-				parseErr = fmt.Errorf("%s/%s must contain valid %s and %s, unable to parse PKCS1 formatted private key in %s", secret.Namespace, secret.Name, corev1.TLSCertKey, corev1.TLSPrivateKeyKey, corev1.TLSPrivateKeyKey)
+				errs = append(errs, fmt.Errorf("%s/%s must contain valid %s and %s, unable to parse PKCS1 formatted private key in %s",
+					secret.Namespace, secret.Name, corev1.TLSCertKey, corev1.TLSPrivateKeyKey, corev1.TLSPrivateKeyKey))
+				continue
 			}
 		case "EC PRIVATE KEY":
 			_, err := x509.ParseECPrivateKey(keyBlock.Bytes)
 			if err != nil {
-				parseErr = fmt.Errorf("%s/%s must contain valid %s and %s, unable to parse EC formatted private key in %s", secret.Namespace, secret.Name, corev1.TLSCertKey, corev1.TLSPrivateKeyKey, corev1.TLSPrivateKeyKey)
+				errs = append(errs, fmt.Errorf("%s/%s must contain valid %s and %s, unable to parse EC formatted private key in %s",
+					secret.Namespace, secret.Name, corev1.TLSCertKey, corev1.TLSPrivateKeyKey, corev1.TLSPrivateKeyKey))
+				continue
 			}
 		default:
-			return nil, fmt.Errorf("%s/%s must contain valid %s and %s, %s key format found in %s, supported formats are PKCS1, PKCS8 or EC", secret.Namespace, secret.Name, corev1.TLSCertKey, corev1.TLSPrivateKeyKey, keyBlock.Type, corev1.TLSPrivateKeyKey)
+			errs = append(errs, fmt.Errorf("%s/%s must contain valid %s and %s, %s key format found in %s, supported formats are PKCS1, PKCS8 or EC",
+				secret.Namespace, secret.Name, corev1.TLSCertKey, corev1.TLSPrivateKeyKey, keyBlock.Type, corev1.TLSPrivateKeyKey))
+			continue
 		}
+
+		validSecrets = append(validSecrets, secret)
 		certs = append(certs, cert)
 	}
 
-	return certs, parseErr
+	// If there are validation errors, determine the appropriate listener reason based on whether any valid certificates were found.
+	// If no valid certs exist, the listener cannot process traffic normally, so this is treated as a InvalidCertificateRef.
+	// If some valid certs exist, this is treated as a PartiallyInvalidCertificateRef to notify cert error to user.
+	if len(errs) > 0 {
+		if len(certs) == 0 {
+			return nil, nil, status.NewListenerStatusError(
+				errors.Join(errs...),
+				gwapiv1.ListenerReasonInvalidCertificateRef,
+			)
+		} else {
+			return validSecrets, certs, status.NewListenerStatusError(
+				errors.Join(errs...),
+				status.ListenerReasonPartiallyInvalidCertificateRef,
+			)
+		}
+	}
+	return validSecrets, certs, nil
 }
 
 // validateCertificate validates all certificates in PEM encoded data.
