4122d631050528d678f538b89fb67d3117ca93c0
Huabing (Robin) Zhao <zhaohuabing@gmail.com>
2026-01-22 17:40:31 +0800

    fix: wrong cluster type with mixed FQDN backend and service backend refs (#7994)
    * fix: wrong cluster type with mixed FQDN backend and service backend refs
    
    Signed-off-by: Huabing (Robin) Zhao <zhaohuabing@gmail.com>
    
    * fix mirror cluster endpoint type
    
    Signed-off-by: Huabing (Robin) Zhao <zhaohuabing@gmail.com>
    
    * simplify the test
    
    Signed-off-by: Huabing (Robin) Zhao <zhaohuabing@gmail.com>
    
    * update comment
    
    Signed-off-by: Huabing (Robin) Zhao <zhaohuabing@gmail.com>
    
    ---------
    
    Signed-off-by: Huabing (Robin) Zhao <zhaohuabing@gmail.com>
    Signed-off-by: Rudrakh Panigrahi <rudrakh97@gmail.com>

--- internal/xds/translator/translator.go
+++ internal/xds/translator/translator.go
@@ -586,9 +586,12 @@ func (t *Translator) addRouteToRouteConfig(
 			}
 
 			var err error
-			// If there are no filters in the destination
-			// settings and ZoneAware routing isn't
-			// enabled we create a regular xds Cluster
+			// In these cases we create a cluster per setting
+			//
+			// * ZoneAware routing is enabled
+			// * There are filters in the destination settings
+			// * There are multiple Address	Type of destination settings(IP, FQDN, UDC, etc.)
+			// * There are invalid/empty settings in the destination settings
 			if !httpRoute.NeedsClusterPerSetting() {
 				err = processXdsCluster(
 					tCtx,
@@ -602,8 +605,6 @@ func (t *Translator) addRouteToRouteConfig(
 					errs = errors.Join(errs, err)
 				}
 			} else {
-				// If a filter does exist, we create a weighted cluster that's
-				// attached to the route, and create a xds Cluster per setting
 				for _, setting := range httpRoute.Destination.Settings {
 					tSettings := []*ir.DestinationSetting{setting}
 					err = processXdsCluster(
@@ -628,7 +629,7 @@ func (t *Translator) addRouteToRouteConfig(
 						name:         mrr.Destination.Name,
 						settings:     mrr.Destination.Settings,
 						tSocket:      nil,
-						endpointType: EndpointTypeStatic,
+						endpointType: buildEndpointType(mrr.Destination.Settings),
 						metrics:      metrics,
 						metadata:     mrr.Destination.Metadata,
 					}); err != nil {
