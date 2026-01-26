fd04a11dab372a1e7adea3465d14060e4c2426a8
Rudrakh Panigrahi <rudrakh97@gmail.com>
2026-01-26 09:48:33 +0530

    [release/v1.6] bump ratelimit version
    Signed-off-by: Rudrakh Panigrahi <rudrakh97@gmail.com>

--- api/v1alpha1/shared_types.go
+++ api/v1alpha1/shared_types.go
@@ -31,7 +31,7 @@ const (
 	// DefaultShutdownManagerImage is the default image used for the shutdown manager.
 	DefaultShutdownManagerImage = "docker.io/envoyproxy/gateway-dev:latest"
 	// DefaultRateLimitImage is the default image used by ratelimit.
-	DefaultRateLimitImage = "docker.io/envoyproxy/ratelimit:a28b84d0"
+	DefaultRateLimitImage = "docker.io/envoyproxy/ratelimit:3fb70258"
 	// HTTPProtocol is the common-used http protocol.
 	HTTPProtocol = "http"
 	// GRPCProtocol is the common-used grpc protocol.
