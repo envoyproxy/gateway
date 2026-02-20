259a2c5550d3138a2f357a2d4107fb4afd039faa
pdefreitas <5927433+pdefreitas@users.noreply.github.com>
2026-02-20 00:35:28 +0000

    refactor based in review input
    Signed-off-by: pdefreitas <5927433+pdefreitas@users.noreply.github.com>

--- internal/xds/translator/compressor.go
+++ internal/xds/translator/compressor.go
@@ -86,13 +86,13 @@ func buildCompressorFilter(compression *ir.Compression) (*hcmv3.HttpFilter, erro
 	switch compression.Type {
 	case egv1a1.BrotliCompressorType:
 		extensionName = "envoy.compression.brotli.compressor"
-		extensionMsg = &brotliv3.Brotli{}
+		extensionMsg = buildBrotliConfig(compression.Config)
 	case egv1a1.GzipCompressorType:
 		extensionName = "envoy.compression.gzip.compressor"
-		extensionMsg = &gzipv3.Gzip{}
+		extensionMsg = buildGzipConfig(compression.Config)
 	case egv1a1.ZstdCompressorType:
 		extensionName = "envoy.compression.zstd.compressor"
-		extensionMsg = &zstdv3.Zstd{}
+		extensionMsg = buildZstdConfig(compression.Config)
 	}
 
 	if extensionAny, err = proto.ToAnyWithValidation(extensionMsg); err != nil {
@@ -193,5 +193,159 @@ func compressorPerRouteConfig() *compressorv3.CompressorPerRoute {
 				ResponseDirectionConfig: &compressorv3.ResponseDirectionOverrides{},
 			},
 		},
+	}
+}
+
+func buildGzipConfig(cfg *egv1a1.Compression) *gzipv3.Gzip {
+	gzip := &gzipv3.Gzip{}
+	if cfg == nil || cfg.Gzip == nil {
+		return gzip
+	}
+
+	if cfg.Gzip.MemoryLevel != nil {
+		gzip.MemoryLevel = wrapperspb.UInt32(*cfg.Gzip.MemoryLevel)
+	}
+	if cfg.Gzip.WindowBits != nil {
+		gzip.WindowBits = wrapperspb.UInt32(*cfg.Gzip.WindowBits)
+	}
+	if cfg.Gzip.ChunkSize != nil {
+		gzip.ChunkSize = wrapperspb.UInt32(*cfg.Gzip.ChunkSize)
+	}
+	if cfg.Gzip.CompressionLevel != nil {
+		gzip.CompressionLevel = mapGzipCompressionLevel(*cfg.Gzip.CompressionLevel)
+	}
+	if cfg.Gzip.CompressionStrategy != nil {
+		gzip.CompressionStrategy = mapGzipCompressionStrategy(*cfg.Gzip.CompressionStrategy)
+	}
+	return gzip
+}
+
+func buildBrotliConfig(cfg *egv1a1.Compression) *brotliv3.Brotli {
+	brotli := &brotliv3.Brotli{}
+	if cfg == nil || cfg.Brotli == nil {
+		return brotli
+	}
+
+	if cfg.Brotli.Quality != nil {
+		brotli.Quality = wrapperspb.UInt32(*cfg.Brotli.Quality)
+	}
+	if cfg.Brotli.WindowBits != nil {
+		brotli.WindowBits = wrapperspb.UInt32(*cfg.Brotli.WindowBits)
+	}
+	if cfg.Brotli.InputBlockBits != nil {
+		brotli.InputBlockBits = wrapperspb.UInt32(*cfg.Brotli.InputBlockBits)
+	}
+	if cfg.Brotli.ChunkSize != nil {
+		brotli.ChunkSize = wrapperspb.UInt32(*cfg.Brotli.ChunkSize)
+	}
+	if cfg.Brotli.EncoderMode != nil {
+		brotli.EncoderMode = mapBrotliEncoderMode(*cfg.Brotli.EncoderMode)
+	}
+	if cfg.Brotli.DisableLiteralContextModeling != nil {
+		brotli.DisableLiteralContextModeling = *cfg.Brotli.DisableLiteralContextModeling
+	}
+	return brotli
+}
+
+func buildZstdConfig(cfg *egv1a1.Compression) *zstdv3.Zstd {
+	zstd := &zstdv3.Zstd{}
+	if cfg == nil || cfg.Zstd == nil {
+		return zstd
+	}
+
+	if cfg.Zstd.CompressionLevel != nil {
+		zstd.CompressionLevel = wrapperspb.UInt32(*cfg.Zstd.CompressionLevel)
+	}
+	if cfg.Zstd.ChunkSize != nil {
+		zstd.ChunkSize = wrapperspb.UInt32(*cfg.Zstd.ChunkSize)
+	}
+	if cfg.Zstd.EnableChecksum != nil {
+		zstd.EnableChecksum = *cfg.Zstd.EnableChecksum
+	}
+	if cfg.Zstd.Strategy != nil {
+		zstd.Strategy = mapZstdStrategy(*cfg.Zstd.Strategy)
+	}
+	return zstd
+}
+
+func mapGzipCompressionLevel(level string) gzipv3.Gzip_CompressionLevel {
+	switch level {
+	case "BEST_SPEED":
+		return gzipv3.Gzip_BEST_SPEED
+	case "BEST_COMPRESSION":
+		return gzipv3.Gzip_BEST_COMPRESSION
+	case "COMPRESSION_LEVEL_1":
+		return gzipv3.Gzip_COMPRESSION_LEVEL_1
+	case "COMPRESSION_LEVEL_2":
+		return gzipv3.Gzip_COMPRESSION_LEVEL_2
+	case "COMPRESSION_LEVEL_3":
+		return gzipv3.Gzip_COMPRESSION_LEVEL_3
+	case "COMPRESSION_LEVEL_4":
+		return gzipv3.Gzip_COMPRESSION_LEVEL_4
+	case "COMPRESSION_LEVEL_5":
+		return gzipv3.Gzip_COMPRESSION_LEVEL_5
+	case "COMPRESSION_LEVEL_6":
+		return gzipv3.Gzip_COMPRESSION_LEVEL_6
+	case "COMPRESSION_LEVEL_7":
+		return gzipv3.Gzip_COMPRESSION_LEVEL_7
+	case "COMPRESSION_LEVEL_8":
+		return gzipv3.Gzip_COMPRESSION_LEVEL_8
+	case "COMPRESSION_LEVEL_9":
+		return gzipv3.Gzip_COMPRESSION_LEVEL_9
+	default:
+		return gzipv3.Gzip_DEFAULT_COMPRESSION
+	}
+}
+
+func mapGzipCompressionStrategy(strategy string) gzipv3.Gzip_CompressionStrategy {
+	switch strategy {
+	case "FILTERED":
+		return gzipv3.Gzip_FILTERED
+	case "HUFFMAN_ONLY":
+		return gzipv3.Gzip_HUFFMAN_ONLY
+	case "RLE":
+		return gzipv3.Gzip_RLE
+	case "FIXED":
+		return gzipv3.Gzip_FIXED
+	default:
+		return gzipv3.Gzip_DEFAULT_STRATEGY
+	}
+}
+
+func mapBrotliEncoderMode(mode string) brotliv3.Brotli_EncoderMode {
+	switch mode {
+	case "GENERIC":
+		return brotliv3.Brotli_GENERIC
+	case "TEXT":
+		return brotliv3.Brotli_TEXT
+	case "FONT":
+		return brotliv3.Brotli_FONT
+	default:
+		return brotliv3.Brotli_DEFAULT
+	}
+}
+
+func mapZstdStrategy(strategy string) zstdv3.Zstd_Strategy {
+	switch strategy {
+	case "FAST":
+		return zstdv3.Zstd_FAST
+	case "DFAST":
+		return zstdv3.Zstd_DFAST
+	case "GREEDY":
+		return zstdv3.Zstd_GREEDY
+	case "LAZY":
+		return zstdv3.Zstd_LAZY
+	case "LAZY2":
+		return zstdv3.Zstd_LAZY2
+	case "BTLAZY2":
+		return zstdv3.Zstd_BTLAZY2
+	case "BTOPT":
+		return zstdv3.Zstd_BTOPT
+	case "BTULTRA":
+		return zstdv3.Zstd_BTULTRA
+	case "BTULTRA2":
+		return zstdv3.Zstd_BTULTRA2
+	default:
+		return zstdv3.Zstd_DEFAULT
 	}
 }
