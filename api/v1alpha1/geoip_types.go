f9e294265d6d39b62700cfd79aa40a6270af4238
Huabing (Robin) Zhao <zhaohuabing@gmail.com>
2026-02-26 15:39:19 +0800

    add db source type
    Signed-off-by: Huabing (Robin) Zhao <zhaohuabing@gmail.com>

--- api/v1alpha1/geoip_types.go
+++ api/v1alpha1/geoip_types.go
@@ -6,7 +6,7 @@
 package v1alpha1
 
 // GeoIPProvider defines provider-specific settings.
-// +kubebuilder:validation:XValidation:rule="self.type == 'MaxMind' ? has(self.MaxMind) : true",message="MaxMind must be set when type is MaxMind"
+// +kubebuilder:validation:XValidation:rule="self.type == 'MaxMind' ? has(self.maxMind) : true",message="maxMind must be set when type is MaxMind"
 type GeoIPProvider struct {
 	// +kubebuilder:validation:Enum=MaxMind
 	// +kubebuilder:validation:Required
@@ -15,7 +15,7 @@ type GeoIPProvider struct {
 	// MaxMind configures the MaxMind provider.
 	//
 	// +optional
-	MaxMind *GeoIPMaxMind `json:"MaxMind,omitempty"`
+	MaxMind *GeoIPMaxMind `json:"maxMind,omitempty"`
 }
 
 // GeoIPProviderType enumerates GeoIP providers supported by Envoy Gateway.
@@ -28,38 +28,62 @@ const (
 
 // GeoIPMaxMind configures the MaxMind provider.
 // These database files are expected to be mounted into the Envoy container, and a sidecar container can be used to update the database files.
-// +kubebuilder:validation:XValidation:rule="has(self.cityDbPath) || has(self.countryDbPath) || has(self.asnDbPath) || has(self.ispDbPath) || has(self.anonymousIpDbPath)",message="At least one MaxMind database path must be specified"
+// +kubebuilder:validation:XValidation:rule="has(self.cityDbSource) || has(self.countryDbSource) || has(self.asnDbSource) || has(self.ispDbSource) || has(self.anonymousIpDbSource)",message="at least one MaxMind database source must be specified"
 type GeoIPMaxMind struct {
-	// CityDBPath is the path to the City database (.mmdb).
+	// CityDBSource configures the City database source.
 	//
 	// +optional
-	// +kubebuilder:validation:Pattern=`^.*\\.mmdb$`
-	CityDBPath *string `json:"cityDbPath,omitempty"`
+	CityDBSource *GeoIPDBSource `json:"cityDbSource,omitempty"`
 
-	// CountryDBPath is the path to the Country database (.mmdb).
+	// CountryDBSource configures the Country database source.
 	//
 	// +optional
-	// +kubebuilder:validation:Pattern=`^.*\\.mmdb$`
-	CountryDBPath *string `json:"countryDbPath,omitempty"`
+	CountryDBSource *GeoIPDBSource `json:"countryDbSource,omitempty"`
 
-	// ASNDBPath is the path to the ASN database (.mmdb).
+	// ASNDBSource configures the ASN database source.
 	//
 	// +optional
-	// +kubebuilder:validation:P	attern=`^.*\\.mmdb$`
-	ASNDBPath *string `json:"asnDbPath,omitempty"`
+	ASNDBSource *GeoIPDBSource `json:"asnDbSource,omitempty"`
 
-	// ISPDBPath is the path to the ISP database (.mmdb).
+	// ISPDBSource configures the ISP database source.
 	//
 	// +optional
-	// +kubebuilder:validation:Pattern=`^.*\\.mmdb$`
-	ISPDBPath *string `json:"ispDbPath,omitempty"`
+	ISPDBSource *GeoIPDBSource `json:"ispDbSource,omitempty"`
 
-	// AnonymousIPDBPath is the path to the Anonymous IP database (.mmdb).
+	// AnonymousIPDBSource configures the Anonymous IP database source.
 	//
 	// +optional
-	// +kubebuilder:validation:Pattern=`^.*\\.mmdb$`
-	AnonymousIPDBPath *string `json:"anonymousIpDbPath,omitempty"`
+	AnonymousIPDBSource *GeoIPDBSource `json:"anonymousIpDbSource,omitempty"`
 }
+
+// GeoIPDBSource defines where a GeoIP .mmdb database can be loaded from.
+// +union
+// +kubebuilder:validation:XValidation:rule="has(self.local) || has(self.remote)",message="at least one of local or remote must be specified"
+type GeoIPDBSource struct {
+	// Local is a database source from a local file.
+	//
+	// +optional
+	Local *LocalGeoIPDBSource `json:"local,omitempty"`
+
+	// Remote is a database source fetched from a remote URL.
+	// TODO: implement this in the future
+	// +notImplementedHide
+	// +optional
+	Remote *RemoteGeoIPDBSource `json:"remote,omitempty"`
+}
+
+// LocalGeoIPDBSource configures a GeoIP database from a local file path.
+type LocalGeoIPDBSource struct {
+	// Path is the path to the database file.
+	//
+	// +kubebuilder:validation:Pattern=`^.*\\.mmdb$`
+	Path string `json:"path"`
+}
+
+// RemoteGeoIPDBSource configures a GeoIP database fetched from a remote URL.
+// TODO: implement this in the future
+// +notImplementedHide
+type RemoteGeoIPDBSource struct{}
 
 // GeoIPAnonymousMatch matches anonymous network signals emitted by the GeoIP provider.
 // If multiple fields are specified, all specified fields must match.
