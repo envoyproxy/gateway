291cc3f5921d194c2e1cb30e6f17e97d2a42714d
Rudrakh Panigrahi <rudrakh97@gmail.com>
2026-03-12 14:36:41 +0530

    Bump envoy proxy version to v1.36.5 (#8498)
    upgrade envoy proxy version to v1.36.5
    
    Signed-off-by: Rudrakh Panigrahi <rudrakh97@gmail.com>

--- api/v1alpha1/shared_types.go
+++ api/v1alpha1/shared_types.go
@@ -23,7 +23,7 @@ const (
 	// DefaultDeploymentMemoryResourceRequests for deployment memory resource
 	DefaultDeploymentMemoryResourceRequests = "512Mi"
 	// DefaultEnvoyProxyImage is the default image used by envoyproxy
-	DefaultEnvoyProxyImage = "docker.io/envoyproxy/envoy:distroless-v1.36.4"
+	DefaultEnvoyProxyImage = "docker.io/envoyproxy/envoy:distroless-v1.36.5"
 	// DefaultShutdownManagerCPUResourceRequests for shutdown manager cpu resource
 	DefaultShutdownManagerCPUResourceRequests = "10m"
 	// DefaultShutdownManagerMemoryResourceRequests for shutdown manager memory resource
