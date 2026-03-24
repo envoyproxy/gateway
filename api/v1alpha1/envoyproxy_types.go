3c2fc03228d04a4b3acc5e41e59e2d57ff1bf413
Huabing (Robin) Zhao <zhaohuabing@gmail.com>
2026-03-24 16:43:22 +0800

    feat: GeoIP (#8453)
    * provider implementation
    
    Signed-off-by: Huabing (Robin) Zhao <zhaohuabing@gmail.com>
    
    GeoIP implementation
    
    Signed-off-by: Huabing (Robin) Zhao <zhaohuabing@gmail.com>
    
    * debug ipv6 failure
    
    Signed-off-by: Huabing (Robin) Zhao <zhaohuabing@gmail.com>
    
    * fix test
    
    Signed-off-by: Huabing (Robin) Zhao <zhaohuabing@gmail.com>
    
    * disable geoIP test in IPv6
    
    Signed-off-by: Huabing (Robin) Zhao <zhaohuabing@gmail.com>
    
    * address comments
    
    Signed-off-by: Huabing (Robin) Zhao <zhaohuabing@gmail.com>
    
    * address comments
    
    Signed-off-by: Huabing (Robin) Zhao <zhaohuabing@gmail.com>
    
    * fix lint
    
    Signed-off-by: Huabing (Robin) Zhao <zhaohuabing@gmail.com>
    
    * simplify code
    
    Signed-off-by: Huabing (Robin) Zhao <zhaohuabing@gmail.com>
    
    * reuse direct response
    
    Signed-off-by: Huabing (Robin) Zhao <zhaohuabing@gmail.com>
    
    * simplify code
    
    Signed-off-by: Huabing (Robin) Zhao <zhaohuabing@gmail.com>
    
    ---------
    
    Signed-off-by: Huabing (Robin) Zhao <zhaohuabing@gmail.com>

--- api/v1alpha1/envoyproxy_types.go
+++ api/v1alpha1/envoyproxy_types.go
@@ -134,6 +134,8 @@ type EnvoyProxySpec struct {
 	//
 	// - envoy.filters.http.dynamic_modules
 	//
+	// - envoy.filters.http.geoip
+	//
 	// - envoy.filters.http.rbac
 	//
 	// - envoy.filters.http.local_ratelimit
@@ -202,7 +204,6 @@ type EnvoyProxySpec struct {
 	// GeoIP defines shared GeoIP provider configuration for this EnvoyProxy fleet.
 	//
 	// +optional
-	// +notImplementedHide
 	GeoIP *EnvoyProxyGeoIP `json:"geoIP,omitempty"`
 }
 
@@ -276,7 +277,7 @@ type FilterPosition struct {
 }
 
 // EnvoyFilter defines the type of Envoy HTTP filter.
-// +kubebuilder:validation:Enum=envoy.filters.http.custom_response;envoy.filters.http.health_check;envoy.filters.http.fault;envoy.filters.http.cors;envoy.filters.http.header_mutation;envoy.filters.http.ext_authz;envoy.filters.http.api_key_auth;envoy.filters.http.basic_auth;envoy.filters.http.oauth2;envoy.filters.http.jwt_authn;envoy.filters.http.stateful_session;envoy.filters.http.buffer;envoy.filters.http.lua;envoy.filters.http.ext_proc;envoy.filters.http.wasm;envoy.filters.http.dynamic_modules;envoy.filters.http.rbac;envoy.filters.http.local_ratelimit;envoy.filters.http.ratelimit;envoy.filters.http.grpc_web;envoy.filters.http.grpc_stats;envoy.filters.http.credential_injector;envoy.filters.http.compressor;envoy.filters.http.dynamic_forward_proxy
+// +kubebuilder:validation:Enum=envoy.filters.http.custom_response;envoy.filters.http.health_check;envoy.filters.http.fault;envoy.filters.http.cors;envoy.filters.http.header_mutation;envoy.filters.http.ext_authz;envoy.filters.http.api_key_auth;envoy.filters.http.basic_auth;envoy.filters.http.oauth2;envoy.filters.http.jwt_authn;envoy.filters.http.stateful_session;envoy.filters.http.buffer;envoy.filters.http.lua;envoy.filters.http.ext_proc;envoy.filters.http.wasm;envoy.filters.http.dynamic_modules;envoy.filters.http.geoip;envoy.filters.http.rbac;envoy.filters.http.local_ratelimit;envoy.filters.http.ratelimit;envoy.filters.http.grpc_web;envoy.filters.http.grpc_stats;envoy.filters.http.credential_injector;envoy.filters.http.compressor;envoy.filters.http.dynamic_forward_proxy
 type EnvoyFilter string
 
 const (
@@ -328,6 +329,9 @@ const (
 
 	// EnvoyFilterDynamicModules defines the Envoy HTTP dynamic modules filter.
 	EnvoyFilterDynamicModules EnvoyFilter = "envoy.filters.http.dynamic_modules"
+
+	// EnvoyFilterGeoIP defines the Envoy HTTP GeoIP filter.
+	EnvoyFilterGeoIP EnvoyFilter = "envoy.filters.http.geoip"
 
 	// EnvoyFilterRBAC defines the Envoy RBAC filter.
 	EnvoyFilterRBAC EnvoyFilter = "envoy.filters.http.rbac"
