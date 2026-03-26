4c883e3a4b6a6844809cf6926d98b068e1737b16
Michal Skalski <michal@skalski.org>
2026-03-26 20:09:28 +0900

    fix lint: preallocate
    Signed-off-by: Michal Skalski <michal@skalski.org>

--- internal/gatewayapi/clienttrafficpolicy.go
+++ internal/gatewayapi/clienttrafficpolicy.go
@@ -720,7 +720,7 @@ func translateHTTP1Settings(http1Settings *egv1a1.HTTP1Settings, connection *ir.
 	if http1Settings == nil {
 		return nil
 	}
-	var ignoreUpgrade []*ir.StringMatch
+	ignoreUpgrade := make([]*ir.StringMatch, 0, len(http1Settings.IgnoreHTTP11Upgrade))
 	for _, match := range http1Settings.IgnoreHTTP11Upgrade {
 		ignoreUpgrade = append(ignoreUpgrade, irStringMatch("", match))
 	}
