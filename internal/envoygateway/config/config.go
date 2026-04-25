30cdc614477d5492464f108b17f753a43a7e05e5
Huabing (Robin) Zhao <zhaohuabing@gmail.com>
2026-04-25 09:34:37 +0800

    performance: use cached kube client for the infra runner (#8764)
    * use cached client from the controller manager
    
    Signed-off-by: Huabing (Robin) Zhao <zhaohuabing@gmail.com>
    
    * fix rbac
    
    Signed-off-by: zhaohuabing <zhaohuabing@gmail.com>
    
    * address comments
    
    Signed-off-by: Huabing (Robin) Zhao <zhaohuabing@gmail.com>
    
    * inject provider kube client to infra runner
    
    Signed-off-by: Huabing (Robin) Zhao <zhaohuabing@gmail.com>
    
    * add retries for rate limit server
    
    Signed-off-by: Huabing (Robin) Zhao <zhaohuabing@gmail.com>
    
    * watch only EG owned resources
    
    Signed-off-by: Huabing (Robin) Zhao <zhaohuabing@gmail.com>
    
    * add release note
    
    Signed-off-by: Huabing (Robin) Zhao <zhaohuabing@gmail.com>
    
    * use context to pass the kube client
    
    Signed-off-by: Huabing (Robin) Zhao <zhaohuabing@gmail.com>
    
    * update release note
    
    Signed-off-by: Huabing (Robin) Zhao <zhaohuabing@gmail.com>
    
    * use config.server to pass the kube client
    
    Signed-off-by: Huabing (Robin) Zhao <zhaohuabing@gmail.com>
    
    * fix lint
    
    Signed-off-by: Huabing (Robin) Zhao <zhaohuabing@gmail.com>
    
    * address comments
    
    Signed-off-by: Huabing (Robin) Zhao <zhaohuabing@gmail.com>
    
    * update release note
    
    Signed-off-by: Huabing (Robin) Zhao <zhaohuabing@gmail.com>
    
    * minor wording
    
    Signed-off-by: Huabing (Robin) Zhao <zhaohuabing@gmail.com>
    
    * fix test
    
    Signed-off-by: Huabing (Robin) Zhao <zhaohuabing@gmail.com>
    
    * fix test
    
    Signed-off-by: Huabing (Robin) Zhao <zhaohuabing@gmail.com>
    
    * wait for the cached client and start ratlimit server first
    
    Signed-off-by: Huabing (Robin) Zhao <zhaohuabing@gmail.com>
    
    * fix gen
    
    Signed-off-by: Huabing (Robin) Zhao <zhaohuabing@gmail.com>
    
    * remove unnecessary change
    
    Signed-off-by: Huabing (Robin) Zhao <zhaohuabing@gmail.com>
    
    * fix rbac
    
    Signed-off-by: Huabing (Robin) Zhao <zhaohuabing@gmail.com>
    
    * fix gen
    
    Signed-off-by: Huabing (Robin) Zhao <zhaohuabing@gmail.com>
    
    ---------
    
    Signed-off-by: Huabing (Robin) Zhao <zhaohuabing@gmail.com>
    Signed-off-by: zhaohuabing <zhaohuabing@gmail.com>

--- internal/envoygateway/config/config.go
+++ internal/envoygateway/config/config.go
@@ -9,6 +9,8 @@ import (
 	"errors"
 	"io"
 
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
 	egv1a1 "github.com/envoyproxy/gateway/api/v1alpha1"
 	"github.com/envoyproxy/gateway/api/v1alpha1/validation"
 	"github.com/envoyproxy/gateway/internal/logging"
@@ -39,10 +41,36 @@ type Server struct {
 	Logger logging.Logger
 	// Elected chan is used to signal when an EG instance is elected as leader.
 	Elected chan struct{}
+	// ProviderReady is closed once the Kubernetes provider cache is synced and the cached client is ready for consumers.
+	ProviderReady chan struct{}
 	// Stdout is the writer for standard output.
 	Stdout io.Writer
 	// Stderr is the writer for error output.
 	Stderr io.Writer
+	// KubernetesClient holds the controller-runtime client created by the Kubernetes provider.
+	// This is used by the infrastructure runner to create the envoy proxy and rate limit infra resources.
+	KubernetesClient *KubernetesClientHolder
+}
+
+type KubernetesClientHolder struct {
+	client client.Client
+}
+
+func NewKubernetesClientHolder() *KubernetesClientHolder {
+	return &KubernetesClientHolder{}
+}
+
+func (h *KubernetesClientHolder) Set(cli client.Client) {
+	if h != nil {
+		h.client = cli
+	}
+}
+
+func (h *KubernetesClientHolder) Get() client.Client {
+	if h == nil {
+		return nil
+	}
+	return h.client
 }
 
 // New returns a Server with default parameters.
@@ -55,6 +83,8 @@ func New(stdout, stderr io.Writer) (*Server, error) {
 		Stdout:              stdout,
 		Stderr:              stderr,
 		Elected:             make(chan struct{}),
+		ProviderReady:       make(chan struct{}),
+		KubernetesClient:    NewKubernetesClientHolder(),
 	}, nil
 }
 
