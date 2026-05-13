da97ce6bec8cee6061205263ad157d006d09d9e5
zirain <zirain2009@gmail.com>
2026-05-13 11:28:47 +0800

    update EXAMPLE_APPS
    Signed-off-by: zirain <zirain2009@gmail.com>

--- tools/make/examples.mk
+++ tools/make/examples.mk
@@ -1,4 +1,14 @@
-EXAMPLE_APPS := simple-extension-server extension-server envoy-ext-auth grpc-ext-proc preserve-case-backend static-file-server dynamic-module-test backend-utilization
+EXAMPLE_APPS := \
+	simple-extension-server \
+	extension-server \
+	envoy-ext-auth \
+	grpc-ext-proc \
+	preserve-case-backend \
+	static-file-server \
+	dynamic-module-test \
+	backend-utilization \
+	sds-server
+
 EXAMPLE_IMAGE_PREFIX ?= envoyproxy/gateway-
 EXAMPLE_TAG ?= latest
 
