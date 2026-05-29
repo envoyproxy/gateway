7590ec2428ab3f05af4dcaa48ae5e46e9342f609
zirain <zirain2009@gmail.com>
2026-05-29 19:07:30 +0800

    fix: MaxStreamDuration should worked for non-route cluster (#9028)
    * fix: MaxStreamDuration should worked on commonHttpProtocolOptions
    
    Signed-off-by: zirain <zirain2009@gmail.com>
    
    * should only worked for non-route cluster
    
    Signed-off-by: zirain <zirain2009@gmail.com>
    
    * fix
    
    Signed-off-by: zirain <zirain2009@gmail.com>
    
    ---------
    
    Signed-off-by: zirain <zirain2009@gmail.com>
    (cherry picked from commit 0edf51a12b9f15d8cdd0eeb0483736b1feb43740)
    Signed-off-by: jukie <10012479+jukie@users.noreply.github.com>

--- internal/xds/translator/cluster.go
+++ internal/xds/translator/cluster.go
@@ -85,6 +85,7 @@ type xdsClusterArgs struct {
 	unstructuredRefs  []*unstructured.Unstructured
 	extensionMgr      *extensionTypes.Manager
 	logger            logging.Logger
+	isRoute           bool
 }
 
 type EndpointType int
@@ -1025,9 +1026,18 @@ func getHealthCheckOverridesHostname(hc *ir.HealthCheck, ep *ir.DestinationEndpo
 	return *ep.Hostname
 }
 
+func hasTimeoutArgs(args *xdsClusterArgs) bool {
+	if args.timeout == nil || args.timeout.HTTP == nil {
+		return false
+	}
+	timeout := args.timeout.HTTP
+	return timeout.MaxConnectionDuration != nil ||
+		timeout.ConnectionIdleTimeout != nil ||
+		(!args.isRoute && timeout.MaxStreamDuration != nil) // Only set cluster-level maxStreamDuration for non-route clusters
+}
+
 func buildTypedExtensionProtocolOptions(args *xdsClusterArgs, requiresAutoHTTPConfig, requiresHTTP2Options, requiresAutoSNI, forceHTTP1UpstreamProtocol bool) (map[string]*anypb.Any, []*tlsv3.Secret, error) {
-	requiresCommonHTTPOptions := (args.timeout != nil && args.timeout.HTTP != nil &&
-		(args.timeout.HTTP.MaxConnectionDuration != nil || args.timeout.HTTP.ConnectionIdleTimeout != nil)) ||
+	requiresCommonHTTPOptions := hasTimeoutArgs(args) ||
 		(args.circuitBreaker != nil && args.circuitBreaker.MaxRequestsPerConnection != nil)
 
 	requiresHTTP1Options := args.http1Settings != nil &&
@@ -1052,6 +1062,10 @@ func buildTypedExtensionProtocolOptions(args *xdsClusterArgs, requiresAutoHTTPCo
 
 			if args.timeout.HTTP.MaxConnectionDuration != nil {
 				protocolOptions.CommonHttpProtocolOptions.MaxConnectionDuration = durationpb.New(args.timeout.HTTP.MaxConnectionDuration.Duration)
+			}
+
+			if !args.isRoute && args.timeout.HTTP.MaxStreamDuration != nil {
+				protocolOptions.CommonHttpProtocolOptions.MaxStreamDuration = durationpb.New(args.timeout.HTTP.MaxStreamDuration.Duration)
 			}
 		}
 
@@ -1380,6 +1394,7 @@ func (route *UDPRouteTranslator) asClusterArgs(name string,
 		dns:          route.DNS,
 		ipFamily:     extra.ipFamily,
 		metadata:     metadata,
+		isRoute:      true,
 	}
 }
 
@@ -1407,6 +1422,7 @@ func (route *TCPRouteTranslator) asClusterArgs(name string,
 		dns:               route.DNS,
 		ipFamily:          extra.ipFamily,
 		metadata:          metadata,
+		isRoute:           true,
 	}
 }
 
@@ -1435,6 +1451,7 @@ func (httpRoute *HTTPRouteTranslator) asClusterArgs(name string,
 		extensionMgr:      extra.extensionMgr,
 		unstructuredRefs:  extra.unstructuredRefs,
 		logger:            extra.logger,
+		isRoute:           true,
 	}
 
 	// Populate traffic features.
