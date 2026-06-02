0297ac4910a316375c38ca52935702928e9485c0
Huabing (Robin) Zhao <zhaohuabing@gmail.com>
2026-06-02 23:00:22 +0800

    fix: force HTTP1 for Service/ServiceImport with websocket appProtocol (#9138)
    force HTTP1 for Service/ServiceImport with websocket appProtocol
    
    Signed-off-by: Huabing Zhao <zhaohuabing@gmail.com>
    Signed-off-by: zirain <zirain2009@gmail.com>

--- internal/gatewayapi/route_test.go
+++ internal/gatewayapi/route_test.go
@@ -22,6 +22,64 @@ import (
 	"github.com/envoyproxy/gateway/internal/ir"
 )
 
+func TestServiceAppProtocolToIRAppProtocol(t *testing.T) {
+	tests := []struct {
+		name              string
+		appProtocol       string
+		defaultProtocol   ir.AppProtocol
+		grpcCompatibility bool
+		want              ir.AppProtocol
+		wantForceHTTP1    bool
+	}{
+		{
+			name:            "h2c",
+			appProtocol:     "kubernetes.io/h2c",
+			defaultProtocol: ir.HTTP,
+			want:            ir.HTTP2,
+		},
+		{
+			name:            "ws",
+			appProtocol:     "kubernetes.io/ws",
+			defaultProtocol: ir.HTTP2,
+			want:            ir.HTTP,
+			wantForceHTTP1:  true,
+		},
+		{
+			name:            "wss",
+			appProtocol:     "kubernetes.io/wss",
+			defaultProtocol: ir.HTTP2,
+			want:            ir.HTTP,
+			wantForceHTTP1:  true,
+		},
+		{
+			name:              "grpc compatible",
+			appProtocol:       "grpc",
+			defaultProtocol:   ir.HTTP,
+			grpcCompatibility: true,
+			want:              ir.GRPC,
+		},
+		{
+			name:            "grpc not compatible",
+			appProtocol:     "grpc",
+			defaultProtocol: ir.HTTP,
+			want:            ir.HTTP,
+		},
+		{
+			name:            "unknown",
+			appProtocol:     "example.com/custom",
+			defaultProtocol: ir.HTTP,
+			want:            ir.HTTP,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			require.Equal(t, tt.want, serviceAppProtocolToIRAppProtocol(tt.appProtocol, tt.defaultProtocol, tt.grpcCompatibility))
+			require.Equal(t, tt.wantForceHTTP1, isWebSocketServiceAppProtocol(tt.appProtocol))
+		})
+	}
+}
+
 func TestGetIREndpointsFromEndpointSlices(t *testing.T) {
 	tests := []struct {
 		name              string
