7dda1e46820a21612388ccaf9b743143f704c65d
Huabing Zhao <zhaohuabing@gmail.com>
2026-06-04 13:39:20 +0800

    Fixed Backend TLS  to disable upstream ALPN instead of inheriting EnvoyProxy BackendTLS defaults.
    Signed-off-by: Huabing Zhao <zhaohuabing@gmail.com>

--- internal/gatewayapi/backendtlspolicy.go
+++ internal/gatewayapi/backendtlspolicy.go
@@ -244,7 +244,7 @@ func mergeClientTLSConfigs(
 		mergedConfig.SignatureAlgorithms = backendClientTLSConfig.SignatureAlgorithms
 	}
 
-	if len(backendClientTLSConfig.ALPNProtocols) > 0 {
+	if backendClientTLSConfig.ALPNProtocols != nil {
 		mergedConfig.ALPNProtocols = backendClientTLSConfig.ALPNProtocols
 	}
 
@@ -373,7 +373,8 @@ func (t *Translator) processClientTLSSettings(
 	if clientTLS.MaxVersion != nil {
 		tlsConfig.MaxVersion = new(ir.TLSVersion(*clientTLS.MaxVersion))
 	}
-	if len(clientTLS.ALPNProtocols) > 0 {
+	// An empty list of ALPNProtocols means APLN is disabled, while a nil value means it is not set.
+	if clientTLS.ALPNProtocols != nil {
 		tlsConfig.ALPNProtocols = make([]string, len(clientTLS.ALPNProtocols))
 		for i := range clientTLS.ALPNProtocols {
 			tlsConfig.ALPNProtocols[i] = string(clientTLS.ALPNProtocols[i])
