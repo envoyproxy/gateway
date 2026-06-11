37cb6983bdd3b551ad54cda7cd286e8f4a912736
Muhammad Waqar <waqar.hameed08@gmail.com>
2026-06-11 16:25:16 -0400

    fix: repair CEL test after rebase (missing comma and import)
    Signed-off-by: Muhammad Waqar <waqar.hameed08@gmail.com>

--- test/cel-validation/backendtrafficpolicy_test.go
+++ test/cel-validation/backendtrafficpolicy_test.go
@@ -19,6 +19,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	gwapiv1 "sigs.k8s.io/gateway-api/apis/v1"
 	gwapiv1a2 "sigs.k8s.io/gateway-api/apis/v1alpha2"
+	mcsapiv1a1 "sigs.k8s.io/mcs-api/pkg/apis/v1alpha1"
 
 	egv1a1 "github.com/envoyproxy/gateway/api/v1alpha1"
 )
@@ -3739,6 +3740,7 @@ func TestBackendTrafficPolicyTarget(t *testing.T) {
 				}
 			},
 			wantErrors: []string{},
+		},
 		{
 			desc: "valid service targetRef with CDS-only fields",
 			mutate: func(btp *egv1a1.BackendTrafficPolicy) {
