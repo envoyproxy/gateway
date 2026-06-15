079cb5c4254a74b03e40d95784a946fd24d683e7
apkatsikas <apkatsikas@gmail.com>
2026-06-15 11:01:57 -0400

    test(e2e): add e2e test for ClientTrafficPolicy targeting ListenerSet
    Signed-off-by: apkatsikas <apkatsikas@gmail.com>

---
+++ test/e2e/tests/listenerset_client_traffic_policy.go
@@ -0,0 +1,86 @@
+// Copyright Envoy Gateway Authors
+// SPDX-License-Identifier: Apache-2.0
+// The full text of the Apache license is available in the LICENSE file at
+// the root of the repo.
+
+//go:build e2e
+
+// This is an EG-specific e2e test, not an upstream conformance test.
+
+package tests
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"testing"
+
+	"k8s.io/apimachinery/pkg/types"
+	gwapiv1 "sigs.k8s.io/gateway-api/apis/v1"
+	"sigs.k8s.io/gateway-api/conformance/utils/kubernetes"
+	"sigs.k8s.io/gateway-api/conformance/utils/suite"
+
+	gatewayapi "github.com/envoyproxy/gateway/internal/gatewayapi"
+	"github.com/envoyproxy/gateway/internal/gatewayapi/resource"
+)
+
+func init() {
+	ConformanceTests = append(ConformanceTests, ListenerSetClientTrafficPolicyTest)
+}
+
+// ListenerSetClientTrafficPolicyTest verifies that a ClientTrafficPolicy targeting a ListenerSet
+// enforces TLS settings on the ListenerSet's listeners. This is an EG-specific feature
+// and is not part of the upstream Gateway API conformance suite.
+var ListenerSetClientTrafficPolicyTest = suite.ConformanceTest{
+	ShortName:   "ListenerSetClientTrafficPolicy",
+	Description: "ClientTrafficPolicy targeting a ListenerSet enforces TLS minVersion on ListenerSet HTTPS listeners",
+	Manifests: []string{
+		"testdata/listenerset-base.yaml",
+		"testdata/listenerset-client-traffic-policy.yaml",
+	},
+	Test: func(t *testing.T, suite *suite.ConformanceTestSuite) {
+		ns := "gateway-conformance-infra"
+		gwNN := types.NamespacedName{Name: "listener-gateway", Namespace: ns}
+		policyNN := types.NamespacedName{Name: "listenerset-ctp", Namespace: ns}
+		lsNN := types.NamespacedName{Name: "listener-set-http", Namespace: ns}
+
+		gwAddrWithPort, err := kubernetes.WaitForGatewayAddress(t, suite.Client, suite.TimeoutConfig, kubernetes.NewGatewayRef(gwNN, "core"))
+		if err != nil {
+			t.Fatalf("failed to get gateway address: %v", err)
+		}
+
+		ancestorRef := gwapiv1.ParentReference{
+			Group:     gatewayapi.GroupPtr(gwapiv1.GroupName),
+			Kind:      gatewayapi.KindPtr(resource.KindListenerSet),
+			Namespace: gatewayapi.NamespacePtr(lsNN.Namespace),
+			Name:      gwapiv1.ObjectName(lsNN.Name),
+		}
+		ClientTrafficPolicyMustBeAccepted(t, suite.Client, policyNN, suite.ControllerName, ancestorRef)
+
+		listenerAddr := getListenerAddr(gwAddrWithPort, "18443")
+
+		certNN := types.NamespacedName{Name: "listener-https-certificate", Namespace: ns}
+		serverCertificate, _, _, err := GetTLSSecret(suite.Client, certNN)
+		if err != nil {
+			t.Fatalf("unexpected error finding TLS secret: %v", err)
+		}
+
+		certPool := x509.NewCertPool()
+		if !certPool.AppendCertsFromPEM(serverCertificate) {
+			t.Fatal("failed to add server certificate to pool")
+		}
+
+		//nolint: gosec
+		baseTLSConfig := &tls.Config{
+			ServerName: "www.example.com",
+			RootCAs:    certPool,
+		}
+
+		t.Run("tls 1.3 succeeds", func(t *testing.T) {
+			dialWithTLSVersion(t, listenerAddr, baseTLSConfig, tls.VersionTLS13, false)
+		})
+
+		t.Run("tls 1.2 rejected by min version policy", func(t *testing.T) {
+			dialWithTLSVersion(t, listenerAddr, baseTLSConfig, tls.VersionTLS12, true)
+		})
+	},
+}
