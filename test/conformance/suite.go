16d6d0093cae1d75507348621dff28d1b634e71d
zirain <zirain2009@gmail.com>
2026-06-17 09:41:50 +0800

    skip TCPRouteMultipleRoutesAttachment
    Signed-off-by: zirain <zirain2009@gmail.com>

--- test/conformance/suite.go
+++ test/conformance/suite.go
@@ -52,6 +52,7 @@ func conformanceOpts(t *testing.T) suite.ConformanceOptions {
 		// TODO: retry after https://github.com/envoyproxy/gateway/pull/9196 merged
 		tests.GatewayListenerUnsupportedProtocol.ShortName,
 		tests.TCPRouteInvalidNonTCPListener.ShortName,
+		tests.TCPRouteMultipleRoutesAttachment.ShortName,
 		// https://github.com/envoyproxy/gateway/pull/9238
 		tests.HTTPRouteRetryConnectionError.ShortName,
 		// https://github.com/kubernetes-sigs/gateway-api/issues/4977
