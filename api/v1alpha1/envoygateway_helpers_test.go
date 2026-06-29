2440de2470afd119e69f39cadcfebcdf44c94e98
Zachary Nixon <nixozach@amazon.com>
2026-06-29 12:39:43 -0700

    Fix review comments Signed-off-by: Zachary Nixon <nixozach@amazon.com>

--- api/v1alpha1/envoygateway_helpers_test.go
+++ api/v1alpha1/envoygateway_helpers_test.go
@@ -160,3 +160,138 @@ func TestIsInfraManagedRemotely(t *testing.T) {
 		})
 	}
 }
+
+func TestWatchesNamespaces(t *testing.T) {
+	tests := []struct {
+		name     string
+		eg       *EnvoyGateway
+		expected bool
+	}{
+		{
+			name: "nil provider",
+			eg: &EnvoyGateway{
+				EnvoyGatewaySpec: EnvoyGatewaySpec{
+					Provider: nil,
+				},
+			},
+			expected: false,
+		},
+		{
+			name: "non-kubernetes provider",
+			eg: &EnvoyGateway{
+				EnvoyGatewaySpec: EnvoyGatewaySpec{
+					Provider: &EnvoyGatewayProvider{
+						Type: ProviderTypeCustom,
+						Custom: &EnvoyGatewayCustomProvider{
+							Resource: EnvoyGatewayResourceProvider{
+								Type: ResourceProviderTypeFile,
+							},
+						},
+					},
+				},
+			},
+			expected: false,
+		},
+		{
+			name: "kubernetes provider with nil watch",
+			eg: &EnvoyGateway{
+				EnvoyGatewaySpec: EnvoyGatewaySpec{
+					Provider: &EnvoyGatewayProvider{
+						Type: ProviderTypeKubernetes,
+						Kubernetes: &EnvoyGatewayKubernetesProvider{
+							EnvoyGatewayKubernetesConfiguration: EnvoyGatewayKubernetesConfiguration{
+								Watch: nil,
+							},
+						},
+					},
+				},
+			},
+			expected: false,
+		},
+		{
+			name: "kubernetes provider with namespace selector watch mode",
+			eg: &EnvoyGateway{
+				EnvoyGatewaySpec: EnvoyGatewaySpec{
+					Provider: &EnvoyGatewayProvider{
+						Type: ProviderTypeKubernetes,
+						Kubernetes: &EnvoyGatewayKubernetesProvider{
+							EnvoyGatewayKubernetesConfiguration: EnvoyGatewayKubernetesConfiguration{
+								Watch: &KubernetesWatchMode{
+									Type: KubernetesWatchModeTypeNamespaceSelector,
+								},
+							},
+						},
+					},
+				},
+			},
+			expected: false,
+		},
+		{
+			name: "kubernetes provider with namespaces watch mode but empty namespaces",
+			eg: &EnvoyGateway{
+				EnvoyGatewaySpec: EnvoyGatewaySpec{
+					Provider: &EnvoyGatewayProvider{
+						Type: ProviderTypeKubernetes,
+						Kubernetes: &EnvoyGatewayKubernetesProvider{
+							EnvoyGatewayKubernetesConfiguration: EnvoyGatewayKubernetesConfiguration{
+								Watch: &KubernetesWatchMode{
+									Type:       KubernetesWatchModeTypeNamespaces,
+									Namespaces: []string{},
+								},
+							},
+						},
+					},
+				},
+			},
+			expected: false,
+		},
+		{
+			name: "kubernetes provider with namespaces watch mode and namespaces set",
+			eg: &EnvoyGateway{
+				EnvoyGatewaySpec: EnvoyGatewaySpec{
+					Provider: &EnvoyGatewayProvider{
+						Type: ProviderTypeKubernetes,
+						Kubernetes: &EnvoyGatewayKubernetesProvider{
+							EnvoyGatewayKubernetesConfiguration: EnvoyGatewayKubernetesConfiguration{
+								Watch: &KubernetesWatchMode{
+									Type:       KubernetesWatchModeTypeNamespaces,
+									Namespaces: []string{"ns-a", "ns-b"},
+								},
+							},
+						},
+					},
+				},
+			},
+			expected: true,
+		},
+		{
+			name: "custom kubernetes resource provider with namespaces watch mode and namespaces set",
+			eg: &EnvoyGateway{
+				EnvoyGatewaySpec: EnvoyGatewaySpec{
+					Provider: &EnvoyGatewayProvider{
+						Type: ProviderTypeCustom,
+						Custom: &EnvoyGatewayCustomProvider{
+							Resource: EnvoyGatewayResourceProvider{
+								Type: ResourceProviderTypeKubernetes,
+								Kubernetes: &EnvoyGatewayKubernetesCustomProvider{
+									EnvoyGatewayKubernetesConfiguration: EnvoyGatewayKubernetesConfiguration{
+										Watch: &KubernetesWatchMode{
+											Type:       KubernetesWatchModeTypeNamespaces,
+											Namespaces: []string{"ns-a"},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			expected: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.expected, tt.eg.WatchesNamespaces())
+		})
+	}
+}
