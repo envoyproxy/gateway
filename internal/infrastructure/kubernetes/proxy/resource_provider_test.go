54872475955c3d8d22b8e7c5ce5a18b8cd95f2cd
zirain <zirain2009@gmail.com>
2026-06-30 14:40:32 +0800

    fix: DaemonSet pod not using the configured custom ServiceAccount name (#9347)
    * add test case
    
    Signed-off-by: zirain <zirain2009@gmail.com>
    
    * fix daemonset pod serviceaccount name
    
    Signed-off-by: zirain <zirain2009@gmail.com>
    
    * release notes
    
    Signed-off-by: zirain <zirain2009@gmail.com>
    
    ---------
    
    Signed-off-by: zirain <zirain2009@gmail.com>
    (cherry picked from commit 59a0cabe4b7703719797ed89f164e6d6cb2fe233)

--- internal/infrastructure/kubernetes/proxy/resource_provider_test.go
+++ internal/infrastructure/kubernetes/proxy/resource_provider_test.go
@@ -1147,6 +1147,12 @@ func TestDaemonSet(t *testing.T) {
 			daemonset:            nil,
 			gatewayNamespaceMode: true,
 		},
+		{
+			caseName:             "custom-sa",
+			infra:                newTestInfraWithCustomServiceAccount(types.NamespacedName{Namespace: "ns1", Name: "gateway-1"}),
+			daemonset:            nil,
+			gatewayNamespaceMode: true,
+		},
 	}
 	for _, tc := range cases {
 		t.Run(tc.caseName, func(t *testing.T) {
