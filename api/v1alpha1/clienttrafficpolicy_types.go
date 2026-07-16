09ba2760dd7d73560fa816d8b5386081a65442c1
aias00 <liuhongyu@apache.org>
2026-07-16 09:33:03 +0800

    Update api/v1alpha1/clienttrafficpolicy_types.go
    Co-authored-by: zirain <zirain2009@gmail.com>
    Signed-off-by: aias00 <liuhongyu@apache.org>

--- api/v1alpha1/clienttrafficpolicy_types.go
+++ api/v1alpha1/clienttrafficpolicy_types.go
@@ -468,7 +468,7 @@ type ProxyProtocolSettings struct {
 	// scheme for the forwarded request.
 	//
 	// +optional
-	ForwardProtoConfig *ForwardProtoConfig `json:"forwardProtoConfig,omitempty"`
+	ForwardProto *ForwardProtoConfig `json:"forwardProtoConfig,omitempty"`
 }
 
 // ForwardProtoConfig configures the ports used to infer the x-forwarded-proto
