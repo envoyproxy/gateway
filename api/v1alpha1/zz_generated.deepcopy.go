beclab/envoy-gateway

feat: whitelist for allowed env and paths for lua
Signed-off-by: Rudrakh Panigrahi <rudrakh97@gmail.com>

api/v1alpha1/zz_generated.deepcopy.go
@@ -3255,6 +3255,11 @@ func (in *EnvoyProxySpec) DeepCopyInto(out *EnvoyProxySpec) {
 		*out = new(LuaValidation)
 		**out = **in
 	}
+	if in.LuaValidationAllowlist != nil {
+		in, out := &in.LuaValidationAllowlist, &out.LuaValidationAllowlist
+		*out = new(LuaValidationAllowlist)
+		(*in).DeepCopyInto(*out)
+	}
 	if in.DynamicModules != nil {
 		in, out := &in.DynamicModules, &out.DynamicModules
 		*out = make([]DynamicModuleEntry, len(*in))
@@ -5918,6 +5923,31 @@ func (in *Lua) DeepCopy() *Lua {
 		return nil
 	}
 	out := new(Lua)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *LuaValidationAllowlist) DeepCopyInto(out *LuaValidationAllowlist) {
+	*out = *in
+	if in.AllowedPaths != nil {
+		in, out := &in.AllowedPaths, &out.AllowedPaths
+		*out = make([]string, len(*in))
+		copy(*out, *in)
+	}
+	if in.AllowedEnvVars != nil {
+		in, out := &in.AllowedEnvVars, &out.AllowedEnvVars
+		*out = make([]string, len(*in))
+		copy(*out, *in)
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new LuaValidationAllowlist.
+func (in *LuaValidationAllowlist) DeepCopy() *LuaValidationAllowlist {
+	if in == nil {
+		return nil
+	}
+	out := new(LuaValidationAllowlist)
 	in.DeepCopyInto(out)
 	return out
 }

envoyproxy/gateway

api/v1alpha1/zz_generated.deepcopy.go
@@ -3294,6 +3294,11 @@ func (in *EnvoyProxySpec) DeepCopyInto(out *EnvoyProxySpec) {
 		*out = new(LuaValidation)
 		**out = **in
 	}
+	if in.LuaValidationConfig != nil {
+		in, out := &in.LuaValidationConfig, &out.LuaValidationConfig
+		*out = new(LuaValidationConfig)
+		(*in).DeepCopyInto(*out)
+	}
 	if in.DynamicModules != nil {
 		in, out := &in.DynamicModules, &out.DynamicModules
 		*out = make([]DynamicModuleEntry, len(*in))
@@ -6007,6 +6012,31 @@ func (in *Lua) DeepCopy() *Lua {
 }
 
 // DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *LuaValidationConfig) DeepCopyInto(out *LuaValidationConfig) {
+	*out = *in
+	if in.Type != nil {
+		in, out := &in.Type, &out.Type
+		*out = new(LuaValidation)
+		**out = **in
+	}
+	if in.Strict != nil {
+		in, out := &in.Strict, &out.Strict
+		*out = new(StrictValidation)
+		(*in).DeepCopyInto(*out)
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new LuaValidationConfig.
+func (in *LuaValidationConfig) DeepCopy() *LuaValidationConfig {
+	if in == nil {
+		return nil
+	}
+	out := new(LuaValidationConfig)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
 func (in *MethodMatch) DeepCopyInto(out *MethodMatch) {
 	*out = *in
 	if in.Invert != nil {
@@ -8262,6 +8292,31 @@ func (in *StatusCodeRange) DeepCopy() *StatusCodeRange {
 		return nil
 	}
 	out := new(StatusCodeRange)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *StrictValidation) DeepCopyInto(out *StrictValidation) {
+	*out = *in
+	if in.AllowedPaths != nil {
+		in, out := &in.AllowedPaths, &out.AllowedPaths
+		*out = make([]string, len(*in))
+		copy(*out, *in)
+	}
+	if in.AllowedEnvVars != nil {
+		in, out := &in.AllowedEnvVars, &out.AllowedEnvVars
+		*out = make([]string, len(*in))
+		copy(*out, *in)
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new StrictValidation.
+func (in *StrictValidation) DeepCopy() *StrictValidation {
+	if in == nil {
+		return nil
+	}
+	out := new(StrictValidation)
 	in.DeepCopyInto(out)
 	return out
 }