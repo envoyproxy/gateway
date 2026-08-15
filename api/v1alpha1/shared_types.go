39763b1c07ec30a5526fe70434a2cfe4181de642
Kota Kimura <86363983+kkk777-7@users.noreply.github.com>
2026-08-15 16:36:24 +0900

    bump ratelimit version (#9757)
    Signed-off-by: kkk777-7 <kota.kimura0725@gmail.com>

--- api/v1alpha1/shared_types.go
+++ api/v1alpha1/shared_types.go
@@ -34,7 +34,7 @@ const (
 	// DefaultShutdownManagerImage is the default image used for the shutdown manager.
 	DefaultShutdownManagerImage = "docker.io/envoyproxy/gateway-dev:latest"
 	// DefaultRateLimitImage is the default image used by ratelimit.
-	DefaultRateLimitImage = "docker.io/envoyproxy/ratelimit:4969da53"
+	DefaultRateLimitImage = "docker.io/envoyproxy/ratelimit:17b1956c"
 	// HTTPProtocol is the common-used http protocol.
 	HTTPProtocol = "http"
 	// GRPCProtocol is the common-used grpc protocol.
