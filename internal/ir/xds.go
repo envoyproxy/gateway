1f9a811a5737f63e60925e023c8d634f0b9cf0e6
Kota Kimura <86363983+kkk777-7@users.noreply.github.com>
2026-09-25 10:53:04 +0900

    fix: scope merged cluster settings by backend protocol (#10016)
    * fix: scope merged cluster settings by backend protocol
    
    Signed-off-by: kkk777-7 <kota.kimura0725@gmail.com>
    
    * rename release note
    
    Signed-off-by: kkk777-7 <kota.kimura0725@gmail.com>
    
    ---------
    
    Signed-off-by: kkk777-7 <kota.kimura0725@gmail.com>

--- internal/ir/xds.go
+++ internal/ir/xds.go
@@ -2278,6 +2278,14 @@ type BackendCluster struct {
 	UseClientProtocol *bool `json:"useClientProtocol,omitempty" yaml:"useClientProtocol,omitempty"`
 }
 
+// Protocol reports the upstream protocol this cluster serves.
+func (b *BackendCluster) Protocol() AppProtocol {
+	if b == nil || b.Setting == nil {
+		return ""
+	}
+	return b.Setting.Protocol
+}
+
 func (b *BackendCluster) Validate() error {
 	var errs error
 	if len(b.Name) == 0 {
